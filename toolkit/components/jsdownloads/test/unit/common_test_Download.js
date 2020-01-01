3af9b58c718aa8350a3669e6c2afc25e56dd95cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 23:04:47 +0000

    Bug 916430 - General Downloads API review. r=enn
    UltraBlame original commit: 36e4e8f37a17466d55ca262aa2732a71d912a1f6

--- toolkit/components/jsdownloads/test/unit/common_test_Download.js
+++ toolkit/components/jsdownloads/test/unit/common_test_Download.js
@@ -4926,7 +4926,7 @@ TEST_TARGET_FILE_NAME
 yield
 Downloads
 .
-startDirect
+fetch
 (
 sourceUrl
 targetFile
@@ -4935,7 +4935,7 @@ targetFile
 yield
 Downloads
 .
-startDirect
+fetch
 (
 sourceUrl
 targetFile
