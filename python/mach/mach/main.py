44f447fd6bf6830a484853b2418216428c3d551d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 14:26:24 +0000

    Bug 795427 - Part 2: Terminate mach silently when KeyboardInterrupt is raised; r=jhammel
    DONTBUILD (NPOTB)
    
    UltraBlame original commit: c24a0fd080310af8028084c89cfdf1052a58d38b

--- python/mach/mach/main.py
+++ python/mach/mach/main.py
@@ -714,6 +714,29 @@ _run
 argv
 )
         
+except
+KeyboardInterrupt
+:
+            
+print
+(
+'
+mach
+interrupted
+by
+signal
+or
+user
+action
+.
+Stopping
+.
+'
+)
+            
+return
+1
+        
 finally
 :
             
