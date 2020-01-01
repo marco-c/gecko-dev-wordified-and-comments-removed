4e29b9814a4c3a02edfbe11adccb0aa55bee5fe7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 10:17:09 +0000

    Bug 726416 - Fix some content/ build warnings; r=mounir
    UltraBlame original commit: 254285dbf95469aeb902d422d12e2def71d77eee

--- content/media/nsBuiltinDecoderStateMachine.cpp
+++ content/media/nsBuiltinDecoderStateMachine.cpp
@@ -86,6 +86,15 @@ StdInt
 .
 h
 "
+#
+include
+"
+mozilla
+/
+Util
+.
+h
+"
 using
 namespace
 mozilla
@@ -707,8 +716,11 @@ thread
 "
 )
 ;
+DebugOnly
+<
 nsresult
-res
+>
+rv
 =
 NS_NewThread
 (
@@ -721,7 +733,7 @@ NS_ABORT_IF_FALSE
 (
 NS_SUCCEEDED
 (
-res
+rv
 )
 "
 Can
