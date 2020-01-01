3b1369a4fff4d22a5f72fd49e2d42aed5c152a0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 10:08:13 +0000

    Bug 235853 - Defer proxy resolution for HTTP and HTTPS PAC to avoid blocking main thread during DNS resolution. r=biesi,sworkman
    UltraBlame original commit: 20240add1d35006c37e0c963a6dc3add30b7e72f

--- netwerk/base/src/nsProtocolProxyService.cpp
+++ netwerk/base/src/nsProtocolProxyService.cpp
@@ -4167,6 +4167,7 @@ kProxyType_HTTP
 kProxyType_SOCKS
 kProxyType_SOCKS4
 kProxyType_DIRECT
+kProxyType_UNKNOWN
 }
 ;
 const
--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -671,6 +671,8 @@ nsIChannel
 *
 bool
 preserveMethod
+bool
+forProxy
 )
 ;
 nsresult
