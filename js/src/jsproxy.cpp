941c8c71f572960d7d1991b395b593e29fca7131
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 08:22:44 +0000

    Merge TM -> JM
    UltraBlame original commit: bdacf8b9c9c4a342d7da79e89ce088bdd65cd189

--- js/src/jsproxy.cpp
+++ js/src/jsproxy.cpp
@@ -170,7 +170,7 @@ JSObject
 proxy
 )
 {
-JSPendingProxyOperation
+PendingProxyOperation
 *
 op
 =
@@ -4241,11 +4241,11 @@ singleton
 class
 AutoPendingProxyOperation
 {
-JSThreadData
+ThreadData
 *
 data
 ;
-JSPendingProxyOperation
+PendingProxyOperation
 op
 ;
 public
