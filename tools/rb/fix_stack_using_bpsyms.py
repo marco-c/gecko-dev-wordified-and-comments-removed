d44e9245dcf01598b867f20b21fb64e251c152f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:42:29 +0000

    bug 751673 - fix Windows assertion stacks. r=dbaron
    UltraBlame original commit: b871dfb2186fb5750284d5c05771b3d7bca8292b

--- tools/rb/fix_stack_using_bpsyms.py
+++ tools/rb/fix_stack_using_bpsyms.py
@@ -68,12 +68,7 @@ hg
 )
 :
     
-(
-junk
-repo
-path
-rev
-)
+bits
 =
 name
 .
@@ -84,6 +79,25 @@ split
 "
 )
     
+if
+len
+(
+bits
+)
+=
+=
+4
+:
+      
+(
+junk
+repo
+path
+rev
+)
+=
+bits
+      
 return
 path
 +
@@ -98,7 +112,7 @@ name
 :
 "
 class
-readSymbolFile
+SymbolFile
 :
   
 def
@@ -483,6 +497,39 @@ d1
 )
 :
     
+fn
+=
+fn
++
+"
+.
+pdb
+"
+    
+d1
+=
+os
+.
+path
+.
+join
+(
+symbolsDir
+fn
+)
+    
+if
+not
+os
+.
+path
+.
+exists
+(
+d1
+)
+:
+      
 return
 None
   
@@ -540,6 +587,27 @@ for
 fn
 )
   
+if
+fn
+.
+endswith
+(
+"
+.
+pdb
+"
+)
+:
+    
+fn
+=
+fn
+[
+:
+-
+4
+]
+  
 return
 os
 .
@@ -564,10 +632,9 @@ parsedSymbolFiles
 {
 }
 def
-addressToSymbol
+getSymbolFile
 (
 file
-address
 symbolsDir
 )
 :
@@ -597,7 +664,7 @@ symfile
       
 p
 =
-readSymbolFile
+SymbolFile
 (
 symfile
 )
@@ -625,6 +692,25 @@ parsedSymbolFiles
 [
 file
 ]
+  
+return
+p
+def
+addressToSymbol
+(
+file
+address
+symbolsDir
+)
+:
+  
+p
+=
+getSymbolFile
+(
+file
+symbolsDir
+)
   
 if
 p
@@ -678,7 +764,7 @@ F
 ]
 {
 1
-8
+16
 }
 )
 \
@@ -822,7 +908,7 @@ n
   
 else
 :
-      
+    
 return
 line
 if
