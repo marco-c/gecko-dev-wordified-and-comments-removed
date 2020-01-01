d2fd820b142985f48336e44e235cc8a8c1709138
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:40:52 +0000

    Sisyphus - timed_run.py should flush stdout before terminating process, bug 418474, r=coop
    UltraBlame original commit: b0763558fe56e13c58dd1fe94368d6763fc32d48

--- testing/sisyphus/bin/timed_run.py
+++ testing/sisyphus/bin/timed_run.py
@@ -95,6 +95,14 @@ argv
 ]
 )
         
+sys
+.
+stdout
+.
+flush
+(
+)
+        
 os
 .
 kill
@@ -106,7 +114,38 @@ SIGKILL
 )
     
 except
+OSError
+e
 :
+        
+print
+"
+timed_run
+.
+py
+:
+exception
+trying
+to
+kill
+process
+:
+%
+d
+(
+%
+s
+)
+"
+%
+(
+e
+.
+errno
+e
+.
+strerror
+)
         
 pass
     
