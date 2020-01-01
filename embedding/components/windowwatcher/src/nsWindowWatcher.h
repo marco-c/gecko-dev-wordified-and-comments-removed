d4c7cec85e76fbd455590f960e9f7c057b6318a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 12:57:00 +0000

    Bug 769254 - Part 2: Modify nsPIWindowWatcher::OpenWindowJS (renamed to OpenWindow2) so we can pass in the URL for target=_blank links without navigating the opened window to that URL. r=bz
    UltraBlame original commit: b9d4cc7af705429fc720c67dd232d9f16d971fd9

--- embedding/components/windowwatcher/src/nsWindowWatcher.h
+++ embedding/components/windowwatcher/src/nsWindowWatcher.h
@@ -219,7 +219,7 @@ aResult
 )
 ;
 nsresult
-OpenWindowJSInternal
+OpenWindowInternal
 (
 nsIDOMWindow
 *
@@ -237,12 +237,14 @@ char
 *
 aFeatures
 bool
+aCalledFromJS
+bool
 aDialog
+bool
+aNavigate
 nsIArray
 *
 argv
-bool
-aCalledFromJS
 nsIDOMWindow
 *
 *
