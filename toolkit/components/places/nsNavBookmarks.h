4ec8edb797ffa529e2e0053d5029ea73cfa964c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 22:32:31 +0000

    Bug 627487 - Bookmark JSON backup should contain new-style GUIDs. r=mano
    UltraBlame original commit: a4fc8aae3053febd012e1886b2e900008706f259

--- toolkit/components/places/nsNavBookmarks.h
+++ toolkit/components/places/nsNavBookmarks.h
@@ -831,11 +831,6 @@ kGetChildrenIndex_PlaceID
 static
 const
 int32_t
-kGetChildrenIndex_FolderTitle
-;
-static
-const
-int32_t
 kGetChildrenIndex_Guid
 ;
 class
--- toolkit/components/places/nsNavHistory.h
+++ toolkit/components/places/nsNavHistory.h
@@ -556,6 +556,11 @@ const
 int32_t
 kGetInfoIndex_Hidden
 ;
+static
+const
+int32_t
+kGetInfoIndex_Guid
+;
 int64_t
 GetTagsFolder
 (
