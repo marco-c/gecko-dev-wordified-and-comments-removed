5e3c5068291531446168344d12b1181c9760c186
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 22:56:59 +0000

    Bug 500388 - Don't buffer xpcshell process output, use a callback to consume output as it is available. r=ted
    UltraBlame original commit: 3c7913e853b86dbe2503819145830689747ab59b

--- testing/xpcshell/mach_commands.py
+++ testing/xpcshell/mach_commands.py
@@ -911,6 +911,34 @@ debuggerInteractive
 '
 :
 debuggerInteractive
+            
+'
+on_message
+'
+:
+(
+lambda
+obj
+msg
+:
+xpcshell
+.
+log
+.
+info
+(
+msg
+)
+)
+\
+                            
+if
+test_path
+is
+not
+None
+else
+None
         
 }
         
