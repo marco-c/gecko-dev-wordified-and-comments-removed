46c07d57b80abeb405a5bc9efb9d1b91e977a028
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:04:35 +0000

    Use JS_GetGlobalForObject to get the global object. Walking back the call chain is not equivalent and not safe.
    UltraBlame original commit: 14f0ea38fac42068e461932ef0c704b5e83bfb83

--- js/src/jstracer.h
+++ js/src/jstracer.h
@@ -302,9 +302,9 @@ JSContext
 *
 cx
 ;
-JSStackFrame
+JSObject
 *
-global
+globalObj
 ;
 Tracker
 tracker
@@ -1051,13 +1051,6 @@ TraceRecorder
 JSContext
 *
 getContext
-(
-)
-const
-;
-JSStackFrame
-*
-getGlobalFrame
 (
 )
 const
