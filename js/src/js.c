5bea1eaa6f1caef30454a63414b81aa2b9324df2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:46:00 +0000

    global_resolve should not do anything if assigning.
    UltraBlame original commit: eeeff8178e5c328fdd49d320770677411452df72

--- js/src/js.c
+++ js/src/js.c
@@ -17855,6 +17855,18 @@ objp
 #
 ifdef
 LAZY_STANDARD_CLASSES
+if
+(
+(
+flags
+&
+JSRESOLVE_ASSIGNING
+)
+=
+=
+0
+)
+{
 JSBool
 resolved
 ;
@@ -17886,6 +17898,7 @@ obj
 return
 JS_TRUE
 ;
+}
 }
 #
 endif
