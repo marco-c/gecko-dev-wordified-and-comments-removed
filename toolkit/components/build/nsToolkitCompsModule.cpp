d667fffc36a94e3b5ebc9d589e6036975c9f24df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 18:48:23 +0000

    Bug 851461 - Make the JavaScript API for downloads available in parallel to nsIDownloadManager. r=mak
    UltraBlame original commit: 833dc6a7c4754a4ee5753e6af0609c0f546e49cc

--- toolkit/components/build/nsToolkitCompsModule.cpp
+++ toolkit/components/build/nsToolkitCompsModule.cpp
@@ -480,9 +480,6 @@ false
 NULL
 nsDownloadManagerConstructor
 }
-#
-ifndef
-MOZ_JSDOWNLOADS
 {
 &
 kNS_DOWNLOAD_CID
@@ -490,8 +487,6 @@ false
 NULL
 nsDownloadProxyConstructor
 }
-#
-endif
 {
 &
 kNS_FIND_SERVICE_CID
@@ -627,16 +622,11 @@ NS_DOWNLOADMANAGER_CONTRACTID
 &
 kNS_DOWNLOADMANAGER_CID
 }
-#
-ifndef
-MOZ_JSDOWNLOADS
 {
 NS_TRANSFER_CONTRACTID
 &
 kNS_DOWNLOAD_CID
 }
-#
-endif
 {
 NS_FIND_SERVICE_CONTRACTID
 &
