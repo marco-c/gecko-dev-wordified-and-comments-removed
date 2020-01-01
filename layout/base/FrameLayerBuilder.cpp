ae173281cd27f595ac9f2b1b7b26d2f683d3d8fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 11:15:37 +0000

    Bug 607417. Improve definition of the 'fixed layer' flag. r=tnikkel
    UltraBlame original commit: ba39b0822992dec1a16ed00647ee14a334cd5791

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -6624,7 +6624,7 @@ SetIsFixedPosition
 nsLayoutUtils
 :
 :
-ScrolledByViewportScrolling
+IsScrolledByRootContentDocumentDisplayportScrolling
 (
 activeScrolledRoot
 mBuilder
@@ -6919,7 +6919,7 @@ SetIsFixedPosition
 nsLayoutUtils
 :
 :
-ScrolledByViewportScrolling
+IsScrolledByRootContentDocumentDisplayportScrolling
 (
 activeScrolledRoot
 mBuilder
