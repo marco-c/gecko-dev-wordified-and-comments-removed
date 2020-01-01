5c5f7848c2bdf334049d917657dba40eb46e8157
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:53:19 +0000

    Backed out changeset 919950c7f0f0
    UltraBlame original commit: d6c920a57c9aed4bcd2e7dbfad4e7ec1195333a4

--- js/src/jsatom.cpp
+++ js/src/jsatom.cpp
@@ -4598,6 +4598,10 @@ space
 ;
 }
 }
+JS_ARENA_ALLOCATE
+(
+space
+&
 parser
 -
 >
@@ -4605,10 +4609,6 @@ context
 -
 >
 tempPool
-.
-allocate
-(
-space
 size
 )
 ;
@@ -4767,6 +4767,11 @@ ale
 entry
 ;
 }
+JS_ARENA_ALLOCATE_TYPE
+(
+ale
+JSAtomListElement
+&
 parser
 -
 >
@@ -4774,13 +4779,6 @@ context
 -
 >
 tempPool
-.
-allocateType
-<
-JSAtomListElement
->
-(
-ale
 )
 ;
 if
