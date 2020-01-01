b355a8eb22caab235e705ece3830e039cbb24305
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:04:05 +0000

    I'm going to just keep bludgeoning these typedefs until they stick
    UltraBlame original commit: eeb774c6cec9c65dc9f137a376f31629530b4612

--- js/src/nanojit/avmplus.h
+++ js/src/nanojit/avmplus.h
@@ -121,8 +121,6 @@ typedef
 JSWord
 intptr_t
 ;
-#
-endif
 typedef
 JSUint8
 uint8_t
@@ -139,6 +137,17 @@ typedef
 JSUint64
 uint64_t
 ;
+#
+else
+#
+include
+<
+stdint
+.
+h
+>
+#
+endif
 #
 if
 defined
