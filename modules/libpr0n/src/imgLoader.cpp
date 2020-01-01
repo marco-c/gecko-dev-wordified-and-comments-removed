416a3ce28e27a8d8c2c5de7672a7218570c25ddd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:35:04 +0000

    Handle 302 redirects properly in imagelib. b=89419 r=bzbarsky sr=vlad/pavlov
    UltraBlame original commit: 74a85a545f0ad4a3ad20875bb141b8c551548f6f

--- modules/libpr0n/src/imgLoader.cpp
+++ modules/libpr0n/src/imgLoader.cpp
@@ -4546,6 +4546,7 @@ Init
 (
 aURI
 loadGroup
+newChannel
 entry
 cacheId
 aCX
@@ -5120,14 +5121,6 @@ nsnull
 }
 else
 {
-nsIThread
-*
-thread
-=
-NS_GetCurrentThread
-(
-)
-;
 NewRequestAndEntry
 (
 uri
@@ -5165,8 +5158,11 @@ Init
 (
 originalURI
 channel
+channel
 entry
-thread
+NS_GetCurrentThread
+(
+)
 aCX
 )
 ;
@@ -6430,6 +6426,7 @@ request
 Init
 (
 originalURI
+channel
 channel
 entry
 NS_GetCurrentThread
