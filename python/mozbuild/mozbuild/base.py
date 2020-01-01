ffbefcb00a35f831d7dcc1dbf9e9e2b491ad7706
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 14:21:13 +0000

    Bug 795034 - Fix append_env argument to mozbuild's _run_command; r=gps
    UltraBlame original commit: 9598fc30867529ca71116c30c937afee79b45aa4

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -13,8 +13,6 @@ pymake
 .
 parser
 import
-shlex
-import
 sys
 import
 subprocess
@@ -32,10 +30,6 @@ pymake
 data
 import
 Makefile
-from
-tempfile
-import
-TemporaryFile
 from
 mozbuild
 .
@@ -1932,7 +1926,7 @@ use_env
 .
 update
 (
-env
+append_env
 )
         
 p
