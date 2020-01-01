a1a1929a05c5ba277564afe3015af643d85fb0db
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:32:01 +0000

    back out bug 118312 as final attempt to fix Ts
    UltraBlame original commit: 17977a79e0035e6144098a53f0da8914b1846745

--- widget/src/gtk2/gtkdrawing.h
+++ widget/src/gtk2/gtkdrawing.h
@@ -172,7 +172,6 @@ MOZ_GTK_TABPANELS
 MOZ_GTK_TREEVIEW
 MOZ_GTK_TREE_HEADER_CELL
 MOZ_GTK_TREE_HEADER_SORTARROW
-MOZ_GTK_EXPANDER
 MOZ_GTK_MENUBAR
 MOZ_GTK_MENUPOPUP
 MOZ_GTK_MENUITEM
@@ -310,14 +309,6 @@ width
 gint
 *
 height
-)
-;
-gint
-moz_gtk_get_expander_size
-(
-gint
-*
-size
 )
 ;
 GtkWidget
