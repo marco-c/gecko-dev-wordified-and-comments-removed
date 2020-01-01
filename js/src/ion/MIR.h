3a7f8e309b52bc366ef23f5c9413ca9ea7275dca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 21:00:11 +0000

    Backed out changeset ad74eb485a87 (bug 891695) for startup crashes on a CLOSED TREE
    UltraBlame original commit: a984893f3f2ac7c116fa7c8968b8d1e589d62c05

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -1349,6 +1349,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -1362,6 +1364,8 @@ congruentIfOperandsEqual
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -2888,6 +2892,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -3017,6 +3023,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -3056,6 +3064,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -6277,6 +6287,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -6615,6 +6627,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -6932,6 +6946,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -7483,6 +7499,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -7718,6 +7736,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -8017,6 +8037,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9375,6 +9397,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9518,6 +9542,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9644,6 +9670,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9760,6 +9788,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9866,6 +9896,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -9994,6 +10026,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -10361,6 +10395,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -11187,6 +11223,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -11391,6 +11429,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -11606,6 +11646,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -11759,6 +11801,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -11902,6 +11946,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -12032,6 +12078,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -12132,6 +12180,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -12371,6 +12421,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -13539,6 +13591,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -13736,6 +13790,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -14264,6 +14320,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -15763,6 +15821,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -15878,6 +15938,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16009,6 +16071,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16132,6 +16196,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16281,6 +16347,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16384,6 +16452,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16578,6 +16648,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16693,6 +16765,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -16804,6 +16878,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -17189,6 +17265,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -20394,6 +20472,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -20534,6 +20614,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -21300,6 +21382,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -21506,6 +21590,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -23704,6 +23790,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -23944,6 +24032,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -24145,6 +24235,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -24330,6 +24422,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -26461,6 +26555,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -26667,6 +26763,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -27636,6 +27734,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -27750,6 +27850,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 ins
 )
 const
@@ -28479,6 +28581,8 @@ congruentTo
 (
 MDefinition
 *
+const
+&
 def
 )
 const
