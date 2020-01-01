f464d3f25a023ba864ddfd5ed0c28c8efa5babe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:57:10 +0000

    backout bug 415563 because a gcc4.3 bug causes it to break our 32bit linux builders r=ted (transplanted from 8bc975526c0fb0912701477a79f0b94789dce9c2)
    UltraBlame original commit: a997842c9f5d3f6053fc068fa7d662b777787c7a

--- nsprpub/pr/src/threads/combined/pruthr.c
+++ nsprpub/pr/src/threads/combined/pruthr.c
@@ -4238,7 +4238,7 @@ flags
 =
 _PR_SYSTEM
 ;
-PR_ATOMIC_INCREMENT
+PR_AtomicIncrement
 (
 &
 _pr_systemActive
@@ -4246,7 +4246,7 @@ _pr_systemActive
 ;
 }
 else
-PR_ATOMIC_INCREMENT
+PR_AtomicIncrement
 (
 &
 _pr_userActive
