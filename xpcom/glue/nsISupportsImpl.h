25782c7ab5dc67baca44bd289cadd7e97a8b036a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:34:15 +0000

    Backed out changeset e94819033c77 (Bug 551298) due to Linux debug mochitest-5 orange
    UltraBlame original commit: c0395cad35f39dbcc3d0514c16f468a1a4ae84b9

--- xpcom/glue/nsISupportsImpl.h
+++ xpcom/glue/nsISupportsImpl.h
@@ -1070,12 +1070,6 @@ refcnt
 )
 ;
 \
-NS_ASSERT_OWNINGTHREAD
-(
-_class
-)
-;
-\
 +
 +
 mRefCnt
@@ -1114,12 +1108,6 @@ mRefCnt
 dup
 release
 "
-)
-;
-\
-NS_ASSERT_OWNINGTHREAD
-(
-_class
 )
 ;
 \
@@ -1172,8 +1160,6 @@ protected
 nsAutoRefCnt
 mRefCnt
 ;
-\
-NS_DECL_OWNINGTHREAD
 \
 public
 :
