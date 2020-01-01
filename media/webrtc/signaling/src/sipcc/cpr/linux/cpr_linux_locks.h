3968a057ca8f29c2d58d0dc48f6b434604a72a90
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 14:30:14 +0000

    Bug 792188: Import SIPCC into media/webrtc/signaling rs=jesup r=gerv
    UltraBlame original commit: f1b524d51e6b055a5e8fd1c07d37ddc4805c4a03

---
+++ media/webrtc/signaling/src/sipcc/cpr/linux/cpr_linux_locks.h
@@ -0,0 +1,12 @@
+#
+ifndef
+_CPR_LINUX_LOCKS_H_
+#
+define
+_CPR_LINUX_LOCKS_H_
+extern
+pthread_mutex_t
+linuxThreadMutex
+;
+#
+endif
---
+++ media/webrtc/signaling/src/sipcc/cpr/win32/cpr_win_assert.h
@@ -0,0 +1,15 @@
+#
+ifndef
+_CPR_CNU_ASSERT_H_
+#
+define
+_CPR_CNU_ASSERT_H_
+#
+include
+"
+assert
+.
+h
+"
+#
+endif
