21021229f7dc962ba038df9931420f33ad5ee341
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 08:06:11 +0000

    Bug 648429 - HTTP cache: compress compressible files; r=michal.novotny
    UltraBlame original commit: d17ac9b0254e79e0a0390c8309db0433fe8bfa46

--- netwerk/cache/nsCacheService.h
+++ netwerk/cache/nsCacheService.h
@@ -273,6 +273,12 @@ entry
 )
 ;
 static
+PRInt32
+CacheCompressionLevel
+(
+)
+;
+static
 nsCacheService
 *
 GlobalInstance
@@ -401,6 +407,14 @@ static
 void
 SetMemoryCache
 (
+)
+;
+static
+void
+SetCacheCompressionLevel
+(
+PRInt32
+level
 )
 ;
 static
