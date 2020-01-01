37fc127e767ebb04a109c6c4973c829bfb13b415
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 16:01:03 +0000

    bug 821593 - remove the nsIDOMRGBColor xpidl now that nothing uses it r=bz
    UltraBlame original commit: bc10be0681179170bb921e83e04f5bc6ce9dbe20

--- layout/style/nsDOMCSSRGBColor.h
+++ layout/style/nsDOMCSSRGBColor.h
@@ -30,13 +30,6 @@ h
 #
 include
 "
-nsIDOMNSRGBAColor
-.
-h
-"
-#
-include
-"
 nsWrapperCache
 .
 h
@@ -48,7 +41,7 @@ class
 nsDOMCSSRGBColor
 :
 public
-nsIDOMNSRGBAColor
+nsISupports
 public
 nsWrapperCache
 {
@@ -80,8 +73,6 @@ void
 )
 ;
 NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-NS_DECL_NSIDOMRGBCOLOR
-NS_DECL_NSIDOMNSRGBACOLOR
 NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS
 (
 nsDOMCSSRGBColor
