124c52d194238fbfd38f62340738a6861bea5aef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:36:33 +0000

    Readd changes to nsJSEnvironment.{cpp,h}, and adjust js::LazilyConstructed so as to force pointer alignment on the theory that's what was wrong.
    UltraBlame original commit: bb4d4c6bf6bcd4c8dd570520e1ec91fcba86c6e1

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -73,12 +73,12 @@ nsIXPConnectJSObjectHolder
 class
 nsAutoPoolRelease
 ;
-class
-JSAutoTempValueRooter
-;
 namespace
 js
 {
+class
+AutoArrayRooter
+;
 template
 <
 class
@@ -713,7 +713,10 @@ js
 :
 LazilyConstructed
 <
-JSAutoTempValueRooter
+js
+:
+:
+AutoArrayRooter
 >
 &
 aRooter
