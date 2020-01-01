f3beb406c9cf2088d672c4f14796d1bceb8dff53
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 14:11:01 +0000

    Bug 785662 - Add NS_OpenAnonymousTemporaryFile(), and use it in nsMediaCache::Init(). r=roc
    UltraBlame original commit: 9d9d8bf662436bd9e2ec0b96f0de95922d077456

--- xpcom/build/nsXPComInit.cpp
+++ xpcom/build/nsXPComInit.cpp
@@ -694,7 +694,7 @@ ifdef
 XP_WIN
 extern
 nsresult
-ScheduleMediaCacheRemover
+ScheduleAnonTempFileRemover
 (
 )
 ;
@@ -2156,7 +2156,7 @@ NS_XPCOM_STARTUP_OBSERVER_ID
 #
 ifdef
 XP_WIN
-ScheduleMediaCacheRemover
+ScheduleAnonTempFileRemover
 (
 )
 ;
