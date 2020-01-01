2e51196d626dac6b0d44970d257c7e518d250964
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 15:17:57 +0000

    Bug 812226 - Package synthetic_gestures.js with the marionette_client, r=jgriffin
    UltraBlame original commit: 31c2faaae4b64180b3926561dda571d5a8970595

--- testing/marionette/client/setup.py
+++ testing/marionette/client/setup.py
@@ -12,7 +12,7 @@ version
 .
 5
 .
-6
+7
 '
 try
 :
@@ -207,6 +207,24 @@ tests
 '
 ]
 )
+      
+package_data
+=
+{
+'
+marionette
+'
+:
+[
+'
+touch
+/
+*
+.
+js
+'
+]
+}
       
 include_package_data
 =
