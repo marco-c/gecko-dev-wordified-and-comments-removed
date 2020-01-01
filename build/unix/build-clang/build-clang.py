c72733dbd723010c0894f26887e16fe5eb8aeed5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 13:12:03 +0000

    Bug 785772 - Don't use -O0 during stage1 on darwin. r=rail. We only need to build stage1 with -O0 on linux because of a bug in the gcc 4.5 we use.
    UltraBlame original commit: f400c8e5cea46069f4e68b2b801eb34ba810a250

--- build/unix/build-clang/build-clang.py
+++ build/unix/build-clang/build-clang.py
@@ -395,6 +395,18 @@ with_env
 env
 f
 )
+isDarwin
+=
+platform
+.
+system
+(
+)
+=
+=
+"
+Darwin
+"
 def
 build_one_stage_aux
 (
@@ -482,6 +494,9 @@ gcc
     
 if
 is_stage_one
+and
+not
+isDarwin
 :
         
 configure_opts
@@ -508,18 +523,6 @@ llvm_source_dir
 build_dir
 configure_opts
 )
-isDarwin
-=
-platform
-.
-system
-(
-)
-=
-=
-"
-Darwin
-"
 if
 isDarwin
 :
