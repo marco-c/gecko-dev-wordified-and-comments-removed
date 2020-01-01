43d1edde0c01f5438ad8e2864b99a32ae5d46370
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:50:08 +0000

    Backing out bug 477187. Breaks xpcshell.
    UltraBlame original commit: 35dc90d184e52c801a9c95c7df86778b89754b25

--- js/src/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/src/xpconnect/loader/mozJSComponentLoader.cpp
@@ -6698,10 +6698,10 @@ if
 targetObj
 )
 {
-JSCLContextHelper
-cx
+JSAutoRequest
+ar
 (
-this
+mContext
 )
 ;
 if
--- js/src/xpconnect/src/xpccomponents.cpp
+++ js/src/xpconnect/src/xpccomponents.cpp
@@ -13134,6 +13134,10 @@ JS_SetOperationCallback
 (
 mJSContext
 ContextHolderOperationCallback
+JS_GetOperationLimit
+(
+aOuterCx
+)
 )
 ;
 }
@@ -13208,6 +13212,15 @@ ok
 callback
 (
 origCx
+)
+;
+JS_SetOperationLimit
+(
+cx
+JS_GetOperationLimit
+(
+origCx
+)
 )
 ;
 return
