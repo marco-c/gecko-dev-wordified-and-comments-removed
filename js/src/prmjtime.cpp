96af196ba18a4a23ab91d292e5eb1975f8ab75df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:44:21 +0000

    Backed out changeset 525e42406396, bug 269538 (jscpucfg-ectomy) due to Windows TUnit bustage.
    UltraBlame original commit: 3b4b3555239203608307ebb31e790e9345ac145c

--- js/src/prmjtime.cpp
+++ js/src/prmjtime.cpp
@@ -453,6 +453,9 @@ low
 G1970GMTMICROLOW
 )
 ;
+#
+ifndef
+JS_HAVE_LONG_LONG
 JSLL_SHL
 (
 g1970GMTMicroSeconds
@@ -467,6 +470,17 @@ g1970GMTMicroSeconds
 16
 )
 ;
+#
+else
+JSLL_SHL
+(
+g1970GMTMicroSeconds
+g1970GMTMicroSeconds
+32
+)
+;
+#
+endif
 JSLL_ADD
 (
 g1970GMTMicroSeconds
