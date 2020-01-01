5a56f225c8562a6f18b96c19d4a36bc9289e72df
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 10:49:22 +0000

    Bug 729044 - Avoid nsIParserService in nsFind. r=bzbarsky.
    UltraBlame original commit: 5df6409bc7d24a8de5806710866897757d8e7062

--- embedding/components/find/src/nsFind.h
+++ embedding/components/find/src/nsFind.h
@@ -42,13 +42,6 @@ h
 #
 include
 "
-nsIParserService
-.
-h
-"
-#
-include
-"
 nsIWordBreaker
 .
 h
@@ -181,12 +174,6 @@ nsCOMPtr
 nsIWordBreaker
 >
 mWordBreaker
-;
-nsCOMPtr
-<
-nsIParserService
->
-mParserService
 ;
 PRInt32
 mIterOffset
