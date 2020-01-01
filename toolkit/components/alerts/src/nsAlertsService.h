787dbd6e99b241293ac586b400be4833a656822f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 05:29:08 +0000

    Bug 592088 - support progress indicators in android status bar notifications r=blassey a=blocking-fennec
    UltraBlame original commit: a3eadf884f811deccb9e982214cbbcb3d30f9321

--- toolkit/components/alerts/src/nsAlertsService.h
+++ toolkit/components/alerts/src/nsAlertsService.h
@@ -23,9 +23,12 @@ nsAlertsService
 :
 public
 nsIAlertsService
+public
+nsIAlertsProgressListener
 {
 public
 :
+NS_DECL_NSIALERTSPROGRESSLISTENER
 NS_DECL_NSIALERTSSERVICE
 NS_DECL_ISUPPORTS
 nsAlertsService
