ba1bd4a1af292d624d9e7c2d1fcc33f69bf624de
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 14:25:04 +0000

    Bug 965376 - Update final copy for Android Firefox Accounts. r=me
    UltraBlame original commit: 5c97f95f439a61cba24f6fe585c8cad386873bc2

--- mobile/android/base/fxa/activities/FxAccountGetStartedActivity.java
+++ mobile/android/base/fxa/activities/FxAccountGetStartedActivity.java
@@ -239,12 +239,12 @@ R
 .
 string
 .
-fxaccount_old_firefox
+fxaccount_getting_started_old_firefox
 R
 .
 string
 .
-fxaccount_link_old_firefox
+fxaccount_getting_started_link_old_firefox
 )
 ;
 View
@@ -311,6 +311,7 @@ CHILD_REQUEST_CODE
 )
 ;
 }
+Override
 public
 void
 onResume
