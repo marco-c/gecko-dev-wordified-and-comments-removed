500aef0fe5101df63acb59542e03c466444c8c7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 19:48:35 +0000

    Bug 1020470 - Package mozlog.structured html formatter support files, r=:ted.mielczarek
    UltraBlame original commit: af052494ad4edc374f795230c98b6073307b73b3

--- testing/mozbase/mozlog/setup.py
+++ testing/mozbase/mozlog/setup.py
@@ -13,7 +13,7 @@ PACKAGE_VERSION
 '
 1
 .
-8
+9
 '
 setup
 (
@@ -219,6 +219,38 @@ Modules
 '
                   
 ]
+      
+package_data
+=
+{
+"
+mozlog
+.
+structured
+"
+:
+[
+"
+formatters
+/
+html
+/
+main
+.
+js
+"
+                                          
+"
+formatters
+/
+html
+/
+style
+.
+css
+"
+]
+}
       
 entry_points
 =
