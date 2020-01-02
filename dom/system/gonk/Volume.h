0c2e2d34dec14e256a47633278265eed5a4c0e80
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 23:33:59 +0000

    Bug 1049240 - Integrate MTP with the AutoMounter. r=echou
    UltraBlame original commit: 19e59c14b0aac4b8f7ec3f21a8ba803395caf62b

--- dom/system/gonk/Volume.h
+++ dom/system/gonk/Volume.h
@@ -158,6 +158,16 @@ return
 mMountPoint
 ;
 }
+uint32_t
+Id
+(
+)
+const
+{
+return
+mId
+;
+}
 int32_t
 MountGeneration
 (
@@ -538,6 +548,9 @@ mIsSharing
 ;
 bool
 mIsFormatting
+;
+uint32_t
+mId
 ;
 static
 EventObserverList
