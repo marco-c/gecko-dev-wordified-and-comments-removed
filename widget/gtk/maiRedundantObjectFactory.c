b34c6c6f3ded657c53921ac091e680b819a51a1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:36:00 +0000

    Bug 945097. Build widget/gtk in unified mode. r=karl,ehsan
    UltraBlame original commit: 21b263335af58ee73f156340039b37d6fd5d27d0

--- widget/gtk/maiRedundantObjectFactory.c
+++ widget/gtk/maiRedundantObjectFactory.c
@@ -40,12 +40,6 @@ mai_redundant_object_factory_get_accessible_type
 void
 )
 ;
-static
-gpointer
-parent_class
-=
-NULL
-;
 GType
 mai_redundant_object_factory_get_type
 (
@@ -135,13 +129,6 @@ AtkObjectFactoryClass
 class
 =
 ATK_OBJECT_FACTORY_CLASS
-(
-klass
-)
-;
-parent_class
-=
-g_type_class_peek_parent
 (
 klass
 )
