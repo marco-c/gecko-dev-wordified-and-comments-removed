e6c8a9efd513071be98bdc80956c4ad65ce11e0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:38:34 +0000

    Bug 868789 - Name computation for SVG is wrong, r=tbsaude
    UltraBlame original commit: fea4b49abfa21c4191b7ae6c7842bf9fb725db3b

--- accessible/src/generic/Accessible.cpp
+++ accessible/src/generic/Accessible.cpp
@@ -1231,7 +1231,7 @@ IsSVG
 nsGkAtoms
 :
 :
-title
+desc
 )
 )
 {
@@ -1528,7 +1528,7 @@ IsSVG
 nsGkAtoms
 :
 :
-title
+desc
 )
 )
 {
@@ -11489,7 +11489,7 @@ IsSVG
 nsGkAtoms
 :
 :
-desc
+title
 )
 )
 {
