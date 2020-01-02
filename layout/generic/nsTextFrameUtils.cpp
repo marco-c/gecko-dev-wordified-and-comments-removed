c74322fbd40281346ef600722d6b6496eea7b611
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 13:38:30 +0000

    bug 955957 - pt 1 - reimplement gfxSkipChars and gfxSkipCharsIterator to perform better with huge text runs. r=roc
    UltraBlame original commit: 4b54755de2a7938cd68296831127af3e36bee632

--- layout/generic/nsTextFrameUtils.cpp
+++ layout/generic/nsTextFrameUtils.cpp
@@ -141,7 +141,7 @@ aCompression
 uint8_t
 *
 aIncomingFlags
-gfxSkipCharsBuilder
+gfxSkipChars
 *
 aSkipChars
 uint32_t
@@ -681,7 +681,7 @@ aCompression
 uint8_t
 *
 aIncomingFlags
-gfxSkipCharsBuilder
+gfxSkipChars
 *
 aSkipChars
 uint32_t
