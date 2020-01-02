10dd49a7558bedbacde7260f30b3ef9818e8ea6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:23:37 +0000

    Backed out changeset 192103a30df7 (bug 941868)
    UltraBlame original commit: 9047f1cc94ca33ac389955421eb34526c5f66066

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1174,7 +1174,7 @@ tab
 )
 )
 {
-loadFavicon
+maybeCancelFaviconLoad
 (
 tab
 )
@@ -1354,6 +1354,16 @@ invalidateOptionsMenu
 )
 ;
 }
+break
+;
+case
+PAGE_SHOW
+:
+loadFavicon
+(
+tab
+)
+;
 break
 ;
 case
