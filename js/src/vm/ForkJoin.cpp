89f1d745af595dbf0ad3450c7d7f0800733cc823
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 13:49:58 +0000

    Bug 952777 followup - fix JS_JITINFO_NATIVE_PARALLEL call in ForkJoin.cpp; r=bustage
    UltraBlame original commit: 524be0420e7933e0158e53ec01eefa6477dd769e

--- js/src/vm/ForkJoin.cpp
+++ js/src/vm/ForkJoin.cpp
@@ -9480,15 +9480,10 @@ return
 true
 ;
 }
-const
-JSJitInfo
-js
-:
-:
-intrinsic_SetForkJoinTargetRegionInfo
-=
 JS_JITINFO_NATIVE_PARALLEL
 (
+intrinsic_SetForkJoinTargetRegionInfo
+intrinsic_SetForkJoinTargetRegionInfo
 intrinsic_SetForkJoinTargetRegionPar
 )
 ;
