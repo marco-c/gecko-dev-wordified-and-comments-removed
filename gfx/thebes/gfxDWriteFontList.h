d9dfdd575ec3feafb3c02f352d961893bbdbae38
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:51:19 +0000

    Bug 947812 - use DirectWrite API for fullname/psname lookup. r=bas
    UltraBlame original commit: 775dfb379e04d15fbca2a6d7b7b5c29ffb422af6

--- gfx/thebes/gfxDWriteFontList.h
+++ gfx/thebes/gfxDWriteFontList.h
@@ -119,6 +119,17 @@ nsAString
 aLocalizedName
 )
 ;
+virtual
+void
+ReadFaceNames
+(
+gfxPlatformFontList
+*
+aPlatformFontList
+bool
+aNeedFullnamePostscriptNames
+)
+;
 void
 SetForceGDIClassic
 (
