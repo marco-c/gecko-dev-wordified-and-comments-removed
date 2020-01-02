e0691fa4f3aa2920a07c5c5ffdc80a1211c5cebb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 14:06:17 +0000

    Bug 620935 - __noSuchMethod__ support for WebIDL r=bz
    UltraBlame original commit: 3da2b2a2899b5dfb5d81c1349ea8af1363038178

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -1887,6 +1887,13 @@ name
 __content
 "
 and
+name
+!
+=
+"
+___noSuchMethod__
+"
+and
 not
 allowDoubleUnderscore
 :
@@ -20305,6 +20312,17 @@ name
 =
 "
 __
+"
+and
+self
+.
+identifier
+.
+name
+!
+=
+"
+__noSuchMethod__
 "
     
 def
