599282d6d7b3582a3072d6a5507b619ff9479b79
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 21:54:55 +0000

    Bug 983504 - Enumeration & MediaEngine changes for screen sharing. r=jesup,mt
    UltraBlame original commit: 47007b47379f75a868fd98d4c5244538d9699b37

--- content/media/webrtc/MediaEngineDefault.h
+++ content/media/webrtc/MediaEngineDefault.h
@@ -502,6 +502,10 @@ virtual
 void
 EnumerateVideoDevices
 (
+dom
+:
+:
+MediaSourceEnum
 nsTArray
 <
 nsRefPtr
@@ -516,6 +520,10 @@ virtual
 void
 EnumerateAudioDevices
 (
+dom
+:
+:
+MediaSourceEnum
 nsTArray
 <
 nsRefPtr
