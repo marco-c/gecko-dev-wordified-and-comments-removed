665710c342192415f394ff5581bbe8553af3e0ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 14:31:01 +0000

    Bug 784759 - Delay database initialization until after first page load. r=mfinkle
    UltraBlame original commit: 2c2c426e4afe7dd8205bf3aa54cce65eed096304

--- mobile/android/base/tests/testFormHistory.java
+++ mobile/android/base/tests/testFormHistory.java
@@ -130,7 +130,7 @@ ContentValues
 (
 )
 ;
-blockForGeckoReady
+blockForGeckoDelayedStartup
 (
 )
 ;
--- mobile/android/base/tests/testPasswordProvider.java
+++ mobile/android/base/tests/testPasswordProvider.java
@@ -137,7 +137,7 @@ ContentValues
 (
 )
 ;
-blockForGeckoReady
+blockForGeckoDelayedStartup
 (
 )
 ;
