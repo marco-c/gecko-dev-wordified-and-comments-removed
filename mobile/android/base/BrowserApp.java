132d407ffc25f5554030ed4e0c0ada038c97e580
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 19:15:25 +0000

    Bug 1014338 - Part 0: move Distribution class into its own package. r=margaret
    UltraBlame original commit: 90150f97a6de612f74ab38cce563eddabd9fce2d

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -178,6 +178,17 @@ mozilla
 .
 gecko
 .
+distribution
+.
+Distribution
+;
+import
+org
+.
+mozilla
+.
+gecko
+.
 favicons
 .
 Favicons
--- mobile/android/base/widget/ActivityChooserModel.java
+++ mobile/android/base/widget/ActivityChooserModel.java
@@ -14,6 +14,8 @@ mozilla
 .
 gecko
 .
+distribution
+.
 Distribution
 ;
 import
