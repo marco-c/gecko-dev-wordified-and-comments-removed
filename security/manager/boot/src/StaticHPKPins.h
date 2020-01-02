ff211c450deb9f5ead14d31a8edc11604289c8d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 18:19:27 +0000

    Bug 998057: Add tests for certificate pinning (r=cviecco,dkeeler)
    UltraBlame original commit: f39b83a39cf28d43dc5817e4d54034e4df7b2d66

--- security/manager/boot/src/StaticHPKPins.h
+++ security/manager/boot/src/StaticHPKPins.h
@@ -77,7 +77,9 @@ kEnd_Entity_Test_CertFingerprint
 ]
 =
 "
-97H5CNFJ2u3u1NvH3ru67t5OiCO8KydOyNh9GCEyAeM
+sEIYDccDj1ULE64YxhvqV7ASqc2qfIofVyArzg
++
+62hU
 =
 "
 ;
@@ -650,7 +652,7 @@ kPinSet_mozilla_test
 "
 include
 -
-subdomain
+subdomains
 .
 pinning
 .
@@ -689,6 +691,6 @@ kPreloadPKPinsExpirationTime
 =
 INT64_C
 (
-1409763023059000
+1409782406553000
 )
 ;
