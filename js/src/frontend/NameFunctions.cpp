d26c368a6a64765035757edbc847546c8149684b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:01:36 +0000

    Bug 895395 - Support NameFunctions when compiling scripts off the main thread Bug 895395 - Support NameFunctions when compiling scripts off the main thread (r=bhackett)
    UltraBlame original commit: b92529577644a37517e87fe053ba29ab9e3f4c90

--- js/src/frontend/NameFunctions.cpp
+++ js/src/frontend/NameFunctions.cpp
@@ -80,7 +80,7 @@ MaxParents
 =
 100
 ;
-JSContext
+ExclusiveContext
 *
 cx
 ;
@@ -1089,7 +1089,7 @@ public
 explicit
 NameResolver
 (
-JSContext
+ExclusiveContext
 *
 cx
 )
@@ -1387,7 +1387,7 @@ frontend
 :
 NameFunctions
 (
-JSContext
+ExclusiveContext
 *
 cx
 ParseNode
