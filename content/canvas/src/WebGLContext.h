914d3a4789b4366397615642d8d41f815debe97c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 14:47:30 +0000

    Bug 912280 - Implement OES_texture_half_float_linear for WebGL. r=kamidphish
    UltraBlame original commit: 8d885b139d7def0c8db7ed8bc93744d4f29546c7

--- content/canvas/src/WebGLContext.h
+++ content/canvas/src/WebGLContext.h
@@ -5105,6 +5105,7 @@ OES_standard_derivatives
 OES_texture_float
 OES_texture_float_linear
 OES_texture_half_float
+OES_texture_half_float_linear
 OES_vertex_array_object
 WEBGL_compressed_texture_atc
 WEBGL_compressed_texture_pvrtc
