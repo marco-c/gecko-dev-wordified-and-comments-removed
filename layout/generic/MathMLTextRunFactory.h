3ec60bcb1ef246fa8e1ce362d9afa732e946805c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 21:01:21 +0000

    Bug 1027354 - Fix fontweight and fontstyle on single char <mi>. r=roc
    UltraBlame original commit: 72f554d3e1c89ce66d0f7b8025bb93030f306f57

--- layout/generic/MathMLTextRunFactory.h
+++ layout/generic/MathMLTextRunFactory.h
@@ -24,6 +24,8 @@ MathMLTextRunFactory
 nsTransformingTextRunFactory
 *
 aInnerTransformingTextRunFactory
+uint32_t
+aFlags
 uint8_t
 aSSTYScriptLevel
 )
@@ -31,6 +33,10 @@ aSSTYScriptLevel
 mInnerTransformingTextRunFactory
 (
 aInnerTransformingTextRunFactory
+)
+mFlags
+(
+aFlags
 )
 mSSTYScriptLevel
 (
@@ -51,6 +57,16 @@ aRefContext
 )
 MOZ_OVERRIDE
 ;
+enum
+{
+MATH_FONT_STYLING_NORMAL
+=
+0x1
+MATH_FONT_WEIGHT_BOLD
+=
+0x2
+}
+;
 protected
 :
 nsAutoPtr
@@ -58,6 +74,9 @@ nsAutoPtr
 nsTransformingTextRunFactory
 >
 mInnerTransformingTextRunFactory
+;
+uint32_t
+mFlags
 ;
 uint8_t
 mSSTYScriptLevel
