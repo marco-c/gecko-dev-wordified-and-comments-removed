e928aab4a6ef6bf0d73f086ae6c6d246effe26be
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 22:35:27 +0000

    Bug 941296 - h.264 platform decoder for OS X. r=cpearce
    Implement an h.264 decoder using Apple's VideoToolbox framework.
    Based on gstreamer's vtdec because there's very little documentation
    for this API beyond the framework's header file.
    
    Reuse the MOZ_APPLEMEDIA define from the mp3 decoder.
    
    AAC decoding is stubbed to use the BlankDecoderModule's 440A tone.
    
    Provide a stub header for the VideoToolbox framework and load
    it dynamically at run time so we can build and run on versions
    prior to MacOS X 10.8 where it's not available.
    
    Dynamically load the CoreMedia framework as well, but build
    against its headers since it's available on MacOS X 10.7 or
    later, which is our minimum build version.
    
    UltraBlame original commit: 110fc4f8fb0916d98adcd1a5cab80b490d956e45

---
+++ content/media/fmp4/apple/AppleCMFunctions.h
@@ -0,0 +1,16 @@
+LINK_FUNC
+(
+CMVideoFormatDescriptionCreate
+)
+LINK_FUNC
+(
+CMBlockBufferCreateWithMemoryBlock
+)
+LINK_FUNC
+(
+CMSampleBufferCreate
+)
+LINK_FUNC
+(
+CMTimeMake
+)
---
+++ content/media/fmp4/apple/AppleVTFunctions.h
@@ -0,0 +1,16 @@
+LINK_FUNC
+(
+VTDecompressionSessionCreate
+)
+LINK_FUNC
+(
+VTDecompressionSessionDecodeFrame
+)
+LINK_FUNC
+(
+VTDecompressionSessionInvalidate
+)
+LINK_FUNC
+(
+VTDecompressionSessionWaitForAsynchronousFrames
+)
