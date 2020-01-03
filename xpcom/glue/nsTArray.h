d163b79e4b387407e6b4baba34b337500442e333
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 22:26:25 +0000

    Bug 1159433 - Part 3: Make nsTArray_CopyChooser only accept memmovable argument types by default; r=froydnj
    UltraBlame original commit: 1f7f2908083106762d07edf06f077b5bd6e874e0

--- xpcom/glue/nsTArray.h
+++ xpcom/glue/nsTArray.h
@@ -34,6 +34,15 @@ include
 "
 mozilla
 /
+Attributes
+.
+h
+"
+#
+include
+"
+mozilla
+/
 BinarySearch
 .
 h
@@ -2466,6 +2475,7 @@ class
 E
 >
 struct
+MOZ_NEEDS_MEMMOVABLE_TYPE
 nsTArray_CopyChooser
 {
 typedef
