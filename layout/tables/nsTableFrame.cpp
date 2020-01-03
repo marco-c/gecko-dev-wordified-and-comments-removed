94a23dd5d9dc4c077fce556f774ac0763442805f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 19:51:22 +0000

    Bug 1169432 part 4: Convert NS_ASSERTION(false, ...) to NS_ERROR(...) in nsTableFrame. r=mats
    UltraBlame original commit: a14d4adad0e64c7ede0992fb84bafcfdc0dbb524

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -8229,9 +8229,8 @@ GetPrevInFlow
 mTableLayoutStrategy
 )
 {
-NS_ASSERTION
+NS_ERROR
 (
-false
 "
 strategy
 should
@@ -15067,9 +15066,8 @@ if
 cellMap
 )
 {
-NS_ASSERTION
+NS_ERROR
 (
-false
 "
 never
 ever
@@ -20811,9 +20809,8 @@ found
 }
 else
 {
-NS_ASSERTION
+NS_ERROR
 (
-false
 "
 error
 in
@@ -33381,9 +33378,8 @@ break
 case
 eAjaRowOwner
 :
-NS_ASSERTION
+NS_ERROR
 (
-false
 "
 program
 error
