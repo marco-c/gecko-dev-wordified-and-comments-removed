dd28f8de8a4fb0c15366dacb6a52f30c5f93b57f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 04:30:26 +0000

    Bug 1091575: Implement Socket module for Bluetooth daemon (under bluetooth2/), r=btian
    This patch adds support for the Bluetooth daemon's Socket module,
    which provides OPP functionality.
    
    UltraBlame original commit: 9b8fec58f964597e19f1b884a7b516a3d113c465

--- dom/bluetooth2/bluedroid/BluetoothDaemonInterface.h
+++ dom/bluetooth2/bluedroid/BluetoothDaemonInterface.h
@@ -19,6 +19,9 @@ class
 BluetoothDaemonProtocol
 ;
 class
+BluetoothDaemonSocketInterface
+;
+class
 BluetoothDaemonInterface
 MOZ_FINAL
 :
@@ -447,6 +450,12 @@ BluetoothResultHandler
 >
 >
 mResultHandlerQ
+;
+nsAutoPtr
+<
+BluetoothDaemonSocketInterface
+>
+mSocketInterface
 ;
 }
 ;
