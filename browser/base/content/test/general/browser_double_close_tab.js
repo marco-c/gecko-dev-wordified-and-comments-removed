eed2cd5f03330375f2417be5694cf30b30f0e69e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 02:30:25 +0000

    Bug 1050638 - fix nits which got missed out in initial commit
    UltraBlame original commit: 1a6016480e7bc562da98883b476b4fae15bd264e

--- browser/base/content/test/general/browser_double_close_tab.js
+++ browser/base/content/test/general/browser_double_close_tab.js
@@ -2,6 +2,7 @@
 use
 strict
 "
+;
 const
 TEST_PAGE
 =
@@ -240,10 +241,9 @@ resolveOuter
 {
 waitForDialog
 (
-function
-(
 dialogNode
-)
+=
+>
 {
 waitForDialogDestroyed
 (
@@ -263,7 +263,7 @@ doCompletion
 setTimeout
 (
 resolveOuter
-10
+0
 )
 ;
 info
