3a19a9ec3970a78b3de8396493dfc6f840a24d24
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 02:07:38 +0000

    Backed out changeset 01a525d9c6c2 (bug 1073846) for suspicion of causing dt1 test failures
    UltraBlame original commit: e54edf3b633c08bbb908e5c4823f4f5fe50e22d0

--- toolkit/components/places/UnifiedComplete.js
+++ toolkit/components/places/UnifiedComplete.js
@@ -3541,10 +3541,8 @@ hasFirstResult
 yield
 this
 .
-_matchKnownUrl
+_matchSearchEngineUrl
 (
-conn
-queries
 )
 ;
 }
@@ -3567,8 +3565,10 @@ hasFirstResult
 yield
 this
 .
-_matchSearchEngineUrl
+_matchKnownUrl
 (
+conn
+queries
 )
 ;
 }
