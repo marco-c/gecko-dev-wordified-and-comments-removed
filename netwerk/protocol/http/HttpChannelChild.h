fb0ce5cc9cf703726067bfb8f861f9c8bb7fa4d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 02:15:10 +0000

    Bug 1064706 - Send timing info to content process r=honzab
    UltraBlame original commit: afecbcdbe6d52dd407300716ca33e7c1a83ac805

--- netwerk/protocol/http/HttpChannelChild.h
+++ netwerk/protocol/http/HttpChannelChild.h
@@ -454,6 +454,10 @@ const
 nsresult
 &
 statusCode
+const
+ResourceTimingStruct
+&
+timing
 )
 ;
 bool
@@ -734,6 +738,10 @@ const
 nsresult
 &
 channelStatus
+const
+ResourceTimingStruct
+&
+timing
 )
 ;
 void
