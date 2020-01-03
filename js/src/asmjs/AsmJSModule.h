80c0d01d61452bd4d02ec8c13850cc3212bfb6e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 00:17:54 +0000

    Bug 1059733: OdinMonkey SIMD: add support for bitwise operations; r=luke
    UltraBlame original commit: 1eb94f3ebf22362eb11b1ac0e5a5043faf46b49c

--- js/src/asmjs/AsmJSModule.h
+++ js/src/asmjs/AsmJSModule.h
@@ -207,6 +207,9 @@ AsmJSSimdOperation_equal
 AsmJSSimdOperation_notEqual
 AsmJSSimdOperation_greaterThan
 AsmJSSimdOperation_greaterThanOrEqual
+AsmJSSimdOperation_and
+AsmJSSimdOperation_or
+AsmJSSimdOperation_xor
 }
 ;
 struct
