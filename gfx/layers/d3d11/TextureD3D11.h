6dd52023cddbfaec08a24c3b7dc6fb15a1bc693e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 01:00:22 +0000

    Bug 1046550 - Part 3: Use Direct2D 1.1 when preffed on. r=bas
    UltraBlame original commit: 7b9201731195c5391405de8d9f52b370a34a4ba0

--- gfx/layers/d3d11/TextureD3D11.h
+++ gfx/layers/d3d11/TextureD3D11.h
@@ -111,9 +111,10 @@ const
 MOZ_OVERRIDE
 {
 return
-!
-!
 mTexture
+|
+|
+mTexture10
 ;
 }
 virtual
@@ -280,6 +281,12 @@ mSize
 RefPtr
 <
 ID3D10Texture2D
+>
+mTexture10
+;
+RefPtr
+<
+ID3D11Texture2D
 >
 mTexture
 ;
