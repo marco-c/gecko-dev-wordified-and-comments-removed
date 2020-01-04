bb95e5bbf52228d708343eb240fa3b0dcf58ec87
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:20:06 +0000

    Bug 1224361 - Added vp8/ivf test case - r=rillian
    UltraBlame original commit: ea96a37f1a43e9926be3a930a2f3c7b25b5286ff

--- dom/media/gtest/TestVPXDecoding.cpp
+++ dom/media/gtest/TestVPXDecoding.cpp
@@ -453,6 +453,16 @@ testFiles
 {
 {
 "
+test_case_1224361
+.
+vp8
+.
+ivf
+"
+VPX_CODEC_OK
+}
+{
+"
 test_case_1224363
 .
 vp8
@@ -476,7 +486,7 @@ VPX_CODEC_CORRUPT_FRAME
 TEST
 (
 libvpx
-test_case_1224363
+test_cases
 )
 {
 for
