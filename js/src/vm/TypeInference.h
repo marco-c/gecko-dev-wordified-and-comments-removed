df41bfb6ca731ab4825424756410357cf4684764
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:17:58 +0000

    Bug 1198861 - Improve type barrier logic to avoid unnecessary tests on primitive types, and fix redundant baseline stubs for SETELEM property adds, r=jandem.
    UltraBlame original commit: faaafe8c3d1e6be8611b7f56297914a92ca1478d

--- js/src/vm/TypeInference.h
+++ js/src/vm/TypeInference.h
@@ -186,6 +186,8 @@ TYPE_FLAG_DOUBLE
 TYPE_FLAG_STRING
 |
 TYPE_FLAG_SYMBOL
+|
+TYPE_FLAG_LAZYARGS
 TYPE_FLAG_OBJECT_COUNT_MASK
 =
 0x3e00
@@ -1629,6 +1631,14 @@ const
 ;
 bool
 objectsAreSubset
+(
+TypeSet
+*
+other
+)
+;
+bool
+primitivesAreSubset
 (
 TypeSet
 *
