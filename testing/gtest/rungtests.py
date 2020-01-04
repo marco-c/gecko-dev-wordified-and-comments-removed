e8fbe2ae290ee25dc47d056ac5b3d67c98f0b49c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:46:09 +0000

    Bug 1136841 - Fix rungtests.py to check for crashes in the correct directory. r=ahal
    UltraBlame original commit: c2b1901fba11e5dc36ea070f9179d7676a85221d

--- testing/gtest/rungtests.py
+++ testing/gtest/rungtests.py
@@ -51,10 +51,8 @@ run_gtest
 self
 prog
 xre_path
-symbols_path
-=
-None
 cwd
+symbols_path
 =
 None
 )
@@ -269,11 +267,7 @@ mozcrash
 .
 check_for_crashes
 (
-os
-.
-getcwd
-(
-)
+cwd
 symbols_path
 test_name
 =
@@ -1095,17 +1089,15 @@ options
 .
 xre_path
                                   
-symbols_path
-=
 options
 .
-symbols_path
+cwd
                                   
-cwd
+symbols_path
 =
 options
 .
-cwd
+symbols_path
 )
     
 except
