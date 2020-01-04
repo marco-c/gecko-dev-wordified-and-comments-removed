72372c9d56a43d34437cfd5bb275c997bf14a8b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:07:51 +0000

    Bug 1229241 - Reject FINAL_TARGET{,_PP}_FILES along DIST_INSTALL = False. r=gps
    UltraBlame original commit: 7b183b8a6116a5f5d65eeccbab97ced59910ef79

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -4264,6 +4264,33 @@ all_files
                 
 continue
             
+if
+dist_install
+is
+False
+:
+                
+raise
+SandboxValidationError
+(
+                    
+'
+%
+s
+cannot
+be
+used
+with
+DIST_INSTALL
+=
+False
+'
+%
+var
+                    
+context
+)
+            
 for
 _
 files
