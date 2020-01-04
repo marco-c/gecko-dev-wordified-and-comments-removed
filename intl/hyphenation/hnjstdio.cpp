e2d335c9f2cbaa37580bec1a56c8e82e33ac3b44
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:41:14 +0000

    Bug 1197925 - Use channel->Open2() in intl/hyphenation/hnjstdio.cpp (r=sicking)
    UltraBlame original commit: 582749996e7ccaa6469ce088492fa86cc9fb69d5

--- intl/hyphenation/hnjstdio.cpp
+++ intl/hyphenation/hnjstdio.cpp
@@ -162,7 +162,7 @@ GetSystemPrincipal
 nsILoadInfo
 :
 :
-SEC_NORMAL
+SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL
 nsIContentPolicy
 :
 :
@@ -192,7 +192,7 @@ rv
 channel
 -
 >
-Open
+Open2
 (
 getter_AddRefs
 (
