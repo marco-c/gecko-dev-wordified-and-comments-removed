bab6abf0560f125c01beeb9c3186ee70f8c7687d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:12:23 +0000

    Backed out changeset 214b6958340a (bug 1270317)
    UltraBlame original commit: 2f71ed818fd8f01acd6e285a9ea35a97cbaf4d4a

--- testing/mozharness/configs/builds/branch_specifics.py
+++ testing/mozharness/configs/builds/branch_specifics.py
@@ -1145,26 +1145,6 @@ clone_with_purge
 True
         
 '
-clone_upstream_url
-'
-:
-'
-https
-:
-/
-/
-hg
-.
-mozilla
-.
-org
-/
-mozilla
--
-central
-'
-        
-'
 tinderbox_build_dir
 '
 :
--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -8537,22 +8537,6 @@ clone_with_purge
 =
 True
         
-vcs_checkout_kwargs
-[
-'
-clone_upstream_url
-'
-]
-=
-c
-.
-get
-(
-'
-clone_upstream_url
-'
-)
-        
 rev
 =
 self
