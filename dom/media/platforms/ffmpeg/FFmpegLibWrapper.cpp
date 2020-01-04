0bf6fda3a79651b838aabca490e96c9dd81ce65f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:22:22 +0000

    Bug 1275339: [ffmpeg] P2. Fetch avcodec_free_frame in libavcodec. r=cpearce
    avcodec_free_frame was incorrectly looked in libavuti. On Linux where this code is used, it makes no difference really but for clarity sake.
    
    MozReview-Commit-ID: DC1WJAAKiE4
    
    UltraBlame original commit: 9ddfddc33c569553a8cbf96cd99277d176f253dd

--- dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
+++ dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
@@ -498,6 +498,11 @@ AV_FUNC_54
 )
 AV_FUNC
 (
+avcodec_free_frame
+AV_FUNC_54
+)
+AV_FUNC
+(
 av_log_set_level
 AV_FUNC_AVUTIL_ALL
 )
@@ -510,11 +515,6 @@ AV_FUNC
 (
 av_freep
 AV_FUNC_AVUTIL_ALL
-)
-AV_FUNC
-(
-avcodec_free_frame
-AV_FUNC_AVUTIL_54
 )
 AV_FUNC
 (
