6f9eccc0b5aece63d1f435173faf113f08c37964
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:18:02 +0000

    Bug 1213719 - Back out bug 1170314 for duplicate functionality. r=smaug
    UltraBlame original commit: fab77933a2c79c0251986081d756246e7bb34d35

--- dom/base/Console.h
+++ dom/base/Console.h
@@ -88,9 +88,6 @@ nsIConsoleAPIStorage
 class
 nsIPrincipal
 ;
-class
-nsIProfiler
-;
 namespace
 mozilla
 {
@@ -827,17 +824,6 @@ JSObjectHolder
 >
 mSandbox
 ;
-#
-ifdef
-MOZ_ENABLE_PROFILER_SPS
-nsCOMPtr
-<
-nsIProfiler
->
-mProfiler
-;
-#
-endif
 nsDataHashtable
 <
 nsStringHashKey
