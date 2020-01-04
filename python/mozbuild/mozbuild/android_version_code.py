6eef23a3a3674a5a94bceb02c82bae0e1b80e753
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 07:21:17 +0000

    Backed out changeset 991a5607ee54 (bug 1155801)
    UltraBlame original commit: 2819a299e08491bc9c76409afd0b927bf5f06fd0

--- python/mozbuild/mozbuild/android_version_code.py
+++ python/mozbuild/mozbuild/android_version_code.py
@@ -363,6 +363,8 @@ is
 targeting
 Android
 API
+11
+/
 14
 +
 and
@@ -416,6 +418,20 @@ due
 to
 significant
 usage
+.
+    
+We
+temporarily
+treat
+both
+11
+and
+14
+the
+same
+:
+Bug
+1219512
 .
     
 We
@@ -848,6 +864,11 @@ min_sdk
 =
 =
 14
+or
+min_sdk
+=
+=
+11
 :
             
 version
--- python/mozbuild/mozbuild/test/test_android_version_code.py
+++ python/mozbuild/mozbuild/test/test_android_version_code.py
@@ -134,6 +134,10 @@ arm_api9
 =
 0b01111000001000000001001001110000
         
+arm_api11
+=
+0b01111000001000000001001001110001
+        
 arm_api14
 =
 0b01111000001000000001001001110001
@@ -162,6 +166,30 @@ max_sdk
 None
 )
 arm_api9
+)
+        
+self
+.
+assertEqual
+(
+android_version_code_v1
+(
+buildid
+cpu_arch
+=
+'
+armeabi
+-
+v7a
+'
+min_sdk
+=
+11
+max_sdk
+=
+None
+)
+arm_api11
 )
         
 self
