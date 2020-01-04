19e4fa016624314fc2e4d41c52bdcd7a60e1f194
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 11:33:46 +0000

    Backed out changeset 07d1058b745e (bug 891107)
    UltraBlame original commit: ea30fdf3bca31dbf290476e3311cadd2fe43f739

--- toolkit/components/ctypes/tests/unit/test_jsctypes.js
+++ toolkit/components/ctypes/tests/unit/test_jsctypes.js
@@ -15811,7 +15811,7 @@ p
 contents
 ;
 }
-TypeError
+Error
 )
 ;
 do_check_throws
@@ -15827,7 +15827,7 @@ contents
 g
 ;
 }
-TypeError
+Error
 )
 ;
 p
@@ -15963,7 +15963,7 @@ f
 contents
 ;
 }
-TypeError
+Error
 )
 ;
 do_check_throws
@@ -15979,7 +15979,7 @@ contents
 0
 ;
 }
-TypeError
+Error
 )
 ;
 f
@@ -16002,7 +16002,7 @@ contents
 0
 ;
 }
-TypeError
+Error
 )
 ;
 do_check_eq
@@ -16374,7 +16374,7 @@ z_t
 .
 contents
 }
-TypeError
+Error
 )
 ;
 z_t
