45d4b4ccee26e99ff990614d9fa09dac564e8b1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:41:48 +0000

    Backed out changeset 8534fa84c490 (bug 1168992) for Spidermonkey bustage on CLOSED TREE
    UltraBlame original commit: 26951c3aa2671ccdc8e78d21c00c2a0501fd2bb5

--- js/src/frontend/ParseNode.cpp
+++ js/src/frontend/ParseNode.cpp
@@ -734,6 +734,9 @@ case
 PNK_DELETEELEM
 :
 case
+PNK_DELETESUPERELEM
+:
+case
 PNK_DELETEEXPR
 :
 case
@@ -771,6 +774,9 @@ PNK_EXPORT
 :
 case
 PNK_EXPORT_DEFAULT
+:
+case
+PNK_SUPERELEM
 :
 return
 PushUnaryNodeChild
