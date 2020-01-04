410c0b9669e03c4d86032ea2ce676930d22e94c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 10:42:53 +0000

    Bug 1247804 - Enable seamless cubemaps where available. - r=jrmuizel
    UltraBlame original commit: ab587ee9793f96c2f92ae26d147438e8455dc79b

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -333,6 +333,7 @@ robustness
 sRGB_framebuffer
 sRGB_texture
 sampler_objects
+seamless_cube_map_opt_in
 split_framebuffer
 standard_derivatives
 sync
@@ -1003,6 +1004,7 @@ ARB_occlusion_query2
 ARB_pixel_buffer_object
 ARB_robustness
 ARB_sampler_objects
+ARB_seamless_cube_map
 ARB_sync
 ARB_texture_compression
 ARB_texture_float
