76d89c176290ec186192641223b754f0715e860b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 19:46:00 +0000

    Bug 1279992 - Inline constructor of typed arrays with non-compile-time known size r=jandem,Waldo
    UltraBlame original commit: f1c5f34ad31b8d9b89428aff7ffc4796c4db7f7d

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -232,6 +232,15 @@ include
 "
 vm
 /
+TypedArrayObject
+.
+h
+"
+#
+include
+"
+vm
+/
 UnboxedObject
 .
 h
@@ -8199,6 +8208,8 @@ fail
 TypedArrayObject
 *
 templateObj
+TypedArrayLength
+lengthKind
 )
 ;
 void
