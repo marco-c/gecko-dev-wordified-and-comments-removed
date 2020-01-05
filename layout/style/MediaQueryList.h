6c3fc1da08c3a0de80f20e1a7ca3bfbeb739fc71
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:19:10 +0000

    Bug 1356074: Use Servo for window.matchMedia and MediaQueryList. r=xidorn
    MozReview-Commit-ID: I6uufe48JZq
    
    UltraBlame original commit: b137d947dbba7df2d5fc98b64c5e668dfee04de9

--- layout/style/MediaQueryList.h
+++ layout/style/MediaQueryList.h
@@ -78,15 +78,15 @@ h
 class
 nsIDocument
 ;
-class
-nsMediaList
-;
 namespace
 mozilla
 {
 namespace
 dom
 {
+class
+MediaList
+;
 class
 MediaQueryList
 final
@@ -295,7 +295,7 @@ mDocument
 ;
 RefPtr
 <
-nsMediaList
+MediaList
 >
 mMediaList
 ;
