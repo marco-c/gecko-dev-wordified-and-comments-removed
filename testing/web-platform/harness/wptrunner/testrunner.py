f67b1f17072ac88032e13c38e23f26b2625c5d84
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:05:21 +0000

    Bug 1352351 - Fix infinite loop getting next test with --run-by-dir, r=ato
    If getting a test from the queue failed, we would loop forever rather
    than trying the next queue.
    
    MozReview-Commit-ID: K5YxCZPtx4l
    
    UltraBlame original commit: 024d5d30566d6cc1e7f7cc10d245cb6ffdf52ee4

--- testing/web-platform/harness/wptrunner/testrunner.py
+++ testing/web-platform/harness/wptrunner/testrunner.py
@@ -3140,14 +3140,24 @@ block
 True
 timeout
 =
-1
+2
 )
             
 except
 Empty
 :
                 
-pass
+if
+test_queue
+.
+empty
+(
+)
+:
+                    
+test_queue
+=
+None
         
 return
 test
