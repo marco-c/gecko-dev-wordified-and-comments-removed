17168f94484c62c408f02037aaf82d0bb9c0d843
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:59:53 +0000

    Bug 1344442 - Part 3: Use smart pointers. r=keeler
    MozReview-Commit-ID: 58BwdPYdjM5
    
    UltraBlame original commit: 55b551e3415b71f1139096ec0efdb7645cd65b3c

--- security/manager/ssl/ScopedNSSTypes.h
+++ security/manager/ssl/ScopedNSSTypes.h
@@ -907,6 +907,12 @@ CERT_DestroyValidity
 )
 MOZ_TYPE_SPECIFIC_UNIQUE_PTR_TEMPLATE
 (
+UniqueHASHContext
+HASHContext
+HASH_Destroy
+)
+MOZ_TYPE_SPECIFIC_UNIQUE_PTR_TEMPLATE
+(
 UniqueNSSCMSMessage
 NSSCMSMessage
 NSS_CMSMessage_Destroy
