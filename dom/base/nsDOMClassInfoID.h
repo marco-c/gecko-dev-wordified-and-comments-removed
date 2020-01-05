ab85f5e9d7fd8bbd388175a6ccbb9553e8775ed5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 07:11:29 +0000

    Backed out changeset 1bbbc436690d (bug 851892)
    UltraBlame original commit: 633b0b55c6aff10d943e7f637e0f5283481f1528

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -16,6 +16,7 @@ nsDOMClassInfoID
 {
 eDOMClassInfo_DOMPrototype_id
 eDOMClassInfo_DOMConstructor_id
+eDOMClassInfo_CSSMediaRule_id
 #
 ifdef
 MOZ_XUL
@@ -37,6 +38,8 @@ eDOMClassInfo_XULTemplateBuilder_id
 eDOMClassInfo_XULTreeBuilder_id
 #
 endif
+eDOMClassInfo_CSSMozDocumentRule_id
+eDOMClassInfo_CSSSupportsRule_id
 eDOMClassInfo_CSSFontFaceRule_id
 eDOMClassInfo_ContentFrameMessageManager_id
 eDOMClassInfo_ContentProcessMessageManager_id
