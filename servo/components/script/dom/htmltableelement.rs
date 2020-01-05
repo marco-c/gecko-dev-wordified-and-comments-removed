ecb0fdfbc30bfaa3de9722d2740199ddfc616121
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 10:43:57 +0000

    servo: Merge #9054 - Implement non-zero dimension attribute parsing (from frewsxcv:parse-non-zero-dimension); r=eefriedman
    Fixes #8445
    
    The only attributes I found that we have implemented that uses non-zero
    dimenion attributes:
    
    * `width` for `<td>` and `<th>` (table cells)
    * `width` for `<table>`
    
    I updated these implementations to use the new non-zero dimension
    attribute parsing and added associated regression tests.
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 0d52170f351e46718c5ea1a811a7d4461eaa3751
    
    UltraBlame original commit: 8344ccc7066fd78949d1abaa234011ad83ce4888

--- servo/components/script/dom/htmltableelement.rs
+++ servo/components/script/dom/htmltableelement.rs
@@ -1034,7 +1034,7 @@ dom
 table
 -
 width
-make_dimension_setter
+make_nonzero_dimension_setter
 !
 (
 SetWidth
@@ -1584,7 +1584,7 @@ width
 AttrValue
 :
 :
-from_dimension
+from_nonzero_dimension
 (
 value
 )
