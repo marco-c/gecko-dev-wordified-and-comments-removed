28006a7a4cc013f8e291fd49b301ca12303f876b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:13:26 +0000

    servo: Merge #16384 - stylo: Implement shape-outside property (from canaltinova:shape-outside); r=Manishearth
    shape-outside property implemented.
    r=Manishearth in bugzilla
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [X] `./mach build -d` does not report any errors
    - [X] `./mach test-tidy` does not report any errors
    - [X] These changes fix #15958 and [Bug 1355003](https://bugzilla.mozilla.org/show_bug.cgi?id=1355003)
    
    <!-- Also, please make sure that "Allow edits from maintainers" checkbox is checked, so that we can help you if you get stuck somewhere along the way.-->
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: 9d5dde26040108c4928bd20b34b70c1be36b0cdb
    
    UltraBlame original commit: b1e0a9bc36556578ac2f9608e0eadf82c73dcb78

--- servo/components/style/gecko_bindings/bindings.rs
+++ servo/components/style/gecko_bindings/bindings.rs
@@ -6299,7 +6299,7 @@ C
 {
 pub
 fn
-Gecko_CopyClipPathValueFrom
+Gecko_CopyShapeSourceFrom
 (
 dst
 :
@@ -6321,9 +6321,9 @@ C
 {
 pub
 fn
-Gecko_DestroyClipPath
+Gecko_DestroyShapeSource
 (
-clip
+shape
 :
 *
 mut
@@ -6358,9 +6358,9 @@ C
 {
 pub
 fn
-Gecko_StyleClipPath_SetURLValue
+Gecko_StyleShapeSource_SetURLValue
 (
-clip
+shape
 :
 *
 mut
