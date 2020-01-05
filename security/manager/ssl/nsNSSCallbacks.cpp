2d60d979ce79c91751d4a6dbe16e0e4b1dfd794f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:18:20 +0000

    Bug 1304919 - PSM changes to support TLS 1.3 key exchange, r=mt
    MozReview-Commit-ID: 1GMk8PajbHj
    
    UltraBlame original commit: 96991c815ab8de388e15cd2b6096064c552ca6e6

--- security/manager/ssl/nsNSSCallbacks.cpp
+++ security/manager/ssl/nsNSSCallbacks.cpp
@@ -3490,7 +3490,7 @@ infoObject
 >
 SetKEAUsed
 (
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -3833,7 +3833,7 @@ static_cast
 int32_t
 >
 (
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -3922,7 +3922,7 @@ POSSIBLE_VERSION_DOWNGRADE
 }
 if
 (
-cipherInfo
+channelInfo
 .
 keaType
 !
@@ -3961,7 +3961,7 @@ static_cast
 int32_t
 >
 (
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -4776,7 +4776,7 @@ value
 break
 ;
 case
-TLS_ECDHE_PSK_WITH_AES_128_GCM_SHA256
+TLS_AES_128_GCM_SHA256
 :
 value
 =
@@ -4785,7 +4785,7 @@ value
 break
 ;
 case
-TLS_ECDHE_PSK_WITH_CHACHA20_POLY1305_SHA256
+TLS_CHACHA20_POLY1305_SHA256
 :
 value
 =
@@ -4794,7 +4794,7 @@ value
 break
 ;
 case
-TLS_ECDHE_PSK_WITH_AES_256_GCM_SHA384
+TLS_AES_256_GCM_SHA384
 :
 value
 =
@@ -5090,7 +5090,7 @@ SECSuccess
 {
 usesFallbackCipher
 =
-cipherInfo
+channelInfo
 .
 keaType
 =
@@ -5118,7 +5118,7 @@ Telemetry
 :
 :
 SSL_KEY_EXCHANGE_ALGORITHM_RESUMED
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -5133,7 +5133,7 @@ GetKEAUsed
 )
 =
 =
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -5150,7 +5150,7 @@ IsFullHandshake
 {
 switch
 (
-cipherInfo
+channelInfo
 .
 keaType
 )
@@ -5225,14 +5225,14 @@ Telemetry
 :
 :
 SSL_AUTH_ALGORITHM_FULL
-cipherInfo
+channelInfo
 .
-authAlgorithm
+authType
 )
 ;
 if
 (
-cipherInfo
+channelInfo
 .
 keaType
 !
@@ -5242,13 +5242,16 @@ ssl_kea_rsa
 {
 switch
 (
-cipherInfo
+channelInfo
 .
-authAlgorithm
+authType
 )
 {
 case
 ssl_auth_rsa
+:
+case
+ssl_auth_rsa_sign
 :
 AccumulateNonECCKeySize
 (
