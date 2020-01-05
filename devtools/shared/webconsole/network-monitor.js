5188d1dd6b3a9f6ee85c5ea122d45b9772579f72
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:59:09 +0000

    Bug 1219556 - report full body size from NetworkResponseListener; r=jryans
    MozReview-Commit-ID: LMMycsbcRg1
    
    UltraBlame original commit: 0bb1f11160a463924046b789c5e74779022865d9

--- devtools/shared/webconsole/network-monitor.js
+++ devtools/shared/webconsole/network-monitor.js
@@ -224,6 +224,12 @@ RESPONSE_BODY_LIMIT
 =
 1048576
 ;
+exports
+.
+RESPONSE_BODY_LIMIT
+=
+RESPONSE_BODY_LIMIT
+;
 function
 matchRequest
 (
@@ -2151,11 +2157,9 @@ response
 .
 size
 =
-response
+this
 .
-text
-.
-length
+bodySize
 ;
 response
 .
