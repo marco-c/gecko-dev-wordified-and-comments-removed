cc7bc73b9efeba238b7e3fb08de41fff96b2b8a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:34:51 +0000

    Backed out changeset ba97ba73f72d (bug 1353333) for failing flake8. r=backout
    UltraBlame original commit: 1cf1b7333d9858ce7f09d163aec78c2825a210b3

--- taskcluster/taskgraph/transforms/push_apk.py
+++ taskcluster/taskgraph/transforms/push_apk.py
@@ -52,7 +52,6 @@ get_push_apk_track
 \
     
 get_push_apk_dry_run_option
-get_push_apk_rollout_percentage
 from
 taskgraph
 .
@@ -324,36 +323,6 @@ get_push_apk_dry_run_option
 (
 config
 )
-        
-rollout_percentage
-=
-get_push_apk_rollout_percentage
-(
-config
-)
-        
-if
-rollout_percentage
-is
-not
-None
-:
-            
-job
-[
-'
-worker
-'
-]
-[
-'
-rollout
--
-percentage
-'
-]
-=
-rollout_percentage
         
 job
 [
