41369108e9221753db8c196a0e281124c4e09be6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 01:00:32 +0000

    Bug 1277803 - Part 1: Add a new ContentPolicy TYPE_INTERNAL_IMAGE_FAVICON for indicating a favicon loading. r=ckerschb
    UltraBlame original commit: d283c59402ce1c63bae3b89457abe9238fb75e1a

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -38744,6 +38744,12 @@ nsIContentPolicy
 :
 TYPE_INTERNAL_IMAGE_PRELOAD
 :
+case
+nsIContentPolicy
+:
+:
+TYPE_INTERNAL_IMAGE_FAVICON
+:
 return
 nsIContentPolicy
 :
--- dom/fetch/InternalRequest.cpp
+++ dom/fetch/InternalRequest.cpp
@@ -948,6 +948,12 @@ nsIContentPolicy
 :
 TYPE_INTERNAL_IMAGE_PRELOAD
 :
+case
+nsIContentPolicy
+:
+:
+TYPE_INTERNAL_IMAGE_FAVICON
+:
 context
 =
 RequestContext
