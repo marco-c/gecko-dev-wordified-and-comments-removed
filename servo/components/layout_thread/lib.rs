96fafaa78574a2b66452f0cfb1c899f0f1a9e84c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 11:46:02 +0000

    servo: Merge #13873 - Remove stacking context nesting from WebRender display construction (from mrobinson:remove-nesting); r=glennw
    <!-- Please describe your changes on the following line: -->
    
    ---
    <!-- Thank you for contributing to Servo! Please replace each `[ ]` by `[X]` when the step is complete, and replace `__` with appropriate data: -->
    - [x] `./mach build -d` does not report any errors
    - [x] `./mach test-tidy` does not report any errors
    - [ ] These changes fix #__ (github issue number if applicable).
    
    <!-- Either: -->
    - [ ] There are tests for these changes OR
    - [x] These changes do not require tests because they should not produce any behavior changes.
    
    <!-- Pull requests that do not address these steps are welcome, but they will require additional verification as part of the review process. -->
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: d5e412b3f19fa72687cf3ad246dfb8312b1f2368
    
    UltraBlame original commit: 6bc685a3f0340c866f34d943f5696e24c6612b6e

--- servo/components/layout_thread/lib.rs
+++ servo/components/layout_thread/lib.rs
@@ -5782,15 +5782,6 @@ pipeline_id
 )
 ;
 let
-root_scroll_layer_id
-=
-frame_builder
-.
-next_scroll_layer_id
-(
-)
-;
-let
 sc_id
 =
 rw_data
@@ -5814,10 +5805,6 @@ self
 webrender_api
 pipeline_id
 epoch
-Some
-(
-root_scroll_layer_id
-)
 &
 mut
 frame_builder
