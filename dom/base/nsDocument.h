a809bb76eacda2ad00512b708cc310faae03747e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:27:21 +0000

    Bug 1305580 part 1.  Remove all the methods from nsIDOMXMLDocument and make it not inherit from nsIDOMDocument anymore.  r=smaug
    This will let us know for sure that we're being called from script in things
    like nsXMLDocument::Load.
    
    UltraBlame original commit: 521286426a93e4194eb9eae13157f8d17a85f772

--- dom/base/nsDocument.h
+++ dom/base/nsDocument.h
@@ -56,7 +56,7 @@ h
 #
 include
 "
-nsIDOMXMLDocument
+nsIDOMDocument
 .
 h
 "
@@ -1731,7 +1731,7 @@ nsDocument
 public
 nsIDocument
 public
-nsIDOMXMLDocument
+nsIDOMDocument
 public
 nsIDOMDocumentXBL
 public
@@ -3168,7 +3168,6 @@ public
 :
 NS_FORWARD_NSIDOMNODE_TO_NSINODE_OVERRIDABLE
 NS_DECL_NSIDOMDOCUMENT
-NS_DECL_NSIDOMXMLDOCUMENT
 NS_DECL_NSIDOMDOCUMENTXBL
 virtual
 nsresult
@@ -5037,11 +5036,16 @@ js
 ExpandoAndGeneration
 mExpandoAndGeneration
 ;
+#
+ifdef
+MOZ_EME
 bool
 ContainsEMEContent
 (
 )
 ;
+#
+endif
 bool
 ContainsMSEContent
 (
