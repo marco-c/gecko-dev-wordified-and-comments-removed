c96577f264c9a6525d1ffb0b7b3d505c5e16a3e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 12:37:39 +0000

    Bug 1325915 - Part 1, derive transportStatus in HttpChannelParent ODA and derive progress/progressMax in child side. r=mayhemer
    MozReview-Commit-ID: 41qlyHKMSFm
    
    UltraBlame original commit: 3593cac8732bbe91591773077757789b79a4b94b

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -1357,6 +1357,10 @@ mWarningReporter
 (
 nullptr
 )
+mIsReadingFromCache
+(
+false
+)
 mDidReval
 (
 false
@@ -29973,6 +29977,15 @@ n
 )
 ;
 }
+mIsReadingFromCache
+=
+(
+request
+=
+=
+mCachePump
+)
+;
 if
 (
 mListener
