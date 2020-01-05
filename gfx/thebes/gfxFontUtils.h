d83f4ab100650ae9c66e9947e037158b47a87d37
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 05:04:09 +0000

    Bug 1320665 - Add support for 'cmap' subtable format 13. r=jrmuizel
    UltraBlame original commit: e92988d277ae11130bf5e6774bc0c1bbaf1521b4

--- gfx/thebes/gfxFontUtils.h
+++ gfx/thebes/gfxFontUtils.h
@@ -3574,7 +3574,7 @@ aCharacterMap
 ;
 static
 nsresult
-ReadCMAPTableFormat12
+ReadCMAPTableFormat12or13
 (
 const
 uint8_t
@@ -3696,7 +3696,7 @@ aCh
 ;
 static
 uint32_t
-MapCharToGlyphFormat12
+MapCharToGlyphFormat12or13
 (
 const
 uint8_t
