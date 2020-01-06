439d8f003e1223e8a15ff17f4e5f3aad08716cf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 06:27:56 +0000

    Backed out changeset 6f89b4ceda12 (bug 1380133)
    UltraBlame original commit: f2298180d46791f41f6073f3a51480f172e903b3

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -2052,14 +2052,9 @@ Gecko_CalcStyleDifference
 (
 nsStyleContext
 *
-old_style_from_frame
-const
-mozilla
-:
-:
-ServoStyleContext
-*
-new_style
+oldstyle
+ServoComputedValuesBorrowed
+newstyle
 bool
 *
 any_style_changed
