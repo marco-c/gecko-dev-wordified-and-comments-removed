1d39693be43bd59bcbc61838bd67a4148b770785
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:40:21 +0000

    Bug 1403366 - Don't set MOZILLA_FIVE_HOME from multiple scripts. r=froydnj
    It was seldom used before previous commit and now does nothing.
    
    UltraBlame original commit: 68765cc0c5303f20a127eb79ddad486f45b0c0da

--- testing/mozbase/mozrunner/mozrunner/utils.py
+++ testing/mozbase/mozrunner/mozrunner/utils.py
@@ -647,15 +647,6 @@ envVar
 LD_LIBRARY_PATH
 "
         
-env
-[
-'
-MOZILLA_FIVE_HOME
-'
-]
-=
-xrePath
-        
 dmdLibrary
 =
 "
--- testing/remotecppunittests.py
+++ testing/remotecppunittests.py
@@ -840,17 +840,6 @@ remote_home_dir
 env
 [
 "
-MOZILLA_FIVE_HOME
-"
-]
-=
-self
-.
-remote_home_dir
-        
-env
-[
-"
 MOZ_XRE_DIR
 "
 ]
