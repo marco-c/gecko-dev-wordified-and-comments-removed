3d4016794803b98c5f0bfc0597c2d9176cb9704a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:06:52 +0000

    Merge m-c to autoland, a=merge
    MozReview-Commit-ID: KLrOCT1a7El
    
    UltraBlame original commit: cf69bd1dca31bc80e55ba597bdbfb5f062c6a5a1

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -27048,7 +27048,10 @@ Ci
 nsIScriptSecurityManager
 .
 DEFAULT_USER_CONTEXT_ID
-aOpener
+aOpenerWindow
+=
+null
+aOpenerBrowser
 =
 null
 aTriggeringPrincipal
@@ -27212,7 +27215,10 @@ forceNotRemote
 aForceNotRemote
 opener
 :
-aOpener
+aOpenerWindow
+openerBrowser
+:
+aOpenerBrowser
 nextTabParentId
 :
 aNextTabParentId
@@ -27770,6 +27776,7 @@ isExternal
 forceNotRemote
 userContextId
 openerWindow
+null
 aTriggeringPrincipal
 )
 ;
@@ -27985,6 +27992,9 @@ isExternal
 false
 userContextId
 null
+aParams
+.
+openerBrowser
 aParams
 .
 triggeringPrincipal
