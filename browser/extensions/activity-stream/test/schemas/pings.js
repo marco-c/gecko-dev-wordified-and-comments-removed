bfde1e78f353d508dc659927f53814a58bcac5c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:25:49 +0000

    Bug 1379522 - Add pinned sites, context menu ordering, cleared history, other fixes to Activity Stream. r=ursula
    MozReview-Commit-ID: ESlcuSuzoDH
    
    UltraBlame original commit: cf54cb59762c049b6aed33b4cf75e89d19ce6970

--- browser/extensions/activity-stream/test/schemas/pings.js
+++ browser/extensions/activity-stream/test/schemas/pings.js
@@ -304,6 +304,15 @@ BOOKMARK_DELETE
 "
 BOOKMARK_ADD
 "
+"
+PIN
+"
+"
+UNPIN
+"
+"
+SAVE_TO_POCKET
+"
 ]
 )
 .
@@ -632,6 +641,9 @@ valid
 "
 menu_plus_or_keyboard
 "
+"
+unexpected
+"
 ]
 )
 .
@@ -648,6 +660,10 @@ counter
 alert
 "
 ]
+)
+.
+required
+(
 )
 visibility_event_rcvd_ts
 :
