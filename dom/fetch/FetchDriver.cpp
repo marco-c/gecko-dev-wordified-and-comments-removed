945bc216e5d5e0c7463a9edf52ac6cbaee8a99b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:26:36 +0000

    Bug 1380255 - FetchDriver should have threadsafe refcounting. r=baku
    UltraBlame original commit: 0db1445c1db99548fa5a03ab1158e527d9f3ba6d

--- dom/fetch/FetchDriver.cpp
+++ dom/fetch/FetchDriver.cpp
@@ -364,6 +364,10 @@ false
 #
 endif
 {
+AssertIsOnMainThread
+(
+)
+;
 MOZ_ASSERT
 (
 aRequest
@@ -388,6 +392,10 @@ FetchDriver
 (
 )
 {
+AssertIsOnMainThread
+(
+)
+;
 MOZ_ASSERT
 (
 mResponseAvailableCalled
@@ -4022,7 +4030,7 @@ CheckListenerChain
 )
 {
 return
-NS_ERROR_NO_INTERFACE
+NS_OK
 ;
 }
 NS_IMETHODIMP
