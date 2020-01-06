6f800de7d236765dc704bda7354e21d05280789b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 17:35:16 +0000

    Bug 1407952: Use stylo for Element::Closest. r=heycam
    MozReview-Commit-ID: 3H2piFT2CfF
    
    UltraBlame original commit: 91e5ad9c8c31f22cda3f3636b248c426b53f2809

--- layout/style/ServoBindingList.h
+++ layout/style/ServoBindingList.h
@@ -423,6 +423,13 @@ RawServoSelectorListBorrowed
 )
 SERVO_BINDING_FUNC
 (
+Servo_SelectorList_Closest
+RawGeckoElementBorrowedOrNull
+RawGeckoElementBorrowed
+RawServoSelectorListBorrowed
+)
+SERVO_BINDING_FUNC
+(
 Servo_StyleSet_AddSizeOfExcludingThis
 void
 mozilla
