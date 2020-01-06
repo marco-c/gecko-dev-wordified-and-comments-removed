0e98c7857240cc7924e64eac045f930d89605c01
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:24:13 +0000

    Bug 1367745 - Make sure 'process_output' messages are logged at the proper mozharness level, r=jgraham
    In addition to saving the log level of 'process_output' messages, this will also start passing 'log'
    messages through the error lists. This means mozharness will start using 'log' errors when determining
    the tbpl_status and worst_log_level.
    
    MozReview-Commit-ID: CZnH6aI1Wo0
    
    UltraBlame original commit: f19ff82014373b5c3763ad6f5bdb30350e5a1dd8

--- testing/mozharness/mozharness/base/log.py
+++ testing/mozharness/mozharness/base/log.py
@@ -2040,6 +2040,10 @@ parse
 "
 "
         
+log_level
+=
+INFO
+        
 for
 error_check
 in
@@ -2141,7 +2145,7 @@ get
 '
 level
 '
-INFO
+log_level
 )
                 
 if
@@ -2280,7 +2284,7 @@ log_output
                 
 self
 .
-info
+log
 (
 '
 %
@@ -2288,7 +2292,13 @@ s
 '
 %
 line
+level
+=
+log_level
 )
+        
+return
+log_level
     
 def
 add_lines
