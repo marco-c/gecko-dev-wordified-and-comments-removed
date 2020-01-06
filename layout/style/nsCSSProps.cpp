759734943e8c356c73fe661ff0c429e31baa057b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 12:51:21 +0000

    Bug 1387410: Stop the grid shorthand from resetting grid-gap properties. r=heycam
    MozReview-Commit-ID: GxU9YuAUc00
    
    UltraBlame original commit: 23a9ea8f5b16a2abe56fbf7639905c9018358544

--- layout/style/nsCSSProps.cpp
+++ layout/style/nsCSSProps.cpp
@@ -12698,8 +12698,6 @@ eCSSProperty_grid_template_columns
 eCSSProperty_grid_auto_flow
 eCSSProperty_grid_auto_rows
 eCSSProperty_grid_auto_columns
-eCSSProperty_grid_row_gap
-eCSSProperty_grid_column_gap
 eCSSProperty_UNKNOWN
 }
 ;
