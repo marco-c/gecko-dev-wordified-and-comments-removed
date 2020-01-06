e06e5c7db38195fce538556d6d3ee0a0afb8ffe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:01:00 +0000

    Bug 1252211 - Convert TreeContentView to WebIDL. r=bz.
    UltraBlame original commit: 629ace67da3bec76161266bcccdbcb96561ec96f

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -16,12 +16,6 @@ nsDOMClassInfoID
 {
 eDOMClassInfo_DOMPrototype_id
 eDOMClassInfo_DOMConstructor_id
-#
-ifdef
-MOZ_XUL
-eDOMClassInfo_TreeContentView_id
-#
-endif
 eDOMClassInfo_ContentFrameMessageManager_id
 eDOMClassInfo_ContentProcessMessageManager_id
 eDOMClassInfo_ChromeMessageBroadcaster_id
