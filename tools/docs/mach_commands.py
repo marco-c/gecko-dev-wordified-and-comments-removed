83724709cf6daddcbe90ebe8a669de148b4ab966
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 11:09:02 +0000

    Bug 1390693 - Use distribution_files() for finding files to upload; r=dustin
    Instead of doing the file finding inside s3_upload(), the function now
    takes the output of distribution_files().
    
    The new code is much simpler.
    
    MozReview-Commit-ID: 43i2Alvyu5i
    
    UltraBlame original commit: c74c6ded8370e1b6c3d6220cc902709b5a0abe26

--- tools/docs/mach_commands.py
+++ tools/docs/mach_commands.py
@@ -1094,12 +1094,19 @@ html
         
 from
 moztreedocs
+import
+distribution_files
+        
+from
+moztreedocs
 .
 upload
 import
 s3_upload
         
-s3_upload
+files
+=
+distribution_files
 (
 os
 .
@@ -1111,10 +1118,16 @@ outdir
 '
 html
 '
+                                                
 '
 Mozilla_Source_Tree_Docs
 '
 )
+)
+        
+s3_upload
+(
+files
 )
 def
 die
