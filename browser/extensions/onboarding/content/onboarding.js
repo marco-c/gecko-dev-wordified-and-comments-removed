d3c83f7f2078ca4a0407b79f88de04fd069ea6c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:28:31 +0000

    Backed out changeset bd9e37c5008d (bug 1382520) for failing mochitest browser/base/content/test/static/browser_all_files_referenced.js. r=backout
    UltraBlame original commit: f667fdab3ac0b9e10ce396f8cdc33499ced1ffbf

--- browser/extensions/onboarding/content/onboarding.js
+++ browser/extensions/onboarding/content/onboarding.js
@@ -2731,7 +2731,7 @@ onboarding
 /
 img
 /
-figure_library
+figure_search
 .
 svg
 "
@@ -3010,7 +3010,7 @@ onboarding
 /
 img
 /
-figure_singlesearch
+figure_search
 .
 svg
 "
@@ -3293,7 +3293,7 @@ onboarding
 /
 img
 /
-figure_performance
+figure_sync
 .
 svg
 "
