a0ff295aa3bdf9d703eec0e686912daae2a8e465
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 19:55:00 +0000

    Bug 1401379 - Part 7: Make nsBrowserStatusFilter cycle collected, r=smaug
    MozReview-Commit-ID: AsyFAgKJkxq
    
    UltraBlame original commit: 9715db005f4aa66acc7be3e77129a6d044f2697c

--- toolkit/components/statusfilter/nsBrowserStatusFilter.h
+++ toolkit/components/statusfilter/nsBrowserStatusFilter.h
@@ -35,6 +35,13 @@ h
 #
 include
 "
+nsCycleCollectionParticipant
+.
+h
+"
+#
+include
+"
 nsITimer
 .
 h
@@ -69,7 +76,12 @@ nsBrowserStatusFilter
 (
 )
 ;
-NS_DECL_ISUPPORTS
+NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+NS_DECL_CYCLE_COLLECTION_CLASS_AMBIGUOUS
+(
+nsBrowserStatusFilter
+nsIWebProgress
+)
 NS_DECL_NSIWEBPROGRESS
 NS_DECL_NSIWEBPROGRESSLISTENER
 NS_DECL_NSIWEBPROGRESSLISTENER2
