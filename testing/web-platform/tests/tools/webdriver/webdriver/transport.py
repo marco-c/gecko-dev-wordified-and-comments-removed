aae7632b3632675137b23ec0d8b550a14c007934
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:57:35 +0000

    Bug 1405325 - Correct HTTPWireProtocol#send documentation. r=maja_zf
    MozReview-Commit-ID: 3ZfXYHOaKRC
    
    UltraBlame original commit: 5a2f8087260d9e35a5ad602bff2fcc3133181125

--- testing/web-platform/tests/tools/webdriver/webdriver/transport.py
+++ testing/web-platform/tests/tools/webdriver/webdriver/transport.py
@@ -373,28 +373,24 @@ remote
 param
 method
 :
-"
-POST
-"
-or
-"
 GET
-"
+POST
+or
+DELETE
 .
         
 :
 param
-url
+uri
 :
-"
-command
-part
-"
+Relative
+endpoint
 of
 the
 requests
 URL
 path
+.
         
 :
 param
@@ -409,14 +405,12 @@ Defaults
 to
 an
 empty
-dictionary
             
+dictionary
 if
 method
 is
-"
 POST
-"
 .
         
 :
@@ -435,17 +429,16 @@ request
 :
 return
 :
-an
-instance
+Instance
 of
 wdclient
 .
 Response
 describing
 the
+            
 HTTP
 response
-            
 received
 from
 the
