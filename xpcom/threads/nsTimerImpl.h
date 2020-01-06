98c3162f9206a73b1a7cece05b8ade102e72ebfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 09:18:35 +0000

    Backed out changeset 37c9e9d07aac (bug 1351148)
    UltraBlame original commit: 248262b1622974d0ca5028ade262e1a7420c74f2

--- xpcom/threads/nsTimerImpl.h
+++ xpcom/threads/nsTimerImpl.h
@@ -497,9 +497,6 @@ nsresult
 InitCommon
 (
 const
-mozilla
-:
-:
 TimeDuration
 &
 aDelay
@@ -750,15 +747,9 @@ mType
 int32_t
 mGeneration
 ;
-mozilla
-:
-:
 TimeDuration
 mDelay
 ;
-mozilla
-:
-:
 TimeStamp
 mTimeout
 ;
