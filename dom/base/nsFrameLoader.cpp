8471c818b037a71a68ad5b326660bd4db237fd95
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:43:59 +0000

    Bug 1405792 - Remove nsIDOMHTMLIFrameElement r=bz
    MozReview-Commit-ID: 1u9C66X8j59
    
    UltraBlame original commit: ef55498169e2a9ae09ae23c40de1d97fa288d2e1

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -24,13 +24,6 @@ h
 #
 include
 "
-nsIDOMHTMLIFrameElement
-.
-h
-"
-#
-include
-"
 nsIDOMMozBrowserFrame
 .
 h
--- editor/libeditor/HTMLEditorDataTransfer.cpp
+++ editor/libeditor/HTMLEditorDataTransfer.cpp
@@ -264,13 +264,6 @@ h
 #
 include
 "
-nsIDOMHTMLIFrameElement
-.
-h
-"
-#
-include
-"
 nsIDOMHTMLImageElement
 .
 h
