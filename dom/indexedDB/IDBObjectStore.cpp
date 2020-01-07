54bcc9a0c266d2fa031bcb5e5198947111df4026
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 16:55:11 +0000

    Backed out changeset 2692e7aaefbd (bug 1434308)
    UltraBlame original commit: 85a77aec89fbf269c6b39fcb037233359665656e

--- dom/indexedDB/IDBObjectStore.cpp
+++ dom/indexedDB/IDBObjectStore.cpp
@@ -511,7 +511,7 @@ JS
 StructuredCloneScope
 :
 :
-DifferentProcess
+SameProcessSameThread
 nullptr
 nullptr
 )
@@ -5755,7 +5755,7 @@ JS
 StructuredCloneScope
 :
 :
-DifferentProcess
+SameProcessSameThread
 aValue
 &
 callbacks
@@ -6451,7 +6451,7 @@ JS
 StructuredCloneScope
 :
 :
-DifferentProcess
+SameProcessSameThread
 aValue
 &
 callbacks
@@ -6877,7 +6877,7 @@ JS
 StructuredCloneScope
 :
 :
-DifferentProcess
+SameProcessSameThread
 aValue
 &
 callbacks
