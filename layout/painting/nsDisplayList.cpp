974570e2b8d698c45af847fd713ce3c25f334102
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 01:04:16 +0000

    Merge inbound to mozilla-central r=merge a=merge
    UltraBlame original commit: 22d2831cc1f41e1b3e1ebac9be5a7aff33684843

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -20743,7 +20743,7 @@ bgClipRect
 &
 mBounds
 ;
-DrawResult
+ImgDrawResult
 result
 =
 nsCSSRendering
@@ -21507,7 +21507,7 @@ bgClipRect
 =
 aClipRect
 ;
-DrawResult
+ImgDrawResult
 result
 =
 nsCSSRendering
@@ -23011,7 +23011,7 @@ nsDisplayBackgroundGeometry
 UpdateDrawResult
 (
 this
-DrawResult
+ImgDrawResult
 :
 :
 SUCCESS
@@ -28821,7 +28821,7 @@ PaintBorderFlags
 (
 )
 ;
-DrawResult
+ImgDrawResult
 result
 =
 nsCSSRendering
@@ -46946,7 +46946,7 @@ mozilla
 image
 :
 :
-DrawResult
+ImgDrawResult
 :
 :
 SUCCESS
