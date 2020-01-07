c43d94d3fbb91633f3b2ed69b75e631e314f3a33
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:50:41 +0000

    Bug 1466222 - Drop unused config upload_branch. r=catlee
    for "Cleanup l10n mozharness config files." (Batch 2)
    
    Differential Revision: https://phabricator.services.mozilla.com/D1562
    
    UltraBlame original commit: 18fe0eb0545cfa2b5809289dbd34e00841a5f41e

--- testing/mozharness/configs/single_locale/mozilla-central_android-api-16.py
+++ testing/mozharness/configs/single_locale/mozilla-central_android-api-16.py
@@ -254,23 +254,6 @@ MOZ_UPDATE_CHANNEL
 }
     
 "
-upload_branch
-"
-:
-"
-%
-s
--
-android
--
-api
--
-16
-"
-%
-BRANCH
-    
-"
 platform
 "
 :
--- testing/mozharness/configs/single_locale/mozilla-release_android-api-16.py
+++ testing/mozharness/configs/single_locale/mozilla-release_android-api-16.py
@@ -266,21 +266,4 @@ MOZ_UPDATE_CHANNEL
 MOZ_UPDATE_CHANNEL
     
 }
-    
-"
-upload_branch
-"
-:
-"
-%
-s
--
-android
--
-api
--
-16
-"
-%
-BRANCH
 }
