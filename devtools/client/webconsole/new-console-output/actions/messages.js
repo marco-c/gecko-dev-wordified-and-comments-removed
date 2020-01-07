af6d658c20fe40fdf0ee7ba2aff16681da137cd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 07:56:06 +0000

    Bug 1307928 - Remove private messages on lastPrivateContextExited event; r=bgrins.
    Add a redux action to handle the event and clear private messages from the store.
    A new `cleanState` function was extracted from `limitTopLevelMessage` so we can
    use it in this case.
    
    MozReview-Commit-ID: Hp6o9iZAbZ7
    
    UltraBlame original commit: e5f12661cc1af27b63aedfc21200e27e996f3637

--- devtools/client/webconsole/new-console-output/actions/messages.js
+++ devtools/client/webconsole/new-console-output/actions/messages.js
@@ -89,6 +89,7 @@ MESSAGE_OPEN
 MESSAGE_CLOSE
 MESSAGE_TYPE
 MESSAGE_TABLE_RECEIVE
+PRIVATE_MESSAGES_CLEAR
 }
 =
 require
@@ -229,6 +230,19 @@ return
 type
 :
 MESSAGES_CLEAR
+}
+;
+}
+function
+privateMessagesClear
+(
+)
+{
+return
+{
+type
+:
+PRIVATE_MESSAGES_CLEAR
 }
 ;
 }
@@ -484,6 +498,7 @@ messageClose
 messageTableDataGet
 networkMessageUpdate
 networkUpdateRequest
+privateMessagesClear
 messageTableDataReceive
 }
 ;
