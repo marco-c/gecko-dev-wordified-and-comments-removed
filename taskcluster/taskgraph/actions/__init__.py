4f53b401fd3cefaf267b6befdffbabbacc84a289
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 19:05:39 +0000

    Bug 1415868 - Remove support for register_action_task; r=jonasfj,tomprince
    In a post-actions-as-hooks world, users will not have scopes to create tasks,
    so this mode of action definition will not be possible. This is not currently
    used from Treeherder (it links to
    https://tools.taskcluster.net/tasks/<taskid>/interactive instead)
    
    This drops support for the JSON-e-only interactive action; that action is not
    currently used from treeherder, so that should have no impact for users.
    
    MozReview-Commit-ID: 9i3POpjahAc
    
    UltraBlame original commit: 2c95df49455b5499bb1bbd5d174c32217daef61a

--- taskcluster/taskgraph/actions/__init__.py
+++ taskcluster/taskgraph/actions/__init__.py
@@ -22,7 +22,6 @@ registry
 import
 (
     
-register_task_action
 register_callback_action
 render_actions_json
 trigger_action_callback
@@ -30,10 +29,6 @@ trigger_action_callback
 __all__
 =
 [
-    
-'
-register_task_action
-'
     
 '
 register_callback_action
