daa72e595d909794b7d303b349e04cdfd4c30d4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 23:25:48 +0000

    Bug 1399501 - Don't allow glsl[130,400) unless we have gpu_shader5. - r=kvark
    MozReview-Commit-ID: EUk2uc64Tok
    
    UltraBlame original commit: 781e6389661eebbdbda056f9c1a27be6eccbae4c

--- gfx/gl/GLContext.cpp
+++ gfx/gl/GLContext.cpp
@@ -511,6 +511,9 @@ GL_ARB_framebuffer_sRGB
 GL_ARB_geometry_shader4
 "
 "
+GL_ARB_gpu_shader5
+"
+"
 GL_ARB_half_float_pixel
 "
 "
@@ -631,6 +634,9 @@ GL_EXT_framebuffer_sRGB
 GL_EXT_gpu_shader4
 "
 "
+GL_EXT_gpu_shader5
+"
+"
 GL_EXT_multisampled_render_to_texture
 "
 "
@@ -722,6 +728,9 @@ GL_NV_framebuffer_blit
 "
 "
 GL_NV_geometry_program4
+"
+"
+GL_NV_gpu_shader5
 "
 "
 GL_NV_half_float
--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -305,6 +305,7 @@ get_integer64_indexed
 get_query_object_i64v
 get_query_object_iv
 gpu_shader4
+gpu_shader5
 instanced_arrays
 instanced_non_arrays
 internalformat_query
@@ -928,6 +929,7 @@ ARB_draw_instanced
 ARB_framebuffer_object
 ARB_framebuffer_sRGB
 ARB_geometry_shader4
+ARB_gpu_shader5
 ARB_half_float_pixel
 ARB_instanced_arrays
 ARB_internalformat_query
@@ -968,6 +970,7 @@ EXT_framebuffer_multisample
 EXT_framebuffer_object
 EXT_framebuffer_sRGB
 EXT_gpu_shader4
+EXT_gpu_shader5
 EXT_multisampled_render_to_texture
 EXT_occlusion_query_boolean
 EXT_packed_depth_stencil
@@ -999,6 +1002,7 @@ NV_draw_instanced
 NV_fence
 NV_framebuffer_blit
 NV_geometry_program4
+NV_gpu_shader5
 NV_half_float
 NV_instanced_arrays
 NV_primitive_restart
