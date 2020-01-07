3e2fdd850761c67edd8228d3c01bab3698ce4523
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:50:39 +0000

    Bug 1466222 - Remove locales_file and locales_platform from desktop and mobile l10n. r=catlee
    Leave support for multil10n uses for now.
    
    for "Cleanup l10n mozharness config files." (Batch 2)
    
    Differential Revision: https://phabricator.services.mozilla.com/D1561
    
    UltraBlame original commit: bb28c2d9d5f51f6e7eb797b892247948f1f7021b

--- testing/mozharness/configs/single_locale/win32_devedition.py
+++ testing/mozharness/configs/single_locale/win32_devedition.py
@@ -130,22 +130,6 @@ l10n
 "
     
 "
-locales_file
-"
-:
-"
-src
-/
-browser
-/
-locales
-/
-all
--
-locales
-"
-    
-"
 locales_dir
 "
 :
--- testing/mozharness/configs/single_locale/win64.py
+++ testing/mozharness/configs/single_locale/win64.py
@@ -128,22 +128,6 @@ l10n
 "
     
 "
-locales_file
-"
-:
-"
-src
-/
-browser
-/
-locales
-/
-all
--
-locales
-"
-    
-"
 locales_dir
 "
 :
