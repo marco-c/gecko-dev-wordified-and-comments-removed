43f24878dbf8aa989c531175ecb10ce13ab2780d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 09:26:37 +0000

    Bug 1416045. r=mayhemer CLOSED TREE
    Reviewers: mayhemer
    
    Reviewed By: mayhemer
    
    Subscribers: freddyb, dveditz, mayhemer, ckerschb, vinoth
    
    Tags: PHID-PROJ-wkydohdk6pajyfn2llkb
    
    Bug #: 1416045
    
    Differential Revision: https://phabricator.services.mozilla.com/D675
    
    UltraBlame original commit: a0a2092724797e534549cc2d80dc9c423bfaf43d

--- netwerk/streamconv/converters/nsMultiMixedConv.h
+++ netwerk/streamconv/converters/nsMultiMixedConv.h
@@ -403,6 +403,12 @@ mContentType
 nsCString
 mContentDisposition
 ;
+nsCString
+mContentSecurityPolicy
+;
+nsCString
+mRootContentSecurityPolicy
+;
 uint64_t
 mContentLength
 ;
@@ -460,6 +466,7 @@ HEADER_CONTENT_DISPOSITION
 HEADER_SET_COOKIE
 HEADER_CONTENT_RANGE
 HEADER_RANGE
+HEADER_CONTENT_SECURITY_POLICY
 HEADER_UNKNOWN
 }
 mResponseHeader
