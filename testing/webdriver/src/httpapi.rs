e82712487c62e8043633699828618a9adecd7b6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:17:42 +0000

    Bug 1396821 - [geckodriver] Switch webdriver crate from rustc_serialize to serde. r=ato
    Instead use serde. This is the simplest possible conversion using the
    serde Value type everywhere. The intent is to use the automatically
    derived deserializers in the future.
    
    MozReview-Commit-ID: F25p325gbiC
    
    UltraBlame original commit: b61ce753f01e0c1a2ae06c58058880e653a857f0

--- testing/webdriver/src/httpapi.rs
+++ testing/webdriver/src/httpapi.rs
@@ -8,15 +8,6 @@ Captures
 }
 ;
 use
-rustc_serialize
-:
-:
-json
-:
-:
-Json
-;
-use
 hyper
 :
 :
@@ -40,6 +31,12 @@ Get
 Post
 Delete
 }
+;
+use
+serde_json
+:
+:
+Value
 ;
 use
 command
@@ -1361,7 +1358,7 @@ self
 &
 Captures
 &
-Json
+Value
 )
 -
 >
@@ -1412,7 +1409,7 @@ Captures
 _
 :
 &
-Json
+Value
 )
 -
 >
