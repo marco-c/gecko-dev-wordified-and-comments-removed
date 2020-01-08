1f93e0950012b22e7e2be87fa312d4812e2d9420
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 21:07:57 +0000

    Bug 1507080 [wpt PR 14048] - service worker: Let register('data://blah') throw a TypeError, a=testonly
    Automatic update from web-platform-testsservice worker: Let register('data://blah') throw a TypeError
    
    register('data://blah') should throw a TypeError instead of SecurityError.
    If scopeURL’s scheme is not one of "http" and "https", reject promise
    with a TypeError and abort these steps.[1]
    
    [1]: https://w3c.github.io/ServiceWorker/#start-register-algorithm
    
    Bug: 877138
    Change-Id: Ic1e8cae52ec9393152044aa37da389eb499165bb
    Reviewed-on: https://chromium-review.googlesource.com/c/1189688
    Commit-Queue: Amos Lim <eui-sang.limsamsung.com>
    Reviewed-by: Matt Falkenhagen <falkenchromium.org>
    Cr-Commit-Position: refs/heads/master{#608635}
    
    --
    
    wpt-commits: 64ee30dc4f4c7cd5016ffcfbbb6e1012b978ed66
    wpt-pr: 14048
    
    UltraBlame original commit: e6d88d9f73affa755a6c09ed70af2a9c76362e30

--- testing/web-platform/tests/service-workers/service-worker/resources/registration-tests-scope.js
+++ testing/web-platform/tests/service-workers/service-worker/resources/registration-tests-scope.js
@@ -799,9 +799,8 @@ promise_rejects
 t
 check_error_types
 ?
-'
-SecurityError
-'
+new
+TypeError
 :
 null
 register_method
@@ -832,7 +831,7 @@ URL
 should
 fail
 with
-SecurityError
+TypeError
 .
 '
 )
