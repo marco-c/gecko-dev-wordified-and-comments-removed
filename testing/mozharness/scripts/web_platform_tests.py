5ff2d5daeea0723309cf3f89890976c5aefc6fff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:04:20 +0000

    Bug 1476574 - Enable reset/dump for wpt and cleanup harness code for supporting reset/dump now that it is supported by all test suites. r=jmaher
    UltraBlame original commit: 7afb8c53da3ae88fef9b78915a4435a7d6908169

--- testing/mozharness/scripts/web_platform_tests.py
+++ testing/mozharness/scripts/web_platform_tests.py
@@ -2932,20 +2932,27 @@ extend
 per_test_args
 )
                 
+final_env
+=
+copy
+.
+copy
+(
+env
+)
+                
 if
 self
 .
 per_test_coverage
 :
                     
-gcov_dir
-jsvm_dir
-=
 self
 .
 set_coverage_env
 (
-env
+final_env
+is_baseline_test
 )
                 
 return_code
@@ -2975,7 +2982,7 @@ parser
                                                
 env
 =
-env
+final_env
 )
                 
 if
@@ -2988,8 +2995,7 @@ self
 .
 add_per_test_coverage_report
 (
-gcov_dir
-jsvm_dir
+final_env
 suite
 per_test_args
 [
