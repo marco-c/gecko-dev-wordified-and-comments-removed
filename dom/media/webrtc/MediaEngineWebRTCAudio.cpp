e4e0d3bb8bf47a959937ae2a06860dda18caae5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 19:07:15 +0000

    Merge inbound to mozilla-central.  a=merge
    UltraBlame original commit: c6a6e76dc5424395e45c3d47aa696eb2f7d09e5e

--- dom/media/webrtc/MediaEngineWebRTCAudio.cpp
+++ dom/media/webrtc/MediaEngineWebRTCAudio.cpp
@@ -847,10 +847,6 @@ ApplySettings
 outputPrefs
 )
 ;
-mCurrentPrefs
-=
-outputPrefs
-;
 return
 NS_OK
 ;
@@ -2102,10 +2098,6 @@ NS_OK
 )
 )
 ;
-mCurrentPrefs
-=
-outputPrefs
-;
 return
 rv
 ;
@@ -2873,11 +2865,6 @@ NS_OK
 ;
 }
 )
-)
-;
-ApplySettings
-(
-mCurrentPrefs
 )
 ;
 MOZ_ASSERT
--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -1325,7 +1325,7 @@ Create
 Module
 :
 :
-ALLOW_IN_GPU_PROCESS
+ALLOW_IN_GPU_AND_VR_PROCESS
 }
 {
 &
@@ -1522,7 +1522,7 @@ nullptr
 Module
 :
 :
-ALLOW_IN_GPU_PROCESS
+ALLOW_IN_GPU_AND_VR_PROCESS
 }
 ;
 static
