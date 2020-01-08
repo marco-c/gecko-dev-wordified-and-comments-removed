bf2715d6f65a6e41c00b41e165ce3b5418741f52
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:34:56 +0000

    Merge mozilla-central to mozilla inbound. a=merge
    UltraBlame original commit: 044705c4d308da03fd8f89f42497ffd7b17614f1

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -7128,8 +7128,6 @@ BytecodeEmitter
 :
 emitNewInit
 (
-JSProtoKey
-key
 )
 {
 const
@@ -7180,10 +7178,7 @@ code
 1
 ]
 =
-jsbytecode
-(
-key
-)
+0
 ;
 code
 [
@@ -16324,7 +16319,6 @@ if
 !
 emitNewInit
 (
-JSProto_Object
 )
 )
 return
@@ -16799,7 +16793,6 @@ if
 !
 emitNewInit
 (
-JSProto_Object
 )
 )
 return
@@ -34699,7 +34692,6 @@ if
 !
 emitNewInit
 (
-JSProto_Object
 )
 )
 return
@@ -37638,7 +37630,6 @@ if
 !
 emitNewInit
 (
-JSProto_Object
 )
 )
 return
