41adbd762103925cfef7708c50ca9e47a9183d8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 17:09:39 +0000

    Bug 1498499 - [Wayland] Implement triple buffering at Wayland, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D8849
    
    UltraBlame original commit: b815b16f74a14ce0173bae0000f47ebf5e277e17

--- widget/gtk/WindowSurfaceWayland.h
+++ widget/gtk/WindowSurfaceWayland.h
@@ -22,6 +22,10 @@ Types
 .
 h
 "
+#
+define
+BACK_BUFFER_NUM
+2
 namespace
 mozilla
 {
@@ -521,6 +525,9 @@ mWaylandBufferDamage
 WindowBackBuffer
 *
 mBackupBuffer
+[
+BACK_BUFFER_NUM
+]
 ;
 RefPtr
 <
