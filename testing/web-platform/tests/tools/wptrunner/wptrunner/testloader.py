8925fddebed915bc29278867da268fdb227043a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 10:40:59 +0000

    Bug 1485403 - Handle ManifestVersionMismatch when loading the test manifest, a=testonly
    UltraBlame original commit: 4f8e3acdef35337054c23776fcb78ce518927fc3

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
@@ -3105,6 +3105,9 @@ self
 manifest_download
 )
         
+try
+:
+            
 manifest_file
 =
 manifest
@@ -3123,6 +3126,21 @@ meta_filters
 self
 .
 meta_filters
+)
+        
+except
+manifest
+.
+ManifestVersionMismatch
+:
+            
+manifest_file
+=
+manifest
+.
+Manifest
+(
+url_base
 )
         
 if
