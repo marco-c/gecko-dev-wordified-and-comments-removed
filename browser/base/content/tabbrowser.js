3e66dfffdb11aac3e86b7c3b995da1660df2caa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 12:44:44 +0000

    Bug 1393570 - Support cookieStoreId in windows.create extension API r=Gijs,rpl
    Other (internal API) changes besides extension API changes:
    
    - This also introduces support for opening a window with multiple tabs
      in a non-default container tab.
    
    - This also adds LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL to the
      gBrowser.loadTabs call, unless allowInheritPrincipal is set. Currently
      there are no callers that set this flag, but in case it's desired,
      I added an opt-in via window.arguments[10] in browser.xul/js.
    
      For single-argument URLs, the flag is an opt-out, since there are
      multiple callers that rely on principal inheritance (bug 1475201).
    
    Depends on D4928
    
    Differential Revision: https://phabricator.services.mozilla.com/D4929
    
    UltraBlame original commit: 23f496cd8a42d49f732e0ba4bae7cb0cc8aa5818

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -6705,6 +6705,7 @@ loadTabs
 (
 aURIs
 {
+allowInheritPrincipal
 allowThirdPartyFixup
 inBackground
 newIndex
@@ -6894,6 +6895,22 @@ nsIWebNavigation
 LOAD_FLAGS_FIXUP_SCHEME_TYPOS
 ;
 }
+if
+(
+!
+allowInheritPrincipal
+)
+{
+flags
+|
+=
+Ci
+.
+nsIWebNavigation
+.
+LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL
+;
+}
 try
 {
 browser
@@ -6933,6 +6950,7 @@ let
 params
 =
 {
+allowInheritPrincipal
 ownerTab
 :
 owner
@@ -7027,6 +7045,7 @@ let
 params
 =
 {
+allowInheritPrincipal
 skipAnimation
 :
 true
