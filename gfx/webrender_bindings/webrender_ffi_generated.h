6aed79dc737515007ca8244ce0b4546e004ffee9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:04:26 +0000

    Bug 1506575 - Re-generate FFI header. r=kats
    Depends on D11633
    
    Differential Revision: https://phabricator.services.mozilla.com/D11634
    
    UltraBlame original commit: ea56fcfdbf72dd653c2a9f63acece63c3b822a4d

--- gfx/webrender_bindings/webrender_ffi_generated.h
+++ gfx/webrender_bindings/webrender_ffi_generated.h
@@ -1472,6 +1472,9 @@ uintptr_t
 depth_target_textures
 ;
 uintptr_t
+swap_chain
+;
+uintptr_t
 total_gpu_bytes_allocated
 ;
 bool
@@ -1597,6 +1600,14 @@ depth_target_textures
 aOther
 .
 depth_target_textures
+&
+&
+swap_chain
+=
+=
+aOther
+.
+swap_chain
 &
 &
 total_gpu_bytes_allocated
