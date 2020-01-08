da354ef4598aa7ef61c75d3f6bc9c8524bbb9bde
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 16:24:10 +0000

    Bug 1498571 - Cleanup FontPropertyTypes. r=jwatt
    Now that they can be constexpr, do that.
    
    Also, make default constructors initialize to the minimum value, now that we no longer need to use the default due to nsCSSValue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7757
    
    UltraBlame original commit: e9b7f6a58cc27e19f3012ffac4f81e5067b689bf

--- gfx/src/FontPropertyTypes.h
+++ gfx/src/FontPropertyTypes.h
@@ -89,12 +89,17 @@ FontPropertyValue
 {
 public
 :
+constexpr
 FontPropertyValue
 (
 )
-=
-default
-;
+:
+FontPropertyValue
+(
+Min
+)
+{
+}
 explicit
 FontPropertyValue
 (
@@ -302,6 +307,7 @@ Max
 protected
 :
 explicit
+constexpr
 FontPropertyValue
 (
 float
@@ -337,6 +343,7 @@ kMax
 ;
 }
 explicit
+constexpr
 FontPropertyValue
 (
 int
@@ -367,6 +374,7 @@ Max
 ;
 }
 explicit
+constexpr
 FontPropertyValue
 (
 InternalType
@@ -473,6 +481,7 @@ uint16_t
 {
 public
 :
+constexpr
 FontWeight
 (
 )
@@ -480,6 +489,7 @@ FontWeight
 default
 ;
 explicit
+constexpr
 FontWeight
 (
 float
@@ -493,6 +503,7 @@ aValue
 {
 }
 explicit
+constexpr
 FontWeight
 (
 int
@@ -506,6 +517,7 @@ aValue
 {
 }
 static
+constexpr
 FontWeight
 Normal
 (
@@ -519,6 +531,7 @@ kNormal
 ;
 }
 static
+constexpr
 FontWeight
 Thin
 (
@@ -532,6 +545,7 @@ kThin
 ;
 }
 static
+constexpr
 FontWeight
 Bold
 (
@@ -611,6 +625,7 @@ class
 WeightRange
 ;
 explicit
+constexpr
 FontWeight
 (
 InternalType
@@ -690,6 +705,7 @@ uint16_t
 {
 public
 :
+constexpr
 FontStretch
 (
 )
@@ -697,6 +713,7 @@ FontStretch
 default
 ;
 explicit
+constexpr
 FontStretch
 (
 float
@@ -710,6 +727,7 @@ aPercent
 {
 }
 static
+constexpr
 FontStretch
 Normal
 (
@@ -723,6 +741,7 @@ kNormal
 ;
 }
 static
+constexpr
 FontStretch
 UltraCondensed
 (
@@ -736,6 +755,7 @@ kUltraCondensed
 ;
 }
 static
+constexpr
 FontStretch
 ExtraCondensed
 (
@@ -749,6 +769,7 @@ kExtraCondensed
 ;
 }
 static
+constexpr
 FontStretch
 Condensed
 (
@@ -762,6 +783,7 @@ kCondensed
 ;
 }
 static
+constexpr
 FontStretch
 SemiCondensed
 (
@@ -775,6 +797,7 @@ kSemiCondensed
 ;
 }
 static
+constexpr
 FontStretch
 SemiExpanded
 (
@@ -788,6 +811,7 @@ kSemiExpanded
 ;
 }
 static
+constexpr
 FontStretch
 Expanded
 (
@@ -801,6 +825,7 @@ kExpanded
 ;
 }
 static
+constexpr
 FontStretch
 ExtraExpanded
 (
@@ -814,6 +839,7 @@ kExtraExpanded
 ;
 }
 static
+constexpr
 FontStretch
 UltraExpanded
 (
@@ -827,6 +853,7 @@ kUltraExpanded
 ;
 }
 static
+constexpr
 FontStretch
 FromStyle
 (
@@ -891,6 +918,7 @@ class
 StretchRange
 ;
 explicit
+constexpr
 FontStretch
 (
 InternalType
@@ -1033,6 +1061,7 @@ kDefaultAngle
 .
 0
 ;
+constexpr
 FontSlantStyle
 (
 )
@@ -1040,6 +1069,7 @@ FontSlantStyle
 default
 ;
 static
+constexpr
 FontSlantStyle
 Normal
 (
@@ -1053,6 +1083,7 @@ kNormal
 ;
 }
 static
+constexpr
 FontSlantStyle
 Italic
 (
@@ -1066,6 +1097,7 @@ kItalic
 ;
 }
 static
+constexpr
 FontSlantStyle
 Oblique
 (
@@ -1112,7 +1144,6 @@ Normal
 )
 ;
 }
-else
 if
 (
 strcmp
@@ -1133,8 +1164,6 @@ Italic
 )
 ;
 }
-else
-{
 if
 (
 mozilla
@@ -1193,7 +1222,6 @@ Italic
 (
 )
 ;
-}
 }
 bool
 IsNormal
@@ -1331,6 +1359,7 @@ class
 SlantStyleRange
 ;
 explicit
+constexpr
 FontSlantStyle
 (
 InternalType
@@ -1344,6 +1373,7 @@ aConstant
 {
 }
 explicit
+constexpr
 FontSlantStyle
 (
 float
