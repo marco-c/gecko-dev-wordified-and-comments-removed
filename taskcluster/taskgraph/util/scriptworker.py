c9f3f7c21c843eae01ca37116cd94a56917e900b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:19:22 +0000

    Bug 1352113 - Shippable Builds - Account for new declarative artifacts work. r=mtabara
    Differential Revision: https://phabricator.services.mozilla.com/D24831
    
    UltraBlame original commit: 4943b23813fe56a358febd1617e51937afc6943f

--- taskcluster/taskgraph/util/scriptworker.py
+++ taskcluster/taskgraph/util/scriptworker.py
@@ -5344,31 +5344,31 @@ platforms
 '
 linux
 -
-nightly
+shippable
 '
         
 '
 linux64
 -
-nightly
+shippable
 '
         
 '
 macosx64
 -
-nightly
+shippable
 '
         
 '
 win32
 -
-nightly
+shippable
 '
         
 '
 win64
 -
-nightly
+shippable
 '
         
 '
@@ -5376,7 +5376,7 @@ win64
 -
 aarch64
 -
-nightly
+shippable
 '
         
 '
