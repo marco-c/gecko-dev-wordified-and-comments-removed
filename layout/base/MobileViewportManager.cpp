f97a1950371838615fc0dcac3042c37045ec472a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:47:38 +0000

    Merge mozilla-inbound to mozilla-central. a=merge
    UltraBlame original commit: 19fc512f3c95b2f24edefbf6ee87a7bf63e618d1

--- layout/base/MobileViewportManager.cpp
+++ layout/base/MobileViewportManager.cpp
@@ -610,7 +610,7 @@ mozilla
 :
 CSSSize
 &
-aViewportSize
+aViewportOrContentSize
 )
 const
 {
@@ -623,7 +623,7 @@ ScreenSize
 (
 aDisplaySize
 )
-aViewportSize
+aViewportOrContentSize
 )
 ;
 MVM_LOG
@@ -1749,10 +1749,13 @@ SetResolutionAndScaleTo
 resolution
 .
 scale
-nsGkAtoms
+nsIPresShell
 :
 :
-other
+ChangeOrigin
+:
+:
+eMainThread
 )
 ;
 MVM_LOG
