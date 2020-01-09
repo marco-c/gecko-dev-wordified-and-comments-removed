add948049ea73e74bdad536aa45dca9f016b6c41
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 02:25:35 +0000

    Bug 1520815 - Make RunCbindgen.py generate less noise on failure. r=boris
    file_generate understands non-zero return codes, and makes the output
    less noisy.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16859
    
    UltraBlame original commit: 230f156b2393e214874269515b1c0059fe14431b

--- layout/style/RunCbindgen.py
+++ layout/style/RunCbindgen.py
@@ -158,19 +158,20 @@ returncode
 0
 :
         
-raise
-TypeError
+print
 (
-"
-cbindgen
-failed
-:
-%
-s
-"
-%
+stdout
+)
+        
+print
+(
 stderr
 )
+        
+return
+p
+.
+returncode
     
 output
 .
