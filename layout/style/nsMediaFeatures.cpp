5e9f69f6c40ce610ba8b0c390e598e7d0fa70827
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 15:25:56 +0000

    Bug 1532850 - Check the return value of LookAndFeel::GetInt() in case of the function failure. r=emilio
    Actually the function will fail on Android since on xpcshell tests we don't
    initialize AndroidBridge so that we can't query the corresponding system
    settings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22270
    
    UltraBlame original commit: e36b1516f12254855e4815afe07a015eedceca01

--- layout/style/nsMediaFeatures.cpp
+++ layout/style/nsMediaFeatures.cpp
@@ -2327,7 +2327,7 @@ _moz_gtk_csd_reversed_placement
 )
 ;
 }
-metricResult
+rv
 =
 LookAndFeel
 :
@@ -2338,10 +2338,18 @@ LookAndFeel
 :
 :
 eIntID_SystemUsesDarkTheme
+&
+metricResult
 )
 ;
 if
 (
+NS_SUCCEEDED
+(
+rv
+)
+&
+&
 metricResult
 )
 {
