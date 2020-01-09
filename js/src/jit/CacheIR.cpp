ef4bc2c8def3155e3cff77f80854d94d35a59620
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 16:48:49 +0000

    Merge mozilla-central to mozilla-inbound. CLOSED TREE
    UltraBlame original commit: 7404e19d25344119de65db17748b3874bc8a19f1

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -12056,7 +12056,7 @@ lastObjId
 env
 -
 >
-maybeShape
+shape
 (
 )
 )
@@ -12878,7 +12878,7 @@ lastObjId
 env
 -
 >
-maybeShape
+shape
 (
 )
 )
@@ -20373,19 +20373,6 @@ objValId
 maybeEmitIdGuard
 (
 id
-)
-;
-MOZ_ASSERT
-(
-obj
--
->
-is
-<
-ShapedObject
->
-(
-)
 )
 ;
 writer
