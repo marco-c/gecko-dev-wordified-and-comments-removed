40ee79472ba7e5ca5d3baba607f5d21ad987f917
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 23:45:27 +0000

    Bug 1542963 - use print_function for mozbuild and mozpack. r=glandium
    Lint python/mozbuild/{mozbuild,mozpack}.
    
    Differential Revision: https://phabricator.services.mozilla.com/D26644
    
    UltraBlame original commit: 1b57b9bd743554516ffac1ce3f65326a0059e0fd

--- python/mozbuild/mozbuild/preprocessor.py
+++ python/mozbuild/mozbuild/preprocessor.py
@@ -130,6 +130,7 @@ from
 __future__
 import
 absolute_import
+print_function
 import
 sys
 import
--- python/mozbuild/mozpack/errors.py
+++ python/mozbuild/mozpack/errors.py
@@ -2,6 +2,7 @@ from
 __future__
 import
 absolute_import
+print_function
 import
 sys
 from
@@ -637,12 +638,14 @@ _count
 1
         
 print
->
->
+(
+msg
+file
+=
 self
 .
 out
-msg
+)
     
 def
 fatal
