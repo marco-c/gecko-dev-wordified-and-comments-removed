4a64c53a5cab059249ec104fd347346058cd5db0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:15:44 +0000

    Bug 1559975 - Make config/ lint with 'py2' and 'py3' r=catlee
    Depends on D36091
    
    Differential Revision: https://phabricator.services.mozilla.com/D36092
    
    UltraBlame original commit: 9c5b3c7e1ca2240f765d62466cc917ad778407ef

--- config/MozZipFile.py
+++ config/MozZipFile.py
@@ -1,3 +1,7 @@
+from
+__future__
+import
+absolute_import
 import
 os
 import
@@ -312,7 +316,7 @@ external_attr
 )
 <
 <
-16L
+16
         
 else
 :
--- config/pythonpath.py
+++ config/pythonpath.py
@@ -1,3 +1,11 @@
+from
+__future__
+import
+absolute_import
+from
+__future__
+import
+print_function
 "
 "
 "
