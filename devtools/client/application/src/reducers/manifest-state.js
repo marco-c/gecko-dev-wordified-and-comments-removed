c2f92ef15e70445dbeb6063303c631cee118ae2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 16:29:53 +0000

    Bug 1572482 - Refetch manifest when reloading or navigating to a new page r=Ola
    Differential Revision: https://phabricator.services.mozilla.com/D45003
    
    UltraBlame original commit: 959078a539ec3504ff88c11f4fca3220f0a3b46b

--- devtools/client/application/src/reducers/manifest-state.js
+++ devtools/client/application/src/reducers/manifest-state.js
@@ -10,6 +10,7 @@ MANIFEST_ISSUE_LEVELS
 FETCH_MANIFEST_FAILURE
 FETCH_MANIFEST_START
 FETCH_MANIFEST_SUCCESS
+RESET_MANIFEST
 }
 =
 require
@@ -455,6 +456,19 @@ manifest
 null
 }
 )
+;
+case
+RESET_MANIFEST
+:
+const
+defaultState
+=
+ManifestState
+(
+)
+;
+return
+defaultState
 ;
 default
 :
