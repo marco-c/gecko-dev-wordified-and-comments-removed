8271b3b376216059a77ac3ac8cbd2dadd99ac3b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:01:06 +0000

    Bug 1564960 - Calaulate network id on stream transport thread r=michal
    Differential Revision: https://phabricator.services.mozilla.com/D39020
    
    UltraBlame original commit: 15d34f2ad3d9eeaee80547e26ae0a812227806f6

--- netwerk/system/mac/nsNetworkLinkService.h
+++ netwerk/system/mac/nsNetworkLinkService.h
@@ -55,7 +55,7 @@ nsIObserver
 {
 public
 :
-NS_DECL_ISUPPORTS
+NS_DECL_THREADSAFE_ISUPPORTS
 NS_DECL_NSINETWORKLINKSERVICE
 NS_DECL_NSIOBSERVER
 nsNetworkLinkService
@@ -143,6 +143,12 @@ info
 ;
 void
 calculateNetworkId
+(
+void
+)
+;
+void
+calculateNetworkIdInternal
 (
 void
 )
