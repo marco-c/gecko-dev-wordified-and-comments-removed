850b4ee0457b3ecdb987061d464087dca9fc4dca
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:59:55 +0000

    Bug 1589285 part 3.  Use the new GetString methods for Web IDL enums in various places.  r=edgar
    The idea is to stop directly accessing EnumTypeValues::strings in type-unsafe
    ways from consumer code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49533
    
    UltraBlame original commit: fbd7b66b2d88fd3e403d4b50eeed9753a61715fc

--- dom/media/webrtc/MediaEngineRemoteVideoSource.cpp
+++ dom/media/webrtc/MediaEngineRemoteVideoSource.cpp
@@ -810,7 +810,7 @@ aFM
 )
 {
 return
-NS_ConvertUTF8toUTF16
+NS_ConvertASCIItoUTF16
 (
 dom
 :
@@ -818,15 +818,10 @@ dom
 VideoFacingModeEnumValues
 :
 :
-strings
-[
-uint32_t
+GetString
 (
 aFM
 )
-]
-.
-value
 )
 ;
 }
