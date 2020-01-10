82b580b39b6a029bb785070c02ed0a1aa30f050e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:22:08 +0000

    Bug 1035892 - Handle 64-bit addresses for EXC_BAD_ACCESS exceptions on Mac r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D24356
    
    UltraBlame original commit: 2b1ffc8004d5e8160bc5bb1109c9f67a003048ef

--- toolkit/crashreporter/breakpad-client/mac/crash_generation/crash_generation_client.h
+++ toolkit/crashreporter/breakpad-client/mac/crash_generation/crash_generation_client.h
@@ -45,7 +45,7 @@ int
 exception_type
 int
 exception_code
-int
+int64_t
 exception_subcode
 mach_port_t
 crashing_thread
--- toolkit/crashreporter/breakpad-client/mac/handler/minidump_generator.h
+++ toolkit/crashreporter/breakpad-client/mac/handler/minidump_generator.h
@@ -247,7 +247,7 @@ int
 type
 int
 code
-int
+int64_t
 subcode
 mach_port_t
 thread_name
@@ -670,7 +670,7 @@ exception_type_
 int
 exception_code_
 ;
-int
+int64_t
 exception_subcode_
 ;
 mach_port_t
