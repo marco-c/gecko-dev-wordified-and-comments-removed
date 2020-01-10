fcee28116bc7f987f7a1d0699226f6f01cf01ab5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 17:47:57 +0000

    Bug 1578661 - Fix use counter test. r=boris
    By reporting the properties in MappedAttrParser that we used to (the test relies
    on counting those), and by adjusting the histogram name to the new thing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44697
    
    UltraBlame original commit: 2f8be5db1786aa0248e1cb342b179c44220a9236

--- dom/base/test/browser_use_counters.js
+++ dom/base/test/browser_use_counters.js
@@ -275,7 +275,7 @@ file_use_counter_svg_getElementById
 svg
 "
 "
-PROPERTY_FILL
+CSS_PROPERTY_Fill
 "
 )
 ;
@@ -288,7 +288,7 @@ file_use_counter_svg_currentScale
 svg
 "
 "
-PROPERTY_FILL
+CSS_PROPERTY_Fill
 "
 )
 ;
@@ -301,7 +301,7 @@ file_use_counter_svg_fill_pattern
 svg
 "
 "
-PROPERTY_FILLOPACITY
+CSS_PROPERTY_FillOpacity
 "
 true
 )
@@ -315,7 +315,7 @@ file_use_counter_svg_fill_pattern_internal
 svg
 "
 "
-PROPERTY_FILLOPACITY
+CSS_PROPERTY_FillOpacity
 "
 )
 ;
