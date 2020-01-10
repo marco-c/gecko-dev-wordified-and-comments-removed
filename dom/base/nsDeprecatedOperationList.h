9629820344cf39cc727d90249c00c9d5d305d280
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 20:28:31 +0000

    Backed out changeset 972db326c375 (bug 1587570) for causing wr failures on subsup-legacy-scriptshift-attributes-001.tentative.html
    UltraBlame original commit: 5808d51ab6cba029cb5aef64762cf395180f3123

--- dom/base/nsDeprecatedOperationList.h
+++ dom/base/nsDeprecatedOperationList.h
@@ -164,10 +164,6 @@ MathML_DeprecatedMencloseNotationRadical
 )
 DEPRECATED_OPERATION
 (
-MathML_DeprecatedScriptShiftAttributes
-)
-DEPRECATED_OPERATION
-(
 MathML_DeprecatedStyleAttribute
 )
 DEPRECATED_OPERATION
