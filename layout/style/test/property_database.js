cc6f9546579e46b2b2eb8d75c7eefa1c161ffa97
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 10:00:23 +0000

    Bug 1353164 Part 2 - Change dominant-baseline from reset to inherit r=heycam
    UltraBlame original commit: 5c8a0f41b86df61e7a1ef26b64fb355b5a11ab7d

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -50218,14 +50218,14 @@ text
 -
 decoration
 -
-thickness
+width
 "
 :
 {
 domProp
 :
 "
-textDecorationThickness
+textDecorationWidth
 "
 inherited
 :
@@ -54419,7 +54419,7 @@ dominantBaseline
 "
 inherited
 :
-false
+true
 type
 :
 CSS_TYPE_LONGHAND
