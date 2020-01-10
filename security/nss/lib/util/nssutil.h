d3d9b762d3ab1f0f9cfba6ab0cd05c9366295c2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 04:36:15 +0000

    Bug 1564499 - land NSS 264f19e7ede7 UPGRADE_NSS_RELEASE, r=me
    UltraBlame original commit: 2b165cf536ba4b59a58eacd86c82b173377e264c

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -22,7 +22,8 @@ NSSUTIL_VERSION
 "
 3
 .
-45
+46
+Beta
 "
 #
 define
@@ -31,7 +32,7 @@ NSSUTIL_VMAJOR
 #
 define
 NSSUTIL_VMINOR
-45
+46
 #
 define
 NSSUTIL_VPATCH
@@ -43,7 +44,7 @@ NSSUTIL_VBUILD
 #
 define
 NSSUTIL_BETA
-PR_FALSE
+PR_TRUE
 SEC_BEGIN_PROTOS
 extern
 const
