b91373ab8305d24f9d079ef93728628f6a82192e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:44:54 +0000

    Bug 1597737 : Importing urlparse from six to keep py2, py3 compatible r=whimboo,marionette-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D54128
    
    UltraBlame original commit: 5fe975a91529160a1e9c7b7a15f61e101a0511e6

--- testing/marionette/harness/marionette_harness/runner/httpd.py
+++ testing/marionette/harness/marionette_harness/runner/httpd.py
@@ -30,8 +30,6 @@ import
 sys
 import
 time
-import
-urlparse
 from
 wptserve
 import
@@ -47,6 +45,16 @@ default_routes
     
 server
 )
+from
+six
+.
+moves
+.
+urllib
+import
+parse
+as
+urlparse
 root
 =
 os
