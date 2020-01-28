e0056a26e3dd45615959581f04396afce560f124
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-28 17:21:50 +0000

    Bug 1599046 - P2. Remove calling NotifyOnContentBlocking in the child process r=timhuang,Ehsan
    This patch does the followings:
    1. Remove NotifyOnContentBlocking in WindowOuter and all the call sites.
       This is because all the content blocking event handling is moved to the parent.
    
    2. Replace UrlClassifierCommon::NotifyChannelBlocked with AntiTrackingCommon:::NotifyContentBlockingEventInParent.
       This is because we don't need to pass the request to the child anymore, which was implemented in NotifyChannelBlocked previously.
    
    3. Add NotifyContentBlockingEvent* utility functions to AntiTrackingCommon.cpp.
    
    4. Update AntiTrackingCommon::NotifyBlockingDecision, when it is called in the parent,
       do not send requests to the child, notify the parent directly
    
    Depends on D56874
    
    Differential Revision: https://phabricator.services.mozilla.com/D56875
    
    UltraBlame original commit: 2bc0fa3bbe0e39e97ac62a8effbe0a555c7830ad

--- toolkit/components/antitracking/AntiTrackingCommon.h
+++ toolkit/components/antitracking/AntiTrackingCommon.h
@@ -418,6 +418,17 @@ static
 void
 NotifyContentBlockingEvent
 (
+nsIChannel
+*
+aChannel
+uint32_t
+aRejectedReason
+)
+;
+static
+void
+NotifyContentBlockingEvent
+(
 nsPIDOMWindowOuter
 *
 aWindow
