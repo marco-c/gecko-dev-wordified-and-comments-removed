4b79878f4c1d47b2dbc9727426e47a8ad5b37dcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-02-13 18:45:36 +0000

    Bug 1614954 - [mozdevice] Do not attempt to grant android.permission.FOREGROUND_SERVICE, r=gbrown,snorp
    Depends on D62593
    
    Differential Revision: https://phabricator.services.mozilla.com/D62594
    
    UltraBlame original commit: d4d6690b242928912e4dd04aa4aeafea03f20871

--- testing/mozbase/mozdevice/mozdevice/adb.py
+++ testing/mozbase/mozdevice/mozdevice/adb.py
@@ -23244,30 +23244,6 @@ RECORD_AUDIO
             
 ]
             
-if
-self
-.
-version
->
-=
-version_codes
-.
-P
-:
-                
-permissions
-.
-append
-(
-'
-android
-.
-permission
-.
-FOREGROUND_SERVICE
-'
-)
-            
 self
 .
 _logger
