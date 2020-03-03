e15719d441955f9fa65f4d98e3e3dd07afa90b92
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-03 20:59:51 +0000

    Bug 1612077 - Use parallel directory processing for Lint when no path is specified or a support file is touched. r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D61219
    
    UltraBlame original commit: 8b2c3a0fa0516e3a140a76a4478b0ba186230eb5

--- python/mozlint/test/test_roller.py
+++ python/mozlint/test/test_roller.py
@@ -27,6 +27,10 @@ result
 import
 Issue
 ResultSummary
+from
+itertools
+import
+chain
 here
 =
 os
@@ -1803,6 +1807,7 @@ lint
 linters
 filedir
 monkeypatch
+files
 )
 :
     
@@ -1885,6 +1890,12 @@ read
 linter_path
 )
     
+lint
+.
+root
+=
+filedir
+    
 path
 =
 os
@@ -1929,11 +1940,19 @@ roll
 path
 )
     
-assert
+actual_files
+=
+sorted
+(
+chain
+(
+*
 jobs
-[
-0
-]
+)
+)
+    
+assert
+actual_files
 =
 =
 [
@@ -1955,18 +1974,22 @@ workdir
 True
 )
     
-assert
+actual_files
+=
+sorted
+(
+chain
+(
+*
 jobs
-[
-0
-]
+)
+)
+    
+assert
+actual_files
 =
 =
-[
-lint
-.
-root
-]
+files
     
 jobs
 =
@@ -1983,18 +2006,22 @@ outgoing
 True
 )
     
-assert
+actual_files
+=
+sorted
+(
+chain
+(
+*
 jobs
-[
-0
-]
+)
+)
+    
+assert
+actual_files
 =
 =
-[
-lint
-.
-root
-]
+files
     
 lint
 .
@@ -2023,18 +2050,22 @@ workdir
 True
 )
     
-assert
+actual_files
+=
+sorted
+(
+chain
+(
+*
 jobs
-[
-0
-]
+)
+)
+    
+assert
+actual_files
 =
 =
-[
-lint
-.
-root
-]
+files
 def
 test_setup
 (
