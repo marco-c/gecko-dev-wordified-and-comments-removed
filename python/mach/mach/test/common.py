41834cdbf53526aeea0bcfa1d1249e23ab4b60d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 00:47:39 +0000

    Bug 1581684 - [mach] Fix regression to mach command completion, r=glandium
    Not all callables have a __name__ attribute.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70229
    
    UltraBlame original commit: 84c16bc42776eb889845850a4f4570a44ea54180

--- python/mach/mach/test/common.py
+++ python/mach/mach/test/common.py
@@ -12,6 +12,8 @@ import
 sys
 import
 unittest
+import
+six
 try
 :
     
@@ -79,7 +81,7 @@ def
 get_mach
 (
 cls
-provider_file
+provider_files
 =
 None
 entry_point
@@ -124,14 +126,56 @@ core
         
 m
 .
+define_category
+(
+'
+misc
+'
+'
+Mach
+misc
+'
+'
+Testing
+for
+mach
+core
+'
+20
+)
+        
+m
+.
 populate_context_handler
 =
 context_handler
         
 if
-provider_file
+provider_files
 :
             
+if
+isinstance
+(
+provider_files
+six
+.
+string_types
+)
+:
+                
+provider_files
+=
+[
+provider_files
+]
+            
+for
+path
+in
+provider_files
+:
+                
 m
 .
 load_commands_from_file
@@ -145,7 +189,7 @@ join
 cls
 .
 provider_dir
-provider_file
+path
 )
 )
         
