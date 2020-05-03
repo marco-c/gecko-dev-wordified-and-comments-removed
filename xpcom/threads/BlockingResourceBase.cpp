c69109f7b61b28ac2d7b03a366f650319e4728ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-03 16:23:02 +0000

    Bug 1609907 - Add AUTO_PROFILER_THREAD_SLEEP around wait_for in DEBUG OffTheBooksCondVar::Wait - r=mstange
    In non-DEBUG builds, `OffTheBooksCondVar::Wait` has `AUTO_PROFILER_THREAD_SLEEP`, but it's not in DEBUG builds.
    `profiler_thread_sleep` does a `MOZ_ASSERT(mSleep == AWAKE)` in DEBUG builds, so the double call that happens in non-DEBUG wouldn't trigger the assertion!
    
    This patch adds `AUTO_PROFILER_THREAD_SLEEP` in DEBUG `OffTheBooksCondVar::Wait`, to catch more misuses during development.
    
    Depends on D72851
    
    Differential Revision: https://phabricator.services.mozilla.com/D72852
    
    UltraBlame original commit: 84811ffff4f354fc05a6060ef344c1952370d002

--- xpcom/threads/BlockingResourceBase.cpp
+++ xpcom/threads/BlockingResourceBase.cpp
@@ -2329,6 +2329,19 @@ nullptr
 ;
 CVStatus
 status
+;
+{
+#
+if
+defined
+(
+MOZILLA_INTERNAL_API
+)
+AUTO_PROFILER_THREAD_SLEEP
+;
+#
+endif
+status
 =
 mImpl
 .
@@ -2339,6 +2352,7 @@ mLock
 aDuration
 )
 ;
+}
 mLock
 -
 >
