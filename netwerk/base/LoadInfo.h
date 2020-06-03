a7b45df67f90099de795d3d9a960339b1089305c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:16:24 +0000

    Bug 1558394 - Pass the TriggeringSandboxFlags to nsILoadinfo r=ckerschb,smaug,necko-reviewers,valentin
    Add triggering Sandbox flags to loadinfo
    ***
    Pass triggering Flags into Loadinfo
    
    Differential Revision: https://phabricator.services.mozilla.com/D69588
    
    UltraBlame original commit: e13ede3c68d41582f6fb231acbb6cffd3db4e4a4

--- netwerk/base/LoadInfo.h
+++ netwerk/base/LoadInfo.h
@@ -471,6 +471,8 @@ nsSecurityFlags
 aSecurityFlags
 uint32_t
 aSandboxFlags
+uint32_t
+aTriggeringSandboxFlags
 nsContentPolicyType
 aContentPolicyType
 LoadTainting
@@ -868,6 +870,9 @@ mSecurityFlags
 ;
 uint32_t
 mSandboxFlags
+;
+uint32_t
+mTriggeringSandboxFlags
 ;
 nsContentPolicyType
 mInternalContentPolicyType
