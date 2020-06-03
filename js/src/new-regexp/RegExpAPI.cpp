e5c65f316dc5f6ca1bb1607f5558b7589f03f4c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-03 03:03:16 +0000

    Bug 1634135: Fix dummy TokenStream r=djvj
    This was broken by changes to token streams in bug 1592105.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73112
    
    UltraBlame original commit: 80d21da86415c1c8fa7f1798f0010716ff963e2d

--- js/src/new-regexp/RegExpAPI.cpp
+++ js/src/new-regexp/RegExpAPI.cpp
@@ -212,7 +212,7 @@ using
 frontend
 :
 :
-TokenStream
+DummyTokenStream
 ;
 using
 frontend
@@ -2214,14 +2214,11 @@ options
 cx
 )
 ;
-TokenStream
+DummyTokenStream
 dummyTokenStream
 (
 cx
 options
-nullptr
-0
-nullptr
 )
 ;
 ReportSyntaxError
