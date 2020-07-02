04b15ae186edd903e33bb78668cd5e17f2db979c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:18:03 +0000

    Bug 1647742 - Move the visibility pass out of prim_store/mod.rs. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D80693
    
    UltraBlame original commit: 969b25c5e866bf69d07fe6ec70e93a5f0760a58f

--- gfx/wr/webrender/src/render_target.rs
+++ gfx/wr/webrender/src/render_target.rs
@@ -174,7 +174,6 @@ prim_store
 PrimitiveStore
 DeferredResolve
 PrimitiveScratchBuffer
-PrimitiveVisibilityMask
 }
 ;
 use
@@ -257,6 +256,15 @@ texture_allocator
 ArrayAllocationTracker
 FreeRectSlice
 }
+;
+use
+crate
+:
+:
+visibility
+:
+:
+PrimitiveVisibilityMask
 ;
 use
 std
