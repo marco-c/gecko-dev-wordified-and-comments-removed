98cc84ab447a1f61525ef80d212e53806fc84be2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:14:59 +0000

    Backed out changeset 49c61831e2a7 (bug 1645488) on request by bebe
    UltraBlame original commit: 6e8ae867ec48d8021e565d76a566c849af3bab06

--- testing/talos/talos/cmdline.py
+++ testing/talos/talos/cmdline.py
@@ -341,6 +341,11 @@ parser
 .
 add_argument
     
+if
+not
+mach_interface
+:
+        
 add_arg
 (
 '
@@ -354,13 +359,13 @@ executablePath
 '
 required
 =
-False
+True
 dest
 =
 "
 browser_path
 "
-            
+                
 help
 =
 "
