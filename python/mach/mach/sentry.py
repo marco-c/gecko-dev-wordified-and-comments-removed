f16dc86e55c41235bde369a9449c40a04efea00f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:22:52 +0000

    Bug 1646099: Scrub hostname from sentry events r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D80112
    
    UltraBlame original commit: eca210facb3e0464b776cdebed9dca635deb1f04

--- python/mach/mach/sentry.py
+++ python/mach/mach/sentry.py
@@ -164,6 +164,7 @@ in
 (
 _settle_mach_module_id
 _patch_absolute_paths
+_delete_server_name
 )
 :
         
@@ -537,6 +538,25 @@ recursive_patch
 sentry_event
 needle
 replacement
+)
+    
+return
+sentry_event
+def
+_delete_server_name
+(
+sentry_event
+_
+)
+:
+    
+sentry_event
+.
+pop
+(
+"
+server_name
+"
 )
     
 return
