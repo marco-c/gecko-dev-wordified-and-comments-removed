cf4f95b2cdf44c90317a99ad2d2c05841e8453c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 13:54:29 +0000

    Bug 1649636 - part1 : update controlled tab Id to the key source. r=chunmin
    On GeckoView, when we update the property on the key source, such as the playback state, metadata. We want to know which tab that those properties change belong to.
    
    Therefore, we create a method `SetControlledTabBrowsingContextId()` to tell the key source which tab is being controlled and all properties update should belong to that tab.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81881
    
    UltraBlame original commit: bfaf4034a513e9212198094a3d575991a06ad6e0

--- dom/media/mediacontrol/MediaControlKeyManager.h
+++ dom/media/mediacontrol/MediaControlKeyManager.h
@@ -60,6 +60,17 @@ const
 override
 ;
 void
+SetControlledTabBrowsingContextId
+(
+Maybe
+<
+uint64_t
+>
+aTopLevelBrowsingContextId
+)
+override
+;
+void
 SetPlaybackState
 (
 MediaSessionPlaybackState
