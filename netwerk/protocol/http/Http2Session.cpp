30f4354a2e24df75e6b6abfcd9f77f882ff40659
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-17 01:26:06 +0000

    Bug 1642559 - Initiate a variable to fix SPDY_REQUEST_PER_CONN_2 r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D80413
    
    UltraBlame original commit: a8174ba258c5a9493a556b6b146984ef072bae54

--- netwerk/protocol/http/Http2Session.cpp
+++ netwerk/protocol/http/Http2Session.cpp
@@ -501,6 +501,10 @@ mOriginFrameActivated
 (
 false
 )
+mCntActivated
+(
+0
+)
 mTlsHandshakeFinished
 (
 false
@@ -938,7 +942,7 @@ Accumulate
 Telemetry
 :
 :
-SPDY_REQUEST_PER_CONN_2
+SPDY_REQUEST_PER_CONN_3
 mCntActivated
 )
 ;
