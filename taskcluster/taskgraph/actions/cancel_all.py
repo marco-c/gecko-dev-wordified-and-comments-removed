365f3dfc6ec7f5a6e9dda0da9919833e912d8f4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-01 01:23:29 +0000

    Bug 1636271 - [taskgraph] Create utility function for listing all tasks in a task group, r=tomprince
    Creates a new utility function in 'taskgraph.util.taskcluster' to return task
    definitions from a task group. Also refactors the existing
    'list_task_group_incomplete_tasks' function to use it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74407
    
    UltraBlame original commit: 7adf02fc1803dc86b766a48002f84e1cc7f88991

--- taskcluster/taskgraph/actions/cancel_all.py
+++ taskcluster/taskgraph/actions/cancel_all.py
@@ -37,7 +37,7 @@ taskcluster
 import
 (
     
-list_task_group_incomplete_tasks
+list_task_group_incomplete_task_ids
     
 cancel_task
     
@@ -236,7 +236,7 @@ t
 for
 t
 in
-list_task_group_incomplete_tasks
+list_task_group_incomplete_task_ids
 (
 task_group_id
 )
