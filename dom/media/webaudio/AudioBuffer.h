785a7ae24dc0dfa1a5273d3067705d30e9ea95d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-03 14:42:53 +0000

    Backed out 2 changesets (bug 1614009) for build bustage at dom/media/webaudio/AudioBuffer.cpp on a CLOSED TREE
    Backed out changeset 2bd694126445 (bug 1614009)
    Backed out changeset 7c315d9e2030 (bug 1614009)
    
    UltraBlame original commit: f7817f1be22afc6d149471f646dbcc69d49bd1cb

--- dom/media/webaudio/AudioBuffer.h
+++ dom/media/webaudio/AudioBuffer.h
@@ -371,7 +371,7 @@ aDestination
 uint32_t
 aChannelNumber
 uint32_t
-aBufferOffset
+aStartInChannel
 ErrorResult
 &
 aRv
@@ -390,7 +390,7 @@ aSource
 uint32_t
 aChannelNumber
 uint32_t
-aBufferOffset
+aStartInChannel
 ErrorResult
 &
 aRv
