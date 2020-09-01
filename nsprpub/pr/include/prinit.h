adb4147163144d4f9f425a0f5d8a902353787775
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:58:51 +0000

    Bug 1660371 - Uplift initial NSPR 4.29 beta snapshot. r=glandium UPGRADE_NSPR_RELEASE
    Differential Revision: https://phabricator.services.mozilla.com/D88589
    
    UltraBlame original commit: 502ffd8d642152c923e482776332fe54dac7f896

--- nsprpub/pr/include/prinit.h
+++ nsprpub/pr/include/prinit.h
@@ -45,7 +45,8 @@ PR_VERSION
 "
 4
 .
-28
+29
+Beta
 "
 #
 define
@@ -54,7 +55,7 @@ PR_VMAJOR
 #
 define
 PR_VMINOR
-28
+29
 #
 define
 PR_VPATCH
@@ -62,7 +63,7 @@ PR_VPATCH
 #
 define
 PR_BETA
-PR_FALSE
+PR_TRUE
 typedef
 PRBool
 (
