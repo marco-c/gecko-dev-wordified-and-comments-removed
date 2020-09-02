cff62750dca932bdb451ca376a4082bb9d3a5b83
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-02 10:32:27 +0000

    Bug 1662278 - Simplify mozperftest metric names. r=tarek
    This patch adds two new options to mozperftest. The --simplify-names argument can be provided to enable simplification of metric names and the --simplify-exclude option allows users to select which metrics to skip in the simplification. A bug relating to setting the default of a list option is also fixed here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88917
    
    UltraBlame original commit: 51251089fe61709267587ad72f459823a4f24869

--- python/mozperftest/mozperftest/metrics/notebookupload.py
+++ python/mozperftest/mozperftest/metrics/notebookupload.py
@@ -602,6 +602,32 @@ split
 by
 "
 )
+            
+simplify_names
+=
+self
+.
+get_arg
+(
+"
+simplify
+-
+names
+"
+)
+            
+simplify_exclude
+=
+self
+.
+get_arg
+(
+"
+simplify
+-
+exclude
+"
+)
         
 )
         
