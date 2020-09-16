a07220f8b1ffff334a235b5ff79fbdb29befc294
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-16 01:08:21 +0000

    Bug 1525854 - TRR shouldn't fallback to DNS on DNSSEC error r=necko-reviewers,dragana
    This patch adds support for the Extended DNS Errors draft code.
    https://datatracker.ietf.org/doc/html/draft-ietf-dnsop-extended-error-16
    
    While not yet in the draft, it seems the OPT code for Extended DNS Error is 15
    https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-11
    
    The list of errors for which we hard fail isn't necessarily final.
    I picked the errors that indicate a DNSSec failure, or an intentional
    filtering done by the resolver.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87698
    
    UltraBlame original commit: 419e26e3f452ff3975128841990610321ebf7bd2

--- testing/xpcshell/dns-packet/optioncodes.js
+++ testing/xpcshell/dns-packet/optioncodes.js
@@ -108,6 +108,13 @@ return
 KEY_TAG
 '
 case
+15
+:
+return
+'
+EDNS_ERROR
+'
+case
 26946
 :
 return
@@ -279,6 +286,13 @@ KEY_TAG
 :
 return
 14
+case
+'
+EDNS_ERROR
+'
+:
+return
+15
 case
 '
 DEVICEID
