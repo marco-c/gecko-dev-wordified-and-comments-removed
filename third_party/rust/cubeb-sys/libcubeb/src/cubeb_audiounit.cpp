ecc998159c86e1dc416dc2826e1f61db95bb5850
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 19:19:58 +0000

    Bug 1670917 - mach vendor rust. r=cubeb-reviewers,chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D93368
    
    UltraBlame original commit: 7947997fb69accf4d466a8196a2bc9875b9a43b4

--- third_party/rust/cubeb-sys/libcubeb/src/cubeb_audiounit.cpp
+++ third_party/rust/cubeb-sys/libcubeb/src/cubeb_audiounit.cpp
@@ -17363,6 +17363,7 @@ audiounit_stream_get_position
 audiounit_stream_get_latency
 NULL
 audiounit_stream_set_volume
+NULL
 audiounit_stream_get_current_device
 audiounit_stream_device_destroy
 audiounit_stream_register_device_changed_callback
