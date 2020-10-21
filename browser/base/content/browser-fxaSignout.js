bdb607a5d882e4746066219396488ae89cb7dbb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-10-21 18:26:29 +0000

    Bug 1668244 - Vertically aligned the sign out icon r=Itiel,markh
    Differential Revision: https://phabricator.services.mozilla.com/D92283
    
    UltraBlame original commit: 154846e026f2849441347c30eea129d8e0179550

--- browser/base/content/browser-fxaSignout.js
+++ browser/base/content/browser-fxaSignout.js
@@ -43,7 +43,7 @@ document
 getElementById
 (
 "
-fxaDeleteLocalDataContent
+shouldDeleteLocalData
 "
 )
 .
@@ -56,7 +56,7 @@ document
 getElementById
 (
 "
-fxaSignoutBody
+fxaSignoutDetail
 "
 )
 .
