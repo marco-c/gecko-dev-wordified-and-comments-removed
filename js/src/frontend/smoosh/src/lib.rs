ef4866a92ded924271cb0d524c07a1e3bbc90273
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:13:50 +0000

    Bug 1678170 - Update opcode for bug 1673553. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D99073
    
    UltraBlame original commit: 1405ea3633c0730ca4b848acf0dfb239d7c959ca

--- js/src/frontend/smoosh/src/lib.rs
+++ js/src/frontend/smoosh/src/lib.rs
@@ -1625,10 +1625,6 @@ fun_length
 :
 u16
 pub
-num_bytecode_type_sets
-:
-u32
-pub
 bytecode
 :
 CVec
@@ -2448,13 +2444,6 @@ script_data
 fun_length
 ;
 let
-num_bytecode_type_sets
-=
-script_data
-.
-num_bytecode_type_sets
-;
-let
 bytecode
 =
 CVec
@@ -2508,7 +2497,6 @@ nslots
 body_scope_index
 num_ic_entries
 fun_length
-num_bytecode_type_sets
 bytecode
 scope_notes
 }
