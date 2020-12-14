06017903eae9d270e51cf50ed680904dbf61bf3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-14 12:17:02 +0000

    Bug 1676346 - Add missing include directives etc. r=andi,necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D99323
    
    UltraBlame original commit: 7eac42a996517dad2b6aa50fcd9548bbf635d560

--- dom/media/autoplay/GVAutoplayPermissionRequest.h
+++ dom/media/autoplay/GVAutoplayPermissionRequest.h
@@ -18,6 +18,9 @@ nsContentPermissionHelper
 .
 h
 "
+class
+nsGlobalWindowInner
+;
 namespace
 mozilla
 {
--- dom/media/mp4/MP4Decoder.cpp
+++ dom/media/mp4/MP4Decoder.cpp
@@ -36,6 +36,13 @@ h
 #
 include
 "
+PlatformDecoderModule
+.
+h
+"
+#
+include
+"
 VideoUtils
 .
 h
