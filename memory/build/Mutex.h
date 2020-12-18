29ef2e489fd4f0fa1cdcf72a3a4741a31f5e7400
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-12-18 10:06:33 +0000

    Bug 1670885 - Replace deprecated NSSpinLocks with os_unfair_locks in the memory allocator r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D99280
    
    UltraBlame original commit: f12f5989419ac645e641036d9c3851170362fdef

--- memory/build/Mutex.h
+++ memory/build/Mutex.h
@@ -26,9 +26,9 @@ XP_DARWIN
 #
 include
 <
-libkern
+os
 /
-OSAtomic
+lock
 .
 h
 >
@@ -70,7 +70,7 @@ defined
 (
 XP_DARWIN
 )
-OSSpinLock
+os_unfair_lock
 mMutex
 ;
 #
@@ -115,7 +115,7 @@ XP_DARWIN
 )
 mMutex
 =
-OS_SPINLOCK_INIT
+OS_UNFAIR_LOCK_INIT
 ;
 #
 elif
@@ -235,7 +235,7 @@ defined
 (
 XP_DARWIN
 )
-OSSpinLockLock
+os_unfair_lock_lock
 (
 &
 mMutex
@@ -276,7 +276,7 @@ defined
 (
 XP_DARWIN
 )
-OSSpinLockUnlock
+os_unfair_lock_unlock
 (
 &
 mMutex
