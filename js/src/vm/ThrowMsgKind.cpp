448709d86e99b5f633652378c8372934b5c75ef1
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-01 00:43:19 +0000

    Bug 1683727 - Improve error messages when private fields are not found. r=mgaudet
    Previously we always said "Trying to read" even when the error occurred while
    trying to write.
    
    The use of "object" in the new messages is inaccurate when the user is trying
    to set a private field on a primitive value, but this is the best I can do
    without adding fields to the error message--which would have to affect
    bytecode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D100258
    
    UltraBlame original commit: dd2cf6eff8e8ab6c465b27247dda3b0efa7d585a

--- js/src/vm/ThrowMsgKind.cpp
+++ js/src/vm/ThrowMsgKind.cpp
@@ -84,6 +84,9 @@ ThrowMsgKind
 :
 MissingPrivateOnGet
 :
+return
+JSMSG_GET_MISSING_PRIVATE
+;
 case
 ThrowMsgKind
 :
@@ -91,7 +94,7 @@ ThrowMsgKind
 MissingPrivateOnSet
 :
 return
-JSMSG_UNDECLARED_PRIVATE
+JSMSG_SET_MISSING_PRIVATE
 ;
 }
 MOZ_CRASH
