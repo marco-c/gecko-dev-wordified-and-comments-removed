ace09435ffbc35d8f1973a98f4c7e9e76f45f66f
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-11 20:40:06 +0000

    Bug 1686031 - Rename GetDirectoryMetadataWithOriginMetadata2 to LoadFullOriginMetadata and GetDirectoryMetadataWithOriginMetadata2WithRestore to LoadFullOriginMetadataWithRestore; r=dom-storage-reviewers,sg
    Differential Revision: https://phabricator.services.mozilla.com/D107607
    
    UltraBlame original commit: f5f020daba36d957a2a3fa106c1ad1768b7df1a3

--- dom/quota/ActorsParent.cpp
+++ dom/quota/ActorsParent.cpp
@@ -17817,7 +17817,7 @@ const
 auto
 &
 metadata
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 directory
 )
@@ -19707,7 +19707,7 @@ nsresult
 QuotaManager
 :
 :
-GetDirectoryMetadataWithOriginMetadata2
+LoadFullOriginMetadata
 (
 nsIFile
 *
@@ -19933,7 +19933,7 @@ nsresult
 QuotaManager
 :
 :
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 nsIFile
 *
@@ -20021,7 +20021,7 @@ nsresult
 {
 QM_TRY_RETURN
 (
-GetDirectoryMetadataWithOriginMetadata2
+LoadFullOriginMetadata
 (
 aDirectory
 persistenceType
@@ -20065,7 +20065,7 @@ aDirectory
 ;
 QM_TRY_RETURN
 (
-GetDirectoryMetadataWithOriginMetadata2
+LoadFullOriginMetadata
 (
 aDirectory
 persistenceType
@@ -20318,7 +20318,7 @@ QM_TRY_UNWRAP
 (
 auto
 metadata
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 childDirectory
 )
@@ -25572,7 +25572,7 @@ const
 auto
 &
 metadata
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 directory
 )
@@ -35955,7 +35955,7 @@ auto
 metadata
 aQuotaManager
 .
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 &
 aOriginDir
@@ -38273,7 +38273,7 @@ auto
 metadata
 aQuotaManager
 .
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 file
 )
@@ -39525,7 +39525,7 @@ auto
 metadata
 aQuotaManager
 .
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 directory
 )
@@ -39805,7 +39805,7 @@ auto
 metadata
 aQuotaManager
 .
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 directory
 )
@@ -40286,7 +40286,7 @@ auto
 metadata
 aQuotaManager
 .
-GetDirectoryMetadataWithOriginMetadata2WithRestore
+LoadFullOriginMetadataWithRestore
 (
 &
 aOriginDir
