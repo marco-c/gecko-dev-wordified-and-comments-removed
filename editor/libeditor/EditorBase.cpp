c51ab059ee088be7d65c280d1e51474d0ade50e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-15 18:04:54 +0000

    Bug 1692673 - Avoid enabling Cut/Copy menu commands in text editor context when there's no selection, unless a listener is present. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D107480
    
    UltraBlame original commit: 8148b9e5dfba1f3de1086f38c0165fd87facc56c

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -5902,8 +5902,11 @@ NS_ERROR_INVALID_ARG
 *
 aCanCut
 =
+MOZ_KnownLive
+(
 AsTextEditor
 (
+)
 )
 -
 >
@@ -5954,8 +5957,11 @@ NS_ERROR_INVALID_ARG
 *
 aCanCopy
 =
+MOZ_KnownLive
+(
 AsTextEditor
 (
+)
 )
 -
 >
