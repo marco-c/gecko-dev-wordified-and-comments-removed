616915f3c5bf8af4ab1f579f8abef7f64e4f687a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:55:36 +0000

    Bug 1670033 - Part 2: Implement "calendar" support for the "Intl Enumeration API" proposal. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D116010
    
    UltraBlame original commit: e5214a5c19ce7f912399a1a7cd72a61897da217d

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -15755,6 +15755,15 @@ intl_SelectPluralRuleRange
 JS_FN
 (
 "
+intl_SupportedValuesOf
+"
+intl_SupportedValuesOf
+1
+0
+)
+JS_FN
+(
+"
 intl_TryValidateAndCanonicalizeLanguageTag
 "
 intl_TryValidateAndCanonicalizeLanguageTag
