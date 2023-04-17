83a31764fd5bfb97308e0b4c211c6a488c7ed49d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:04:16 +0000

    Bug 739096 - Expose cairo_font_options_{get,set}_lcd_filter as public API. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D112585
    
    UltraBlame original commit: c26360666d39186bf40ee86f33dfe64ab8863303

--- gfx/cairo/cairo/src/cairo-font-options.c
+++ gfx/cairo/cairo/src/cairo-font-options.c
@@ -1090,7 +1090,7 @@ subpixel_order
 ;
 }
 void
-_cairo_font_options_set_lcd_filter
+cairo_font_options_set_lcd_filter
 (
 cairo_font_options_t
 *
@@ -1117,7 +1117,7 @@ lcd_filter
 ;
 }
 cairo_lcd_filter_t
-_cairo_font_options_get_lcd_filter
+cairo_font_options_get_lcd_filter
 (
 const
 cairo_font_options_t
