2008d8730e962eb899ee3a722eaabdfe24416c61
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 20:25:01 +0000

    Bug 1733374 - land NSS de3db3a55aef UPGRADE_NSS_RELEASE, r=bbeurdouche
    Differential Revision: https://phabricator.services.mozilla.com/D128763
    
    UltraBlame original commit: ad9449ed493ecc944d5d0291de23bf5e707788b0

--- security/nss/lib/softoken/lowpbe.c
+++ security/nss/lib/softoken/lowpbe.c
@@ -2950,7 +2950,7 @@ KDFCacheItem
 #
 define
 KDF2_CACHE_COUNT
-2
+3
 static
 struct
 {
--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -22,7 +22,8 @@ NSSUTIL_VERSION
 "
 3
 .
-71
+72
+Beta
 "
 #
 define
@@ -31,7 +32,7 @@ NSSUTIL_VMAJOR
 #
 define
 NSSUTIL_VMINOR
-71
+72
 #
 define
 NSSUTIL_VPATCH
@@ -43,7 +44,7 @@ NSSUTIL_VBUILD
 #
 define
 NSSUTIL_BETA
-PR_FALSE
+PR_TRUE
 SEC_BEGIN_PROTOS
 extern
 const
