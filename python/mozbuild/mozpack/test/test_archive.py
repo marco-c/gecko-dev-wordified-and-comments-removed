e1b8b917a4b072f968ddc86485a2f79f21ee2feb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 18:25:32 +0000

    Bug 1733705 - mozbuild test migrate from r7 -> r8. r=releng-reviewers,gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D127331
    
    UltraBlame original commit: 92aec5b01cfd6d4c0121397ad1654527d6d5940e

--- python/mozbuild/mozpack/test/test_archive.py
+++ python/mozbuild/mozpack/test/test_archive.py
@@ -1170,9 +1170,6 @@ filename
 "
 foobar
 "
-compresslevel
-=
-1
 )
             
 self
@@ -1184,7 +1181,7 @@ file_hash
 gp
 )
 "
-1cc8b96f0262350977c2e9d61f40a1fa76f35c52
+721e00083c17d16df2edbddf40136298c06d0c49
 "
 )
             
