9987f8f2fddeae7b68ee11956f39123b75353701
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 15:43:40 +0000

    Bug 1717051: Validate if optional dependencies are out-of-date r=ahal
    Though it's acceptable for an optional dependency to not be installed,
    it *is* a problem if it's installed to the wrong version.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122886
    
    UltraBlame original commit: 312e5766e25d76f1a64233a8a9b5aa8bf54ab8b4

--- python/mozbuild/mozbuild/virtualenv.py
+++ python/mozbuild/mozbuild/virtualenv.py
@@ -1199,9 +1199,18 @@ return
 False
         
 if
+(
+            
 env_requirements
 .
 pypi_requirements
+            
+or
+env_requirements
+.
+pypi_optional_requirements
+        
+)
 :
             
 pip_json
@@ -1295,6 +1304,42 @@ requirement
 version
                 
 )
+:
+                    
+return
+False
+            
+for
+requirement
+in
+env_requirements
+.
+pypi_optional_requirements
+:
+                
+installed_version
+=
+installed_packages
+.
+get
+(
+                    
+requirement
+.
+package_name
+None
+                
+)
+                
+if
+installed_version
+and
+installed_version
+!
+=
+requirement
+.
+version
 :
                     
 return
