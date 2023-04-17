c26bd20b27a4ccc2b0426bcdbc0148be0ef6893c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 13:41:09 +0000

    Bug 1673870 - [devtools] Fix browser_resources_getAllResources.js intermittent. r=ladybenko.
    The test was failing because we were getting cached messages
    from a worker that were emitted late.
    To fix this, we create a dedicated document with workers
    only when we need them and remove workers from fission_document.html.
    This allows us to cleanup a few tests that were already working
    around the worker logs issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122326
    
    UltraBlame original commit: 3cefcc8f7eed235ca591b69bef5228f8652cce47

--- devtools/shared/commands/resource/tests/browser_resources_console_messages_workers.js
+++ devtools/shared/commands/resource/tests/browser_resources_console_messages_workers.js
@@ -9,7 +9,7 @@ FISSION_TEST_URL
 URL_ROOT_SSL
 +
 "
-fission_document
+fission_document_workers
 .
 html
 "
@@ -29,7 +29,7 @@ IFRAME_FILE
 {
 URL_ROOT_ORG_SSL
 }
-fission_iframe
+fission_iframe_workers
 .
 html
 ;
