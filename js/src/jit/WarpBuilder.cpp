cbfeb02a775f25b3894505a87390529fbc69000f
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 14:53:53 +0000

    Bug 1724031 - Part 13: Fix race writing recover info r=tcampbell
    This fixes an intermittent TSAN failure due to accessing JSFunction off main thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123729
    
    UltraBlame original commit: 6f1eb9f44fd881e15d12d13ac3b53f22fd92a656

--- js/src/jit/WarpBuilder.cpp
+++ js/src/jit/WarpBuilder.cpp
@@ -1059,7 +1059,7 @@ info
 (
 )
 .
-funMaybeLazy
+hasFunMaybeLazy
 (
 )
 )
@@ -2500,7 +2500,7 @@ info
 (
 )
 .
-funMaybeLazy
+hasFunMaybeLazy
 (
 )
 )
@@ -10282,7 +10282,7 @@ info
 (
 )
 .
-funMaybeLazy
+hasFunMaybeLazy
 (
 )
 )
@@ -14903,7 +14903,7 @@ info
 (
 )
 .
-funMaybeLazy
+hasFunMaybeLazy
 (
 )
 )
