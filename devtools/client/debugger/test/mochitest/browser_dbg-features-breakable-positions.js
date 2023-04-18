411ed8fc583f6a162d9fe66a7c29cac94c469acc
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:59:56 +0000

    Bug 1764055 - [devtools] Add tests for breakpoints in original source whose generated source content changes on reload r=ochameau
    Updating the bug id
    
    Differential Revision: https://phabricator.services.mozilla.com/D142047
    
    UltraBlame original commit: 83c8bb7087f50dec500df7ddd6e353b13483b964

--- devtools/client/debugger/test/mochitest/browser_dbg-features-breakable-positions.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-features-breakable-positions.js
@@ -94,7 +94,7 @@ index
 .
 html
 "
-59
+60
 [
 {
 line
@@ -524,12 +524,12 @@ index
 .
 html
 "
-30
+31
 [
 {
 line
 :
-22
+25
 columns
 :
 [
@@ -540,7 +540,7 @@ columns
 {
 line
 :
-24
+27
 columns
 :
 [
