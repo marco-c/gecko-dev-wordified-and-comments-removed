bea19b85a97c4219b2e6e01933397f808964d716
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 17:53:30 +0000

    Bug 1765949 - Change the search params for TCP rollout. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D144397
    
    UltraBlame original commit: 4c742ffdfbbfedf95fe26ba146d1e5db89009f7a

--- browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
+++ browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
@@ -154,7 +154,7 @@ param
 google_channel_us
 "
 "
-xus7
+tus7
 "
 ]
 [
@@ -168,7 +168,7 @@ param
 google_channel_row
 "
 "
-xrow7
+trow7
 "
 ]
 [
@@ -182,7 +182,7 @@ param
 bing_ptag
 "
 "
-MOZZ0000000032
+MOZZ0000000031
 "
 ]
 ]
