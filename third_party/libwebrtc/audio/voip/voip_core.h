1a654d166d8828f012daf626a81169a76f1572d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 11:57:18 +0000

    Bug 1766646 - Vendor libwebrtc from 37827c9058
    Upstream commit: https://webrtc.googlesource.com/src/+/37827c90582176943c87301b1120d50d0dfb1e58
        Revert "ChannelStatistics used for RTP stats in VoipStatistics."
    
        This reverts commit 444e04be6988fbdcc039d775481ac22481ff9ff4.
    
        Reason for revert: breaks downstream project
    
        Original change's description:
        > ChannelStatistics used for RTP stats in VoipStatistics.
        >
        > - Added local and remote RTP statistics query API.
        > - Change includes simplifying remote SSRC change handling
        >   via received RTP and RTCP packets.
        >
        > Bug: webrtc:11989
        > Change-Id: Ia3ee62c1191baaedc67e033ea3c661d8c9301abc
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/199060
        > Reviewed-by: Harald Alvestrand <htawebrtc.org>
        > Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        > Reviewed-by: Sam Zackrisson <sazawebrtc.org>
        > Commit-Queue: Tim Na <natimwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#32954}
    
        TBR=mbonadeiwebrtc.org,sazawebrtc.org,htawebrtc.org,natimwebrtc.org
    
        Change-Id: I5ce6a698c1216c7d56e32fce3308c16daac852f4
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:11989
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/201460
        Reviewed-by: Alex Loiko <aleloigoogle.com>
        Commit-Queue: Alex Loiko <aleloigoogle.com>
        Cr-Commit-Position: refs/heads/master{#32956}
    
    UltraBlame original commit: 4fac60ce7ffc0a9295e4448d37484574430b8bf5

--- third_party/libwebrtc/audio/voip/voip_core.h
+++ third_party/libwebrtc/audio/voip/voip_core.h
@@ -546,17 +546,6 @@ ingress_stats
 override
 ;
 VoipResult
-GetChannelStatistics
-(
-ChannelId
-channe_id
-ChannelStatistics
-&
-channel_stats
-)
-override
-;
-VoipResult
 SetInputMuted
 (
 ChannelId
