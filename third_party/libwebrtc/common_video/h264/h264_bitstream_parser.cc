03d2cccf122b0d23c4d6fa391478c4113b42963c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 12:42:22 +0000

    Bug 1766646 - Vendor libwebrtc from 48a4d33719
    Upstream commit: https://webrtc.googlesource.com/src/+/48a4d33719390b7bcaf8445a1581a00825f67bfb
        Revert "Remove Invoke from VideoChannel::FillBitrateInfo."
    
        This reverts commit 1a1795768e1bdb65054ebe15aa238c6edc78dd14.
    
        Reason for revert: Speculative revert (breaks downstream project).
    
        Original change's description:
        > Remove Invoke from VideoChannel::FillBitrateInfo.
        >
        > The method is relied upon by StatsCollector where it was called from the
        > signaling thread in a loop. Now there's at most one invoke (not N).
        >
        > Uncommenting thread checks and removing TODOs in SendStatisticsProxy,
        > VideoSendStream. Updating all related tests that fetched stats from
        > the wrong context.
        >
        > Bug: webrtc:12726
        > Change-Id: Ia7db1afd7e103ec4f9816f5647203c4e2495586e
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/216688
        > Commit-Queue: Tommi <tommiwebrtc.org>
        > Reviewed-by: Niels Moller <nissewebrtc.org>
        > Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#33894}
    
        TBR=ilnikwebrtc.org,nissewebrtc.org,tommiwebrtc.org,webrtc-scopedluci-project-accounts.iam.gserviceaccount.com
    
        Change-Id: I2520957cdb33492d187f04320c7416788fd0f820
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:12726
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/217240
        Reviewed-by: Mirko Bonadei <mbonadeiwebrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#33898}
    
    UltraBlame original commit: 16a1f1cac2b1a2877fbd3aa5a42ce6d8aa5bb57c

--- third_party/libwebrtc/common_video/h264/h264_bitstream_parser.cc
+++ third_party/libwebrtc/common_video/h264/h264_bitstream_parser.cc
@@ -1350,7 +1350,7 @@ if
 !
 sps_
 )
-RTC_DLOG
+RTC_LOG
 (
 LS_WARNING
 )
@@ -1406,7 +1406,7 @@ if
 !
 pps_
 )
-RTC_DLOG
+RTC_LOG
 (
 LS_WARNING
 )
@@ -1465,7 +1465,7 @@ res
 =
 kOk
 )
-RTC_DLOG
+RTC_LOG
 (
 LS_INFO
 )
