3e72bdf2c739b9b3f9104306e2a6a59f4662e51d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 01:25:23 +0000

    Bug 1776109 - Part 3: Hide caches/indexedDB/serviceWorker in PBMode r=asuth,twisniewski
    Differential Revision: https://phabricator.services.mozilla.com/D150064
    
    UltraBlame original commit: b6222b68a40369bbd7cbb5d5f44f782d9ea6027e

--- dom/serviceworkers/ServiceWorker.cpp
+++ dom/serviceworkers/ServiceWorker.cpp
@@ -300,6 +300,16 @@ aGlobal
 ;
 if
 (
+StaticPrefs
+:
+:
+dom_serviceWorkers_hide_in_pbmode_enabled
+(
+)
+)
+{
+if
+(
 const
 nsCOMPtr
 <
@@ -335,6 +345,7 @@ ePrivateBrowsing
 return
 false
 ;
+}
 }
 }
 if
