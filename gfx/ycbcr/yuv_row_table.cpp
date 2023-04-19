2fec26a4f9fd87a80778ff98ce34ebc11a22b66e
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 01:09:12 +0000

    Bug 1698699 - P0 - remove use of deprecated libyuv int types;r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D150300
    
    UltraBlame original commit: 548ed1a0458ff3ae6f11dfd453a14c7461996cf6

--- gfx/ycbcr/yuv_row_table.cpp
+++ gfx/ycbcr/yuv_row_table.cpp
@@ -20,7 +20,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 1
@@ -42,7 +42,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 1
@@ -64,7 +64,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 1
@@ -97,7 +97,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 2
@@ -119,7 +119,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 -
@@ -144,7 +144,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 256
@@ -167,7 +167,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 -
@@ -190,7 +190,7 @@ i
 \
 static_cast
 <
-int16
+int16_t
 >
 (
 1
@@ -216,7 +216,7 @@ i
 SIMD_ALIGNED
 (
 const
-int16
+int16_t
 kCoefficientsRgbY
 [
 256
