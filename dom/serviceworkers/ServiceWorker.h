5f9a93eeac3198236f620b1afd0f615dd38266ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 01:24:38 +0000

    Bug 1776109 - Part 2: Add ServiceWorkersEnabled() for service worker interfaces exposure r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D150441
    
    UltraBlame original commit: 1cdbbad1d99662431ef36fc8a1c59458e1fea8ae

--- dom/serviceworkers/ServiceWorker.h
+++ dom/serviceworkers/ServiceWorker.h
@@ -93,6 +93,17 @@ NS_DOM_SERVICEWORKER_IID
 \
 }
 bool
+ServiceWorkersEnabled
+(
+JSContext
+*
+aCx
+JSObject
+*
+aGlobal
+)
+;
+bool
 ServiceWorkerVisible
 (
 JSContext
@@ -100,7 +111,7 @@ JSContext
 aCx
 JSObject
 *
-aObj
+aGlobal
 )
 ;
 class
