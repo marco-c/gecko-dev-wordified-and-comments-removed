bfd5c9f11f85e6add640748d836f1cb199018dd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 00:37:26 +0000

    Bug 1758055 - Establish file system content and parent process IPC. r=janv,dom-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D140486
    
    UltraBlame original commit: 8e304bf129b791c6e4685d14bc49e659f0202338

--- ipc/glue/BackgroundParentImpl.h
+++ ipc/glue/BackgroundParentImpl.h
@@ -108,6 +108,15 @@ override
 ;
 already_AddRefed
 <
+PBackgroundFileSystemParent
+>
+AllocPBackgroundFileSystemParent
+(
+)
+override
+;
+already_AddRefed
+<
 PBackgroundIDBFactoryParent
 >
 AllocPBackgroundIDBFactoryParent
