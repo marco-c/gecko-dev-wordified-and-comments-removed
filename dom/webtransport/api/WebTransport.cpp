3ef4b57c17e2d0ec7083a31db177c0c2d74950ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 23:50:22 +0000

    Bug 1820434: Isolate AsyncInputStream from InputToReadableStream for workers r=asuth
    nsIAsyncInputStreams keep a reference to the callback object, causing
    possible leaks which will result in assertions in workers.  Also use a
    StrongWorkerRef to ensure the worker remains around until the
    OnInputStreamReady callback occurs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171810
    
    UltraBlame original commit: 39f22b0883018de7895d99f7aa9d6ee86fafbc8c

--- dom/webtransport/api/WebTransport.cpp
+++ dom/webtransport/api/WebTransport.cpp
@@ -255,6 +255,14 @@ mGlobal
 )
 NS_IMPL_CYCLE_COLLECTION_UNLINK
 (
+mUnidirectionalStreams
+)
+NS_IMPL_CYCLE_COLLECTION_UNLINK
+(
+mBidirectionalStreams
+)
+NS_IMPL_CYCLE_COLLECTION_UNLINK
+(
 mIncomingUnidirectionalStreams
 )
 NS_IMPL_CYCLE_COLLECTION_UNLINK
