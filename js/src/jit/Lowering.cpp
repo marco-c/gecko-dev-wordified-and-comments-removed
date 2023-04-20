4ec2332a6ea44785450003e7909278b852b63b81
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 18:17:08 +0000

    Bug 1814746 - Use useFixedAtStart in MegamorphicSetElement lowering r=jandem
    The previous patch was just incomplete, turns out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168749
    
    UltraBlame original commit: dbe95581bb1540d718ae2906c9be6836404c4445

--- js/src/jit/Lowering.cpp
+++ js/src/jit/Lowering.cpp
@@ -36729,7 +36729,7 @@ alloc
 )
 LMegamorphicSetElement
 (
-useRegisterAtStart
+useFixedAtStart
 (
 ins
 -
@@ -36737,6 +36737,7 @@ ins
 object
 (
 )
+CallTempReg0
 )
 useBoxFixedAtStart
 (
@@ -36762,7 +36763,7 @@ CallTempReg4
 )
 tempFixed
 (
-CallTempReg0
+CallTempReg5
 )
 )
 ;
