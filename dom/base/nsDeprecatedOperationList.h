6c2801e80d12e6f403c0bf8cff8ec25212414853
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 05:42:51 +0000

    Backed out changeset 183400ded7d4 (bug 1294360) for bustages on CanvasRenderingContext2D.cpp. CLOSED TREE
    UltraBlame original commit: 3b21fbb7ca8df32cf62c876be21072f4156ec7ab

--- dom/base/nsDeprecatedOperationList.h
+++ dom/base/nsDeprecatedOperationList.h
@@ -200,5 +200,13 @@ IDBMutableFileOpen
 )
 DEPRECATED_OPERATION
 (
+MozCurrentTransform
+)
+DEPRECATED_OPERATION
+(
+MozCurrentTransformInverse
+)
+DEPRECATED_OPERATION
+(
 MozPreservesPitchDeprecatedPrefix
 )
