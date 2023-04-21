8597e155dd10db3ea84217c984b2f52f8d3b5eb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 10:25:04 +0000

    Bug 1821512 - Update fuzz for Skia update. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D173326
    
    UltraBlame original commit: 698104c7a5d17acbb888ea0bdc1419ce7c00a939

--- browser/components/screenshots/tests/browser/browser_screenshots_test_full_page.js
+++ browser/components/screenshots/tests/browser/browser_screenshots_test_full_page.js
@@ -334,7 +334,7 @@ Assert
 .
 equal
 (
-55
+56
 result
 .
 color
@@ -354,7 +354,7 @@ Assert
 .
 equal
 (
-155
+156
 result
 .
 color
@@ -374,7 +374,7 @@ Assert
 .
 equal
 (
-155
+156
 result
 .
 color
@@ -394,7 +394,7 @@ Assert
 .
 equal
 (
-105
+106
 result
 .
 color
@@ -414,7 +414,7 @@ Assert
 .
 equal
 (
-55
+56
 result
 .
 color
@@ -434,7 +434,7 @@ Assert
 .
 equal
 (
-105
+106
 result
 .
 color
@@ -454,7 +454,7 @@ Assert
 .
 equal
 (
-52
+53
 result
 .
 color
@@ -474,7 +474,7 @@ Assert
 .
 equal
 (
-127
+128
 result
 .
 color
@@ -494,7 +494,7 @@ Assert
 .
 equal
 (
-152
+153
 result
 .
 color
@@ -878,7 +878,7 @@ Assert
 .
 equal
 (
-55
+56
 result
 .
 color
@@ -898,7 +898,7 @@ Assert
 .
 equal
 (
-155
+156
 result
 .
 color
@@ -918,7 +918,7 @@ Assert
 .
 equal
 (
-155
+156
 result
 .
 color
@@ -938,7 +938,7 @@ Assert
 .
 equal
 (
-105
+106
 result
 .
 color
@@ -958,7 +958,7 @@ Assert
 .
 equal
 (
-55
+56
 result
 .
 color
@@ -978,7 +978,7 @@ Assert
 .
 equal
 (
-105
+106
 result
 .
 color
@@ -998,7 +998,7 @@ Assert
 .
 equal
 (
-52
+53
 result
 .
 color
@@ -1018,7 +1018,7 @@ Assert
 .
 equal
 (
-127
+128
 result
 .
 color
@@ -1038,7 +1038,7 @@ Assert
 .
 equal
 (
-152
+153
 result
 .
 color
