098a3a7e216a9af7276ce21ee1bd06f9c025fbbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-01 00:25:38 +0000

    Backed out 2 changesets (bug 1828248) for causing wr bustages.
    Backed out changeset d6cb8770e9ba (bug 1828248)
    Backed out changeset 85d5bfd8f946 (bug 1828248)
    
    UltraBlame original commit: ffe23d9c2596910146f07de601a9df42aa62aaf6

--- third_party/rust/gleam/build.rs
+++ third_party/rust/gleam/build.rs
@@ -196,9 +196,6 @@ GL_ARB_texture_rectangle
 GL_ARB_texture_storage
 "
 "
-GL_ARB_vertex_attrib_binding
-"
-"
 GL_EXT_debug_marker
 "
 "
@@ -325,9 +322,6 @@ GL_KHR_blend_equation_advanced_coherent
 "
 "
 GL_ANGLE_copy_texture_3d
-"
-"
-GL_QCOM_tiled_rendering
 "
 ]
 ;
