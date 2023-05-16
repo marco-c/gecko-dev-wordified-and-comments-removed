ec183abb0f89109889c810ea0e65574f0bf487a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 01:54:39 +0000

    Bug 1828517 - Vendor libwebrtc from 5671c64103
    Upstream commit: https://webrtc.googlesource.com/src/+/5671c6410395d6bc3f6041cc15d16ca256688c8e
        Stop overriding extensions in rampup tests
        Instead, ensure extensions are registered so that both transport and send streams are aware.
    
        Bug: webrtc:7135,webrtc:14795,b/266658815
        Change-Id: I7710113893e2c5e23c1365de6aa3b761e3408308
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/291333
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Per Kjellander <perkjwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#39193}
    
    UltraBlame original commit: d975c6f5dfd95ca1db5b01f5b37ece60876a2a82

--- third_party/libwebrtc/call/rampup_tests.h
+++ third_party/libwebrtc/call/rampup_tests.h
@@ -192,11 +192,6 @@ int
 start_bitrate_bps
 int64_t
 min_run_time_ms
-absl
-:
-:
-string_view
-extension_type
 bool
 rtx
 bool
@@ -527,13 +522,6 @@ test_start_ms_
 int64_t
 ramp_up_finished_ms_
 ;
-const
-std
-:
-:
-string
-extension_type_
-;
 std
 :
 :
@@ -592,11 +580,6 @@ num_flexfec_streams
 unsigned
 int
 start_bitrate_bps
-absl
-:
-:
-string_view
-extension_type
 bool
 rtx
 bool
