e874f74a46f44f862b3b897bb40fc77f69a14f8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-16 00:44:40 +0000

    Bug 1806788 - [wdspec] Fix wrong helper name in bidi pointer_touch.py test r=webdriver-reviewers,jgraham
    Depends on D177174
    
    Differential Revision: https://phabricator.services.mozilla.com/D177080
    
    UltraBlame original commit: fa7dde91f5f95e30031bf6c6b33d7bbeed8f2ad4

--- testing/web-platform/tests/webdriver/tests/bidi/input/perform_actions/pointer_touch.py
+++ testing/web-platform/tests/webdriver/tests/bidi/input/perform_actions/pointer_touch.py
@@ -65,7 +65,7 @@ pointerArea
 center
 =
 await
-get_inview_center
+get_inview_center_bidi
 (
         
 bidi_session
@@ -687,7 +687,7 @@ pointerArea
 center
 =
 await
-get_inview_center
+get_inview_center_bidi
 (
         
 bidi_session
