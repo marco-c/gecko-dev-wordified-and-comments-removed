db9c8a46ed01bd93db4895c06f78e5564b432cce
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-06-16 01:43:08 +0000

    Bug 1833192 [wpt PR 40015] - Fenced frames: Local network access., a=testonly
    Automatic update from web-platform-tests
    Fenced frames: Local network access.
    
    Fenced frames are only allowed in secure context. So the tests are
    all in secure contexts.
    
    1. Subresource fetch:
    
    Fenced frame's IP address space is set to `kPublic` in order to make
    it subject to local network access check.
    
    web_tests/external/wpt/fetch/local-network-access/
    fetch.https.window.js is replicated and replaced iframes with fenced
    frames. All test cases are passing with the same behaviors as iframes.
    
    2. Document fetch:
    
    Fenced frame's document fetch initiator can only be the parent.
    Fenced frames can only be navigated in two ways:
    
    1. Directly by their parent, and never by another frame at a distance
    via `window.location` or `window.open`; in this case the `ClientSecurityState` needs to come from the parent.
    2. By themselves; in this case the `ClientSecurityState` also needs to
    come from its embedder/parent.
    
    The ClientSecurityState of its parent is supplied to the
    NavigationURLLoader.
    
    web_tests/external/wpt/fetch/local-network-access/
    iframe.tentative.https.window.js is replicated and replaced iframes
    with fenced frames. All test cases have the same results as the
    iframe test expectations, except one:
    
    treat-as-public-address to local (same-origin): no preflight
    required
    
    - Iframe: the request is made without preflight. The nested iframe is
    loaded successfully.
    
    - Fenced frame: a preflight is made, and gets blocked. See a. below.
    
    I changed the test expectation for this test only. (PASS for iframe,
    but FAIL for fenced frame)
    
    Here are some noteworthy things we observed for document fetch. The
    following only applies to embedder-initiated navigations (i.e., the
    initial navigation of the frame):
    a. Fenced frame's document fetch's preflight request is always sent
    with `Origin: null`. This applies to embedder-initiated navigations
    (i.e., the initial navigation of the frame). I think this affects the
    outcome of Local Network Access check algorithm.
    https://source.chromium.org/chromium/chromium/src/+/main:content/browser/fenced_frame/fenced_frame.cc;l=119-126?q=fencedframe::n&ss=chromium%2Fchromium%2Fsrc
    
    A `null` origin implies
    LocalNetworkAccessChecker::is_potentially_trustworthy_same_origin_
    will always be false.
    
    b. For testing purposes, we tried manually overriding the initiator
    origin with a real origin and found that the preflight request still
    failed. This is because the credentials mode of the navigation is
    `'include'`, which prevents `Access-Control-Allow-Origin: '*'` from
    working, which iframes equivalently suffer from: https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/web_tests/external/wpt/fetch/local-network-access/iframe.tentative.https.window-expected.txt?q=%22FAIL%20%22%20f:third_party%2Fblink%2Fweb_tests%2Fexternal%2Fwpt%2Ffetch%2Flocal-network-access%2Fiframe.tentative.https.window-expected.txt.
    
    Bug: 1420626
    Change-Id: I74c97369d235e1725c650bfe87f29372992cb56b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4532557
    Reviewed-by: Titouan Rigoudy <titouanchromium.org>
    Reviewed-by: Alex Moshchuk <alexmoschromium.org>
    Reviewed-by: Weizhong Xia <weizhonggoogle.com>
    Commit-Queue: Xiaochen Zhou <xiaochenzhchromium.org>
    Cr-Commit-Position: refs/heads/main{#1154027}
    
    --
    
    wpt-commits: 7690497bf37a4ecb28cf2cfef5e611c91882e76d
    wpt-pr: 40015
    
    UltraBlame original commit: d6a9f5cb4a283e4adfbeedbeeab51d01e0c7b9d3

--- testing/web-platform/tests/fetch/local-network-access/iframe.tentative.window.js
+++ testing/web-platform/tests/fetch/local-network-access/iframe.tentative.window.js
@@ -44,7 +44,7 @@ HTTP_LOCAL
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -90,7 +90,7 @@ HTTP_PRIVATE
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -136,7 +136,7 @@ HTTP_PUBLIC
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -182,7 +182,7 @@ HTTP_LOCAL
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 FAILURE
 }
@@ -226,7 +226,7 @@ HTTP_PRIVATE
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -272,7 +272,7 @@ HTTP_PUBLIC
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -318,7 +318,7 @@ HTTP_LOCAL
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 FAILURE
 }
@@ -362,7 +362,7 @@ HTTP_PRIVATE
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 FAILURE
 }
@@ -406,7 +406,7 @@ HTTP_PUBLIC
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -455,7 +455,7 @@ HTTP_LOCAL
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 FAILURE
 }
@@ -508,7 +508,7 @@ HTTP_PRIVATE
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 FAILURE
 }
@@ -561,7 +561,7 @@ HTTP_PUBLIC
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
@@ -624,7 +624,7 @@ HTTP_LOCAL
 }
 expected
 :
-IframeTestResult
+FrameTestResult
 .
 SUCCESS
 }
