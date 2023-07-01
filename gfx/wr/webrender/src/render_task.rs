d0c7fc730dfe622eacc039b3a73d2a1d495f128d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 00:42:28 +0000

    Backed out changeset 2ac17e8a81a2 (bug 1837902) as requested by dev for causing multiple regressions CLOSED TREE
    UltraBlame original commit: baa510ae8c9f1043f538edd65d87377fe3e9b422

--- gfx/wr/webrender/src/render_task.rs
+++ gfx/wr/webrender/src/render_task.rs
@@ -914,10 +914,6 @@ prim_spatial_node_index
 :
 SpatialNodeIndex
 pub
-raster_spatial_node_index
-:
-SpatialNodeIndex
-pub
 transform_id
 :
 TransformPaletteId
@@ -2632,9 +2628,6 @@ new_prim
 prim_spatial_node_index
 :
 SpatialNodeIndex
-raster_spatial_node_index
-:
-SpatialNodeIndex
 device_pixel_scale
 :
 DevicePixelScale
@@ -2672,7 +2665,6 @@ Prim
 PrimTask
 {
 prim_spatial_node_index
-raster_spatial_node_index
 device_pixel_scale
 content_origin
 prim_address
