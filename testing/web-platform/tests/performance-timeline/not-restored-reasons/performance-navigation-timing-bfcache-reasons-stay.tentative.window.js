2c9f29f56f6f872bb49a7cdeebd8287853c6c302
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-16 03:31:16 +0000

    Bug 1848583 [wpt PR 41434] - [bfcache] Introduce NotRestoredReasons interface, a=testonly
    Automatic update from web-platform-tests
    [bfcache] Introduce NotRestoredReasons interface
    
    This CL introduces NotRestoredReasons interface to make the implementation match the spec draft[1].
    This also changes NotRestoredReasons attribute's name from `blocked` to `preventedBackForwardCache` and its return value, thus changing WPT as well.
    
    [1]: https://github.com/whatwg/html/pull/9360
    
    Bug: 1472637
    Change-Id: I4dcecd5a012c36c883933ed3d04c776bfe844f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770594
    Reviewed-by: Kentaro Hara <harakenchromium.org>
    Commit-Queue: Yuzu Saijo <yuzuschromium.org>
    Cr-Commit-Position: refs/heads/main{#1185723}
    
    --
    
    wpt-commits: d4021e194b6a79b59a256ed4efbaab5598bf4185
    wpt-pr: 41434
    
    UltraBlame original commit: ceba96ba9e7a89bd66567451cd62d8904a8a2238

--- testing/web-platform/tests/performance-timeline/not-restored-reasons/performance-navigation-timing-bfcache-reasons-stay.tentative.window.js
+++ testing/web-platform/tests/performance-timeline/not-restored-reasons/performance-navigation-timing-bfcache-reasons-stay.tentative.window.js
@@ -74,7 +74,9 @@ await
 assertNotRestoredReasonsEquals
 (
 rc1
-true
+"
+yes
+"
 rc1_url
 null
 null
@@ -99,7 +101,9 @@ await
 assertNotRestoredReasonsEquals
 (
 rc1
-true
+"
+yes
+"
 rc1_url
 null
 null
