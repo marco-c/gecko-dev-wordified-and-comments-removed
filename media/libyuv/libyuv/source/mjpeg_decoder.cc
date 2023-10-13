624df62882169a39f993a8696e859b615bd88793
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:21:54 +0000

    Bug 1816576 - Apply mozilla patches for libyuv 2bdc210be9eb11ded16bf3ef1f6cadb0d4dcb0c2 r=ng
    Depends on D185940
    
    Differential Revision: https://phabricator.services.mozilla.com/D185941
    
    UltraBlame original commit: f8d3ca1ad1bc8dbfc166f1088ac085ae14697531

--- media/libyuv/libyuv/source/mjpeg_decoder.cc
+++ media/libyuv/libyuv/source/mjpeg_decoder.cc
@@ -310,6 +310,9 @@ error_exit
 &
 ErrorHandler
 ;
+#
+ifndef
+DEBUG_MJPEG
 error_mgr_
 -
 >
@@ -320,6 +323,8 @@ output_message
 &
 OutputHandler
 ;
+#
+endif
 #
 endif
 decompress_struct_
@@ -2155,6 +2160,9 @@ setjmp_buffer
 )
 ;
 }
+#
+ifndef
+DEBUG_MJPEG
 void
 OutputHandler
 (
@@ -2168,6 +2176,8 @@ void
 cinfo
 ;
 }
+#
+endif
 #
 endif
 void
