cd83849a33eee8722cce3c531bc625f42eb07fc1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-10 12:20:51 +0000

    Backed out 5 changesets (bug 1826198, bug 1852223) for causing dt failures in browser_networkobserver_auth_listener.js. CLOSED TREE
    Backed out changeset b8d7a9ec9aa2 (bug 1852223)
    Backed out changeset 0e7a2180e566 (bug 1852223)
    Backed out changeset a7ad7ccbc6a3 (bug 1826198)
    Backed out changeset 081fda2ff03d (bug 1826198)
    Backed out changeset c50d12ac515b (bug 1826198)
    
    UltraBlame original commit: 578787894d0e996af4d743caf71b5501330fb2ab

--- testing/web-platform/tests/webdriver/tests/bidi/network/response_started/response_started.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/response_started/response_started.py
@@ -1229,9 +1229,8 @@ test_www_authenticate
 (
     
 bidi_session
-url
-fetch
 new_tab
+url
 wait_for_event
 setup_network_test
 )
@@ -1290,22 +1289,33 @@ wait_for_event
 RESPONSE_STARTED_EVENT
 )
     
-asyncio
+await
+bidi_session
 .
-ensure_future
+browsing_context
+.
+navigate
 (
-fetch
-(
-auth_url
+        
 context
 =
 new_tab
-method
+[
+"
+context
+"
+]
+        
+url
+=
+auth_url
+        
+wait
 =
 "
-GET
+none
 "
-)
+    
 )
     
 await
