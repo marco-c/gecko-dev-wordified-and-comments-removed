d63aa2488e0258231f2ba394ffe16920bd54bf16
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-16 00:36:06 +0000

    Bug 1862699 [wpt PR 42921] - [wdspec] use subscribe_events instead of session.subscribe, a=testonly
    Automatic update from web-platform-tests
    [wdspec] use subscribe_events instead of session.subscribe (#42921)
    
    subscribe_event properly cleans up by unsubscribing at the end of the
    test.
    --
    
    wpt-commits: 833039958a5bb6093347dc6e1c40fce9aed1eba9
    wpt-pr: 42921
    
    UltraBlame original commit: c928e8d5724bfe681b6b2baede68ddd18fb69d8c

--- testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/console.py
+++ testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/console.py
@@ -144,6 +144,7 @@ test_text_with_argument_variation
 (
     
 bidi_session
+subscribe_events
 top_context
 wait_for_event
 log_argument
@@ -152,11 +153,7 @@ expected_text
 :
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
@@ -311,6 +308,7 @@ test_level
 (
     
 bidi_session
+subscribe_events
 top_context
 wait_for_event
 log_method
@@ -319,11 +317,7 @@ expected_level
 :
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
@@ -422,6 +416,7 @@ def
 test_timestamp
 (
 bidi_session
+subscribe_events
 top_context
 wait_for_event
 current_time
@@ -429,11 +424,7 @@ current_time
 :
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
@@ -581,17 +572,14 @@ def
 test_new_context_with_new_window
 (
 bidi_session
+subscribe_events
 top_context
 wait_for_event
 )
 :
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
@@ -729,17 +717,14 @@ def
 test_new_context_with_refresh
 (
 bidi_session
+subscribe_events
 top_context
 wait_for_event
 )
 :
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
@@ -897,6 +882,8 @@ test_different_contexts
     
 bidi_session
     
+subscribe_events
+    
 top_context
     
 wait_for_event
@@ -984,11 +971,7 @@ children
 ]
     
 await
-bidi_session
-.
-session
-.
-subscribe
+subscribe_events
 (
 events
 =
