c300cbb0df09dda25686a58426fb1dd0ece5a8c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 15:19:46 +0000

    Bug 1857222 [wpt PR 42360] - [WebDriver BiDi] Update tilt tests, a=testonly
    Automatic update from web-platform-tests
    [WebDriver BiDi] Update tilt tests
    
    --
    
    wpt-commits: 50889ef9e56f54f1416b234ee396cb8923fb3f4c
    wpt-pr: 42360
    
    UltraBlame original commit: f26d106de83d07a2193172d44fb31efcfc7c72db

--- testing/web-platform/tests/webdriver/tests/classic/perform_actions/pointer_pen.py
+++ testing/web-platform/tests/webdriver/tests/classic/perform_actions/pointer_pen.py
@@ -493,13 +493,16 @@ pressure
 0
 .
 36
-tilt_x
+altitude_angle
 =
--
-72
-tilt_y
+0
+.
+3
+azimuth_angle
 =
-9
+0
+.
+2419
 twist
 =
 86
@@ -783,7 +786,6 @@ tiltX
 ]
 =
 =
--
 72
     
 assert
@@ -798,7 +800,7 @@ tiltY
 ]
 =
 =
-9
+38
     
 assert
 events
