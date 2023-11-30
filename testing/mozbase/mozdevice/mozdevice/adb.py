3f6ac7656725208d8b24393249294744870ebcf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-11-30 16:34:58 +0000

    Bug 1866829 - Replace obsolete distutils reference by portable alternatives r=ahochheiden
    distutils.dir_util.copy_tree -> shutil.copytree
    distutils.spawn.find_executable -> shutil.which
    
    Also fix a warning about escape sequence in the process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194781
    
    UltraBlame original commit: 2de51d702554d9ecb100f01f2d4eb75837ddf8be

--- testing/mozbase/mozdevice/mozdevice/adb.py
+++ testing/mozbase/mozdevice/mozdevice/adb.py
@@ -25,9 +25,9 @@ time
 import
 traceback
 from
-distutils
+shutil
 import
-dir_util
+copytree
 from
 threading
 import
@@ -20574,9 +20574,7 @@ temp_parent
 remote_name
 )
             
-dir_util
-.
-copy_tree
+copytree
 (
 local
 new_local
@@ -21537,12 +21535,13 @@ if
 copy_required
 :
                 
-dir_util
-.
-copy_tree
+copytree
 (
 local
 original_local
+dirs_exist_ok
+=
+True
 )
                 
 shutil
