9cd773904de42f13d4d06dfa8c028389d268fb75
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-12-07 16:14:51 +0000

    Bug 1862170 - Add chrome driver 120 r=perftest-reviewers,taskgraph-reviewers,bhearsum,sparky,kshampur
    Differential Revision: https://phabricator.services.mozilla.com/D193668
    
    UltraBlame original commit: d7ec576e1046afdd1d78ec401cb31a844b623ce9

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -2776,14 +2776,6 @@ linux64
 -
 chromedriver
 -
-117
-"
-                
-"
-linux64
--
-chromedriver
--
 118
 "
                 
@@ -2793,6 +2785,14 @@ linux64
 chromedriver
 -
 119
+"
+                
+"
+linux64
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2810,14 +2810,6 @@ linux64
 -
 chromedriver
 -
-117
-"
-                
-"
-linux64
--
-chromedriver
--
 118
 "
                 
@@ -2827,6 +2819,14 @@ linux64
 chromedriver
 -
 119
+"
+                
+"
+linux64
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2844,14 +2844,6 @@ mac64
 -
 chromedriver
 -
-117
-"
-                
-"
-mac64
--
-chromedriver
--
 118
 "
                 
@@ -2861,6 +2853,14 @@ mac64
 chromedriver
 -
 119
+"
+                
+"
+mac64
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2880,16 +2880,6 @@ arm
 -
 chromedriver
 -
-117
-"
-                
-"
-mac
--
-arm
--
-chromedriver
--
 118
 "
                 
@@ -2901,6 +2891,16 @@ arm
 chromedriver
 -
 119
+"
+                
+"
+mac
+-
+arm
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2921,14 +2921,6 @@ win32
 -
 chromedriver
 -
-117
-"
-                
-"
-win32
--
-chromedriver
--
 118
 "
                 
@@ -2938,6 +2930,14 @@ win32
 chromedriver
 -
 119
+"
+                
+"
+win32
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2959,14 +2959,6 @@ win32
 -
 chromedriver
 -
-117
-"
-                
-"
-win32
--
-chromedriver
--
 118
 "
                 
@@ -2976,6 +2968,14 @@ win32
 chromedriver
 -
 119
+"
+                
+"
+win32
+-
+chromedriver
+-
+120
 "
             
 ]
@@ -2997,14 +2997,6 @@ win32
 -
 chromedriver
 -
-117
-"
-                
-"
-win32
--
-chromedriver
--
 118
 "
                 
@@ -3014,6 +3006,14 @@ win32
 chromedriver
 -
 119
+"
+                
+"
+win32
+-
+chromedriver
+-
+120
 "
             
 ]
