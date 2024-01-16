e9e85105510efd9b1e228627fb2def3f9d4c1a39
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:52:49 +0000

    Bug 1873782: Remove firefox-bin on macOS. r=mac-reviewers,bradwerth,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D197419
    
    UltraBlame original commit: 3dc6f8a13dc98d21dc5cc535bbad379210938b38

--- toolkit/crashreporter/tools/unit-symbolstore.py
+++ toolkit/crashreporter/tools/unit-symbolstore.py
@@ -4215,6 +4215,43 @@ exe
             
 )
         
+elif
+target_platform
+(
+)
+=
+=
+"
+Darwin
+"
+:
+            
+self
+.
+target_bin
+=
+os
+.
+path
+.
+join
+(
+                
+buildconfig
+.
+topobjdir
+"
+dist
+"
+"
+bin
+"
+"
+firefox
+"
+            
+)
+        
 else
 :
             
