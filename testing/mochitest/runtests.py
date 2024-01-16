c33e1703378016f3ef93a5833544022fa5eea9e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-16 00:25:02 +0000

    Bug 1724089 - Remove browser.tabs.remote.autostart pref r=webdriver-reviewers,perftest-reviewers,geckoview-reviewers,jgraham,extension-reviewers,mossop,sparky,robwu,owlish
    Differential Revision: https://phabricator.services.mozilla.com/D192136
    
    UltraBlame original commit: 399d9781e72b3a4a01e67c7f902f80c9df9da386

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -13931,6 +13931,24 @@ MOZ_HEADLESS
 "
         
 if
+not
+options
+.
+e10s
+:
+            
+browserEnv
+[
+"
+MOZ_FORCE_DISABLE_E10S
+"
+]
+=
+"
+1
+"
+        
+if
 options
 .
 dmd
@@ -16705,20 +16723,6 @@ mochitest
 prefs
 =
 {
-            
-"
-browser
-.
-tabs
-.
-remote
-.
-autostart
-"
-:
-options
-.
-e10s
             
 "
 remote
