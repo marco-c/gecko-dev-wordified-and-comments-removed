d2b218cf898e9825768edc743e9d9f55fc56772b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-01 00:52:53 +0000

    [components] Bug 1583870 - Let taskgraph load tasks for pushes
    UltraBlame original commit: 7c5807d1438cc9ee9949e49374a928ff2554ce44

--- mobile/android/android-components/automation/taskcluster/decision_task.py
+++ mobile/android/android-components/automation/taskcluster/decision_task.py
@@ -787,26 +787,15 @@ builder
 artifacts_info
 )
     
-other_tasks
-=
 all_tasks
-[
-1
-]
-    
-other_tasks
-[
-taskcluster
 .
-slugId
+append
 (
-)
-]
-=
 builder
 .
 craft_ui_tests_task
 (
+)
 )
     
 return
