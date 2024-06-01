488df0dccd1d808d9b237401b89d59d5a056a75a
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-01 00:53:41 +0000

    [components] Bug 1583870 - Migrate lint tasks
    UltraBlame original commit: 457880bbdb569180522e7c43b6b1106c92be0eca

--- mobile/android/android-components/automation/taskcluster/decision_task.py
+++ mobile/android/android-components/automation/taskcluster/decision_task.py
@@ -719,7 +719,7 @@ in
 artifacts_info
 ]
     
-build_tasks
+tasks
 =
 [
         
@@ -740,38 +740,6 @@ module
 )
     
 ]
-    
-other_tasks
-=
-[
-        
-craft_function
-(
-)
-        
-for
-craft_function
-in
-        
-(
-builder
-.
-craft_detekt_task
-builder
-.
-craft_ktlint_task
-builder
-.
-craft_compare_locales_task
-)
-    
-]
-    
-tasks
-=
-build_tasks
-+
-other_tasks
     
 for
 task
