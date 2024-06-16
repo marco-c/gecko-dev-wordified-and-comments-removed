7c53ffdf18cfe8fda0c4eb7eef1705edbde8ddd9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 02:05:16 +0000

    Bug 1863224 - Update sctp to 2952e9324299c5729ba9f03a87924f787868e494 r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D204718
    
    UltraBlame original commit: 66c5ef864ccefedde193ebeef7e95b47ca1e314a

--- netwerk/sctp/src/user_socket.c
+++ netwerk/sctp/src/user_socket.c
@@ -17290,6 +17290,11 @@ USRSCTP_SYSCTL_SET_DEF
 sctp_initial_cwnd
 SCTPCTL_INITIAL_CWND
 )
+USRSCTP_SYSCTL_SET_DEF
+(
+sctp_ootb_with_zero_cksum
+SCTPCTL_OOTB_WITH_ZERO_CKSUM
+)
 #
 ifdef
 SCTP_DEBUG
@@ -17637,6 +17642,10 @@ sctp_buffer_splitting
 USRSCTP_SYSCTL_GET_DEF
 (
 sctp_initial_cwnd
+)
+USRSCTP_SYSCTL_GET_DEF
+(
+sctp_ootb_with_zero_cksum
 )
 #
 ifdef
