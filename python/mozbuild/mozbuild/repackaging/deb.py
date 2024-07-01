3a925f9da2c7f43d9f60d7cacae803a42a7e8a68
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-07-01 01:04:41 +0000

    Bug 1882556 - Use official branding on ESR .deb packages r=releng-reviewers,gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D214466
    
    UltraBlame original commit: 55009dbc142a7f21590f4ad1a3c35ad4e1c02327

--- python/mozbuild/mozbuild/repackaging/deb.py
+++ python/mozbuild/mozbuild/repackaging/deb.py
@@ -2736,6 +2736,29 @@ aurora
 "
 )
     
+elif
+release_type
+.
+startswith
+(
+"
+esr
+"
+)
+:
+        
+return
+branding_fluent_filename_template
+.
+format
+(
+brand
+=
+"
+official
+"
+)
+    
 else
 :
         
