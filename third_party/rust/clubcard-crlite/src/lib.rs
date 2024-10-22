5ce5e2168c6e12a37ba497a38132807c3bf8ef40
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-22 21:34:06 +0000

    Bug 1924577 - vendor clubcard-crlite 0.3.0. r=keeler,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D225552
    
    UltraBlame original commit: 6d0cf0e555c512f4693eb99528c71b9588c10cc9

--- third_party/rust/clubcard-crlite/src/lib.rs
+++ third_party/rust/clubcard-crlite/src/lib.rs
@@ -24,6 +24,7 @@ query
 {
 CRLiteClubcard
 CRLiteCoverage
+CRLiteKey
 CRLiteQuery
 CRLiteStatus
 }
--- third_party/rust/clubcard/src/lib.rs
+++ third_party/rust/clubcard/src/lib.rs
@@ -24,6 +24,7 @@ clubcard
 {
 ApproximateSizeOf
 Clubcard
+ClubcardIndex
 ClubcardIndexEntry
 Membership
 }
