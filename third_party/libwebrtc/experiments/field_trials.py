de46bd6a2ef6f9f60735c202fec97856ef3cfdb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-01 01:40:29 +0000

    Bug 1924098 - Vendor libwebrtc from 43c0cf9cf8
    Upstream commit: https://webrtc.googlesource.com/src/+/43c0cf9cf86d473dc942eaac36138a9f43c5018f
        Support borrowing of underused audio bitrate.
    
        Controlled via added field trial WebRTC-ElasticBitrateAllocation.
    
        Bug: webrtc:350555527
        Change-Id: If57552144bd4a50421d618fd8bdab31d7c4afc35
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/359506
        Reviewed-by: Erik Språng <sprangwebrtc.org>
        Commit-Queue: Dan Tan <dwtangoogle.com>
        Cr-Commit-Position: refs/heads/main{#42834}
    
    UltraBlame original commit: 3c94b71070c82c81661dea1d3c30ba7ecb3ce75c

--- third_party/libwebrtc/experiments/field_trials.py
+++ third_party/libwebrtc/experiments/field_trials.py
@@ -366,6 +366,24 @@ FieldTrial
 '
 WebRTC
 -
+ElasticBitrateAllocation
+'
+               
+350555527
+               
+date
+(
+2025
+3
+1
+)
+)
+    
+FieldTrial
+(
+'
+WebRTC
+-
 EncoderDataDumpDirectory
 '
                
