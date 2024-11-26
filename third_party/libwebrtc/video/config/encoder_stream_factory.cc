38d06331570e302858814e92fee2dbdc5b960e0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 23:38:24 +0000

    Bug 1928110 - Vendor libwebrtc from b6ee51b7a5
    Upstream commit: https://webrtc.googlesource.com/src/+/b6ee51b7a55386f028b3e9195cf222082a1839bc
        Don't restrict max simulcast layers when `requested_resolution` is used.
    
        The code that restricts the maximum number of simulcast layers based on
        resolution is a spec-compliance bug and doesn't make much sense: if the
        app asks for 3 layers it should get 3 layers. Since the app knows the
        size of the track, it could very easily ask for 1 layer when resolution
        is small if that is the behavior it wanted. If the app doesn't ask to
        disable layers, WebRTC shouldn't disable layers on its behalf.
    
        This behavior makes even less sense with this "new" API since the app
        is explicitly controlling the send resolution in absolute terms.
    
        Removing this behavior in the general case is out of scope since it
        would break backwards compatibility, but since `requested_resolution`
        has not been exposed to the web yet and existing usage is small, this
        is an opportunity to fix the compliance bug for this API.
    
        This CL makes the last web platform test for "scaleResolutionDownTo"
        pass.
    
        Bug: chromium:363544347
        Change-Id: Ic6fadf3cad69d3beec4ae03d3d031e8062382ad9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/363100
        Commit-Queue: Henrik Boström <hboswebrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnikwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#43061}
    
    UltraBlame original commit: 5a699c85b8c4ad4134e594c1ae883280baabb004

--- third_party/libwebrtc/video/config/encoder_stream_factory.cc
+++ third_party/libwebrtc/video/config/encoder_stream_factory.cc
@@ -3018,6 +3018,13 @@ encoder_config
 size_t
 max_num_layers
 =
+!
+encoder_config
+.
+HasRequestedResolution
+(
+)
+?
 LimitSimulcastLayerCount
 (
 min_num_layers
@@ -3031,6 +3038,10 @@ encoder_config
 .
 codec_type
 )
+:
+encoder_config
+.
+number_of_streams
 ;
 RTC_DCHECK_LE
 (
