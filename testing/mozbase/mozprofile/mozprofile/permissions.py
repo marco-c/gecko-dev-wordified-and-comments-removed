8b2200015889bd9c7ac1a7e39913dceb5d99f96b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-11-26 22:26:32 +0000

    Backed out changeset 4b08bd7f7168 (bug 1931349) for causing py3 unit test failures CLOSED TREE
    UltraBlame original commit: 0a2fa9d1fc107da1be39df6553950353c2ebcfc3

--- testing/mozbase/mozprofile/mozprofile/permissions.py
+++ testing/mozbase/mozprofile/mozprofile/permissions.py
@@ -1564,26 +1564,6 @@ force_use_https_rr
 True
 )
 )
-                
-user_prefs
-.
-append
-(
-                    
-(
-"
-network
-.
-dns
-.
-https_rr
-.
-check_record_with_cname
-"
-False
-)
-                
-)
             
 else
 :
