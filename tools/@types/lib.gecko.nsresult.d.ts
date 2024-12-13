684aa409ed04696bd26a8b09c11ffc059f4239eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 02:46:28 +0000

    Bug 1928511: Add NS_ERROR_NET_ERROR_RESPONSE to error_list.json and export values of nsIXPCComponents_Results r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D227558
    
    UltraBlame original commit: 0731fc0f120cf2344b9a347ea0d950aec791bd2d

--- tools/@types/lib.gecko.nsresult.d.ts
+++ tools/@types/lib.gecko.nsresult.d.ts
@@ -525,6 +525,10 @@ NS_ERROR_NET_RESET
 :
 0x804b0014
 ;
+NS_ERROR_NET_ERROR_RESPONSE
+:
+0x804b0023
+;
 NS_ERROR_NET_INTERRUPT
 :
 0x804b0047
@@ -830,3 +834,12 @@ NS_ERROR_WDBA_BUILD
 0x80720004
 ;
 }
+type
+nsIXPCComponents_Values
+=
+nsIXPCComponents_Results
+[
+keyof
+nsIXPCComponents_Results
+]
+;
