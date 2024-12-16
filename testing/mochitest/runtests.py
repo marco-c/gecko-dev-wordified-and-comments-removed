c44b8ecc66fc86a5fe9b37043872d53705f05db1
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-16 00:55:35 +0000

    Backed out changeset b07b6247a0fd (bug 1902032) for causing python failure on test_new_config.py
    UltraBlame original commit: 66c0c55c8852f757bdfa9013b2dd2333f3c4961a

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -23324,24 +23324,6 @@ options
 verify_fission
                 
 "
-vertical_tab
-"
-:
-self
-.
-extraPrefs
-.
-get
-(
-"
-sidebar
-.
-verticalTabs
-"
-False
-)
-                
-"
 webgl_ipc
 "
 :
