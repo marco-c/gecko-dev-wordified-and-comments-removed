5350b9b93afa2a2423616618527e4d38c54bad8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-02-16 02:39:10 +0000

    Backed out changeset 8adf0a890d71 (bug 1943056) for causing build bustages  TestStandardURL.cpp CLOSED TREE
    UltraBlame original commit: c7c0bea7741cbe9eece736e9a925ea5400734025

--- netwerk/base/nsStandardURL.h
+++ netwerk/base/nsStandardURL.h
@@ -1241,6 +1241,8 @@ CheckIfHostIsAscii
 void
 CoalescePath
 (
+netCoalesceFlags
+coalesceFlag
 char
 *
 path
@@ -2741,12 +2743,10 @@ af5a
 /
 \
 0xb8e3e97b
-\
 0x1ccd
-\
 0x4b45
-\
 {
+\
 0xaf
 0x5a
 0x79
@@ -2755,7 +2755,9 @@ af5a
 0x70
 0xf5
 0xd7
+\
 }
+\
 }
 inline
 nsDependentCSubstring
