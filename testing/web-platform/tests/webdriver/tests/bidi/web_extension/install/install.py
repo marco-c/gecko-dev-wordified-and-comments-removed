6b3daf43c42d4c3e1c278312ecd665ad24efa10c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 01:27:04 +0000

    Bug 1951052 [wpt PR 51016] - [wdspec] fix extension tests for Chrome, a=testonly
    Automatic update from web-platform-tests
    [wdspec] fix extension tests for Chrome (#51016)
    
    --
    
    wpt-commits: eb34aee12c74becc4618a4ab179034049bbe9463
    wpt-pr: 51016
    
    UltraBlame original commit: aa10ddaa4e7ad4f0946385f58b356b8b803223b2

--- testing/web-platform/tests/webdriver/tests/bidi/web_extension/install/install.py
+++ testing/web-platform/tests/webdriver/tests/bidi/web_extension/install/install.py
@@ -133,6 +133,27 @@ path
 try
 :
         
+if
+extension_data
+[
+"
+id
+"
+]
+is
+None
+:
+          
+assert
+isinstance
+(
+web_extension
+str
+)
+        
+else
+:
+          
 assert
 web_extension
 =
