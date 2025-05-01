bc15a35ed4dff9ad445d149e10d92c7a06cf6b7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 01:28:28 +0000

    Bug 1948069 - Fix pipelint on Windows r=ahal
    The pipelint tool was not working correctly on Windows for three reasons:
    
    1. The module was being imported into every child process spawned by the
       lint roller's multiprocessing executor. Because the import of
       pipelint has side-effects (the actual running of mach lint on the
       input), each child process would end up recursively triggering these
       side-effects and causing exponential process growth until they all
       eventually crashed.
    
       To address this we now have the logic of the pipelint script behind a
       `if __name__ == "__main__" guard.
    
    2. We were keeping the file handle open before we called into mach,
       which resulted in a permissions error trying to read from the file
       handle after mach had finished.
    
       To address this we now immediately close the file before invoking
       `mach lint` and re-open it for reading after.
    
    3. We were using default newline handling inside `pipelint` and
       `mozlint`, which resulted in LFs being translated to CRLFs
       automatically in all the intermediate temporary files.
    
       To address this, we open files for reading and writing in binary mode
       so that we deal directly with the underlying bytes instead of
       particular interpretations of bytes (e.g., newlines, CP252 vs UTF-8),
       including `stdin` (when reading file input from `jj`) and `stdout`
       (when outputting the final formatted files).
    
    Differential Revision: https://phabricator.services.mozilla.com/D244513
    
    UltraBlame original commit: f5a67487d18f277a27c5b5e8c875cad873df3b94

--- python/mozlint/mozlint/cli.py
+++ python/mozlint/mozlint/cli.py
@@ -2753,7 +2753,7 @@ NamedTemporaryFile
 mode
 =
 "
-w
+wb
 "
             
 delete
@@ -2787,17 +2787,39 @@ fpath
         
 )
         
+with
+os
+.
+fdopen
+(
+sys
+.
+stdin
+.
+fileno
+(
+)
+"
+rb
+"
+closefd
+=
+False
+)
+as
+stdin
+:
+            
 stdin_tempfile
 .
 write
 (
+b
 "
 "
 .
 join
 (
-sys
-.
 stdin
 )
 )
@@ -3116,7 +3138,7 @@ open
 (
 dump_stdin_file
 "
-w
+wb
 "
 )
         
@@ -3129,25 +3151,23 @@ open
 stdin_tempfile
 .
 name
+"
+rb
+"
 )
 as
 fp
 :
                 
-print
+dump_stdin_file
+.
+write
 (
 fp
 .
 read
 (
 )
-.
-strip
-(
-)
-file
-=
-dump_stdin_file
 )
         
 finally
