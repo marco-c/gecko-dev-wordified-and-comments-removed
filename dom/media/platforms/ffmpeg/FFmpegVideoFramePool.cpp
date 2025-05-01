6a1c558a630a3a641dea5b6826aadc99ffdc3dd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-01 02:13:03 +0000

    Backed out changeset 3d697e0c9308 (bug 1960639) for causing bustages at DMABufSurface.cpp. CLOSED TREE
    UltraBlame original commit: c7a7a3beee3b53af82ce57acade5e5eeb3a2b30f

--- dom/media/platforms/ffmpeg/FFmpegVideoFramePool.cpp
+++ dom/media/platforms/ffmpeg/FFmpegVideoFramePool.cpp
@@ -1936,7 +1936,7 @@ vaDesc
 .
 fourcc
 =
-VA_FOURCC_I420
+VA_FOURCC_YV12
 ;
 MOZ_ASSERT
 (
--- widget/gtk/DMABufSurface.h
+++ widget/gtk/DMABufSurface.h
@@ -117,15 +117,6 @@ VA_FOURCC_NV12
 endif
 #
 ifndef
-VA_FOURCC_I420
-#
-define
-VA_FOURCC_I420
-0x30323449
-#
-endif
-#
-ifndef
 VA_FOURCC_YV12
 #
 define
