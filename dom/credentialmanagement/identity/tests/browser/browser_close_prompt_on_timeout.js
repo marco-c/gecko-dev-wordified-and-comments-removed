86f155d44a7a0c8376f83dbdeed7395e89b25fe8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-14 11:57:20 +0000

    Bug 1959669 - Block FedCM for some cases where there is not user activation - r=anti-tracking-reviewers,webidl,smaug,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D246048
    
    UltraBlame original commit: e60cf29630344090cca58817410a2a309a6da57d

--- dom/credentialmanagement/identity/tests/browser/browser_close_prompt_on_timeout.js
+++ dom/credentialmanagement/identity/tests/browser/browser_close_prompt_on_timeout.js
@@ -74,6 +74,14 @@ function
 (
 )
 {
+content
+.
+document
+.
+notifyUserGestureActivation
+(
+)
+;
 let
 promise
 =
@@ -89,6 +97,11 @@ get
 identity
 :
 {
+mode
+:
+"
+active
+"
 providers
 :
 [
@@ -104,7 +117,7 @@ example
 .
 net
 /
-tests
+browser
 /
 dom
 /
