f64d7653d3ed329d33caced8e98c2b03d043cf1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-28 15:22:19 +0000

    Bug 1955060 - Allow using BigInt64Array etc. in WebIDL. r=webidl,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D243473
    
    UltraBlame original commit: 1781a48f69a82bb6f5bbe83489b52fa776ab3789

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -21333,6 +21333,14 @@ Float32Array
 "
 Float64Array
 "
+        
+"
+BigInt64Array
+"
+        
+"
+BigUint64Array
+"
     
 )
     
@@ -21659,6 +21667,26 @@ IDLType
 Tags
 .
 interface
+        
+Types
+.
+BigInt64Array
+:
+IDLType
+.
+Tags
+.
+interface
+        
+Types
+.
+BigUint64Array
+:
+IDLType
+.
+Tags
+.
+interface
     
 }
     
@@ -21927,6 +21955,22 @@ Float64Array
 :
 "
 Float64Array
+"
+        
+Types
+.
+BigInt64Array
+:
+"
+BigInt64Array
+"
+        
+Types
+.
+BigUint64Array
+:
+"
+BigUint64Array
 "
     
 }
@@ -22922,7 +22966,7 @@ IDLBuiltinType
 .
 Types
 .
-Float64Array
+BigUint64Array
         
 )
     
@@ -24992,6 +25036,68 @@ IDLBuiltinType
 Types
 .
 Float64Array
+    
+)
+    
+IDLBuiltinType
+.
+Types
+.
+BigInt64Array
+:
+IDLBuiltinType
+(
+        
+BuiltinLocation
+(
+"
+<
+builtin
+type
+>
+"
+)
+        
+"
+BigInt64Array
+"
+        
+IDLBuiltinType
+.
+Types
+.
+BigInt64Array
+    
+)
+    
+IDLBuiltinType
+.
+Types
+.
+BigUint64Array
+:
+IDLBuiltinType
+(
+        
+BuiltinLocation
+(
+"
+<
+builtin
+type
+>
+"
+)
+        
+"
+BigUint64Array
+"
+        
+IDLBuiltinType
+.
+Types
+.
+BigUint64Array
     
 )
 }
@@ -54041,7 +54147,7 @@ IDLBuiltinType
 .
 Types
 .
-Float64Array
+BigUint64Array
 +
 1
         
