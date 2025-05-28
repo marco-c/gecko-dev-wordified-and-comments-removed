6fd70581d4f3d8a55db5abb1bfb3ee569b6c459e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-05-28 15:28:01 +0000

    Revert "Bug 1857116 part 3: Enable browser_textSelectionContainer.js on CI. Tag it as os_integration so it is verified when upgrading Windows on CI. r=jmaher" for causing mochitest failures in browser_textSelectionContainer.js
    This reverts commit cadea370d849aff6d0e0162627b9b279d52340c2.
    
    Revert "Bug 1857116 part 2: Register the IAccessible2 proxy dll for automated tests on CI. r=morgan,jmaher"
    
    This reverts commit 7336c118e3f37d264f97c0b1b780339e403c051d.
    
    Revert "Bug 1857116 part 1: Reinstate building of the IAccessible2 proxy dll. r=morgan"
    
    This reverts commit 71c9f3666b4a03878a5de02699f5d2e8ec7dc4c2.
    
    UltraBlame original commit: ad3632591368fdad9155f308886b244a8e16c2e6

--- python/mozbuild/mozbuild/action/test_archive.py
+++ python/mozbuild/mozbuild/action/test_archive.py
@@ -1956,46 +1956,6 @@ mochitest
 "
         
 }
-        
-{
-            
-"
-source
-"
-:
-buildconfig
-.
-topobjdir
-            
-"
-base
-"
-:
-"
-dist
-/
-bin
-"
-            
-"
-pattern
-"
-:
-"
-IA2Marshal
-.
-dll
-"
-            
-"
-dest
-"
-:
-"
-mochitest
-"
-        
-}
     
 ]
     
