89a063f2bb195abdf5a2b2ab8b9115d3fd738eeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-01 01:01:29 +0000

    Bug 1967793 - Properly implement hardware check in VideoEncoderConfig.isConfigSupported. r=tjr,media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D250780
    
    UltraBlame original commit: 1fdc8d90f80e738883fb3c5d564a3d779eab5a41

--- dom/media/webcodecs/VideoEncoder.cpp
+++ dom/media/webcodecs/VideoEncoder.cpp
@@ -12,6 +12,13 @@ h
 #
 include
 "
+EncoderConfig
+.
+h
+"
+#
+include
+"
 mozilla
 /
 dom
@@ -190,6 +197,13 @@ h
 #
 include
 "
+nsIGlobalObject
+.
+h
+"
+#
+include
+"
 nsPrintfCString
 .
 h
@@ -197,7 +211,7 @@ h
 #
 include
 "
-nsReadableUtils
+nsRFPService
 .
 h
 "
@@ -1107,6 +1121,7 @@ Maybe
 <
 T
 >
+&
 aRHS
 )
 {
@@ -2350,6 +2365,9 @@ VideoEncoderConfigInternal
 >
 &
 aConfig
+nsIGlobalObject
+*
+aGlobal
 )
 {
 if
@@ -2471,6 +2489,12 @@ false
 ;
 }
 }
+ApplyResistFingerprintingIfNeeded
+(
+aConfig
+aGlobal
+)
+;
 return
 EncoderSupport
 :
@@ -2789,6 +2813,7 @@ VideoEncoderConfigInternal
 (
 aConfig
 )
+nullptr
 )
 ;
 }
@@ -3744,6 +3769,7 @@ VideoEncoderConfigInternal
 (
 config
 )
+global
 )
 ;
 VideoEncoderSupport
