d869be19fb7799e4e2ffe7f1dd1cee3f328f965f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-11 01:11:19 +0000

    Bug 1963852 - Vendor libwebrtc from f14533349f
    Upstream commit: https://webrtc.googlesource.com/src/+/f14533349fc3ebcbf82ac8a371abef69ec4acdd8
        avoid shadowing codec_name in video codec test fixture
    
        by renaming the argument. Sadly initializer lists do not work here
    
        BUG=webrtc:42223409
    
        Change-Id: Ice0c1109e88e01013e2a0585ca2c147ef531bb57
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/382062
        Reviewed-by: Danil Chapovalov <danilchapwebrtc.org>
        Commit-Queue: Philipp Hancke <phanckemeta.com>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#44201}
    
    UltraBlame original commit: d3421a2ad58ad574cd34c75253774ca0596fed6d

--- third_party/libwebrtc/modules/video_coding/codecs/test/videocodec_test_fixture_impl.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/test/videocodec_test_fixture_impl.cc
@@ -1535,7 +1535,7 @@ std
 :
 :
 string
-codec_name
+codec_name_to_set
 size_t
 num_simulcast_streams
 size_t
@@ -1554,12 +1554,9 @@ size_t
 height
 )
 {
-this
--
->
 codec_name
 =
-codec_name
+codec_name_to_set
 ;
 VideoCodecType
 codec_type
