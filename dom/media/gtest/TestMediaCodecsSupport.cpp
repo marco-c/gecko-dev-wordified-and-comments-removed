1aa08a8a47e413fe38f3c553b52976a2efe538e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-06-16 00:59:08 +0000

    Bug 1965705 - Part 2. Expose software/hardware encoding status on about:support. r=media-playback-reviewers,webcompat-reviewers,fluent-reviewers,twisniewski,padenot,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D248880
    
    UltraBlame original commit: 1b2b1850ed9ea6b449894cf88ee8dc000e397dae

--- dom/media/gtest/TestMediaCodecsSupport.cpp
+++ dom/media/gtest/TestMediaCodecsSupport.cpp
@@ -1041,8 +1041,8 @@ targetString
 =
 "
 H264
-SW
-HW
+SWDEC
+HWDEC
 "
 _ns
 ;
@@ -1066,7 +1066,7 @@ targetString
 =
 "
 VP8
-SW
+SWDEC
 "
 _ns
 ;
@@ -1090,7 +1090,7 @@ targetString
 =
 "
 VP9
-HW
+HWDEC
 "
 _ns
 ;
