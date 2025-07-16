fdee6ebb39642fd2ac23d1c264e1803af19cde88
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 02:38:31 +0000

    Bug 1976135 - Remove nightly only limitation of pref gfx.webrender.layer-compositor r=gfx-reviewers,gw
    Differential Revision: https://phabricator.services.mozilla.com/D256379
    
    UltraBlame original commit: 23763b72c35abeead04f63e4503c28abc73d065d

--- gfx/webrender_bindings/DCLayerTree.cpp
+++ gfx/webrender_bindings/DCLayerTree.cpp
@@ -2620,9 +2620,6 @@ UseLayerCompositor
 )
 const
 {
-#
-ifdef
-NIGHTLY_BUILD
 return
 UseNativeCompositor
 (
@@ -2636,13 +2633,6 @@ gfx_webrender_layer_compositor_AtStartup
 (
 )
 ;
-#
-else
-return
-false
-;
-#
-endif
 }
 void
 DCLayerTree
