8489e88f2db610086165965ebeb27ae63f6f84b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-28 18:46:33 +0000

    Bug 1977769 - Remove nearestViewportElement and farthestViewportElement r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D257648
    
    UltraBlame original commit: 62fc123c73ba55d45b768fb967ba4942f9b4fdde

--- dom/base/nsDeprecatedOperationList.h
+++ dom/base/nsDeprecatedOperationList.h
@@ -208,14 +208,6 @@ SVGDeselectAll
 )
 DEPRECATED_OPERATION
 (
-SVGNearestViewportElement
-)
-DEPRECATED_OPERATION
-(
-SVGFarthestViewportElement
-)
-DEPRECATED_OPERATION
-(
 IDBObjectStoreCreateIndexLocale
 )
 DEPRECATED_OPERATION
