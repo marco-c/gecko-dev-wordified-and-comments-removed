21ee7e83dff244d081b80f68d2625a99139f7325
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-08 14:38:56 +0000

    Bug 1385832 - Added support for getBytesInUse() to local and managed storage areas. r=extension-reviewers,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D259498
    
    UltraBlame original commit: 54a170b0d86da31456e799faa6d2fbd44c3125c5

--- toolkit/components/extensions/test/xpcshell/test_ext_storage_content_sync.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_storage_content_sync.js
@@ -121,7 +121,7 @@ test_contentscript_bytes_in_use_sync
 )
 {
 await
-test_contentscript_storage_area_with_bytes_in_use
+test_contentscript_storage_sync_getBytesInUse
 (
 "
 sync
--- toolkit/components/extensions/test/xpcshell/test_ext_storage_content_sync_kinto.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_storage_content_sync_kinto.js
@@ -121,7 +121,7 @@ test_contentscript_storage_no_bytes_in_use
 )
 {
 await
-test_contentscript_storage_area_with_bytes_in_use
+test_contentscript_storage_sync_getBytesInUse
 (
 "
 sync
