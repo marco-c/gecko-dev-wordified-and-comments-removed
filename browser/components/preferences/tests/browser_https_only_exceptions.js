921beb64c60c6436f13f56dc10d43b387f6e3a8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-01 02:40:28 +0000

    Revert "Bug 1971430, Bug 1971431 - Convert Deceptive Content section to config-based prefs - r=emz,mkennedy" for causing bc failures on browser_security-2.js
    This reverts commit a49815e45eed608b02638eef45c6818f3c01df44.
    
    Revert "Bug 1971430 - Convert HTTPS-Only Mode to config-based prefs - r=emz,fluent-reviewers,mkennedy,bolsson"
    
    This reverts commit f0bd12bf24c27d97912e37197a2ae413f50c8553.
    
    UltraBlame original commit: 073f6764364889c11350c3357674167eafee558e

--- browser/components/preferences/tests/browser_https_only_exceptions.js
+++ browser/components/preferences/tests/browser_https_only_exceptions.js
@@ -289,7 +289,7 @@ xhtml
 ;
 exceptionButton
 .
-click
+doCommand
 (
 )
 ;
@@ -1368,19 +1368,6 @@ observances
 }
 )
 ;
-SpecialPowers
-.
-clearUserPref
-(
-"
-dom
-.
-security
-.
-https_only_mode_ever_enabled_pbm
-"
-)
-;
 BrowserTestUtils
 .
 removeTab
@@ -1541,7 +1528,7 @@ httpsOnlyExceptionButton
 "
 )
 .
-click
+doCommand
 (
 )
 ;
