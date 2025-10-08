a6927fbd2927226dbb12e9c39fbc958abeb767ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-08 09:14:47 +0000

    Bug 1988076: Shutdown cleanup issues for Compression Dictionaries r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D264524
    
    UltraBlame original commit: adbfcbccfbb5ef04699b38c68697ffbacda91090

--- netwerk/cache2/Dictionary.h
+++ netwerk/cache2/Dictionary.h
@@ -201,6 +201,7 @@ NS_DECL_THREADSAFE_ISUPPORTS
 NS_DECL_NSICACHEENTRYOPENCALLBACK
 NS_DECL_NSIREQUESTOBSERVER
 NS_DECL_NSISTREAMLISTENER
+explicit
 DictionaryCacheEntry
 (
 const
@@ -1169,6 +1170,12 @@ Init
 (
 )
 ;
+static
+void
+Shutdown
+(
+)
+;
 nsresult
 AddEntry
 (
@@ -1326,7 +1333,7 @@ this
 private
 :
 static
-nsCOMPtr
+StaticRefPtr
 <
 nsICacheStorage
 >
