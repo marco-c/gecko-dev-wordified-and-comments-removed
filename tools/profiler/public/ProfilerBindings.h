0a15cd3ceec61f1d932d09c446ec7c0505d9e14b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-30 05:46:27 +0000

    Revert "Bug 1968065 - expose Flow Marker API to Rust r=profiler-reviewers,canaltinova" for causing build bustages
    This reverts commit a0e172eccc37c27c5bcfb9e8c7291d54c341a29b.
    
    UltraBlame original commit: 6e0dd1421c8affd4915c151970e6fe9e979c759b

--- tools/profiler/public/ProfilerBindings.h
+++ tools/profiler/public/ProfilerBindings.h
@@ -423,17 +423,6 @@ aLabelLength
 )
 ;
 void
-gecko_profiler_marker_schema_set_stack_based
-(
-mozilla
-:
-:
-MarkerSchema
-*
-aSchema
-)
-;
-void
 gecko_profiler_marker_schema_add_key_format
 (
 mozilla
