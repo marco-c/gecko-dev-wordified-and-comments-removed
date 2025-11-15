ff5461211f40e9bb4c4781abcdd45cbbc070a194
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-15 00:40:20 +0000

    Revert "Bug 1996818 - Store gpu addresses as a u32. r=gw" for causing mochitests-plain failures in test_ext_web_accessible_resources.html
    This reverts commit f04bdbe632dd6c596bce383ddc76e2b26ef0c3f9.
    
    This reverts commit c4c1d2a01faeae04eabd837aa393dc621c46304a.
    
    This reverts commit de3703a4ecf74306332c88cea762c265dda69b0e.
    
    This reverts commit 5e29586453931121d577a3c3fe7f80e8dbc5a304.
    
    This reverts commit 0f3a303c01170f22166784f30d2e1d135942296d.
    
    UltraBlame original commit: 2ea9dceae1ce7c427e86eea23e5a4c3740271e3d

--- gfx/layers/wr/WebRenderMessageUtils.h
+++ gfx/layers/wr/WebRenderMessageUtils.h
@@ -1991,6 +1991,12 @@ a
 clip_stores
 a
 .
+gpu_cache_metadata
+a
+.
+gpu_cache_cpu_mirror
+a
+.
 hit_testers
 a
 .
@@ -2025,6 +2031,9 @@ frame_allocator
 a
 .
 render_tasks
+a
+.
+gpu_cache_textures
 a
 .
 vertex_data_textures
