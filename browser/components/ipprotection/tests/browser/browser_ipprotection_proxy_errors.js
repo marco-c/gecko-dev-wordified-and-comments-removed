a4f97461b4a885c73e26e395bb37903cbfe89744
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-11-19 00:21:02 +0000

    Bug 1998454 - Use Masque when declared supported by server r=ip-protection-reviewers,baku
    Differential Revision: https://phabricator.services.mozilla.com/D271613
    
    UltraBlame original commit: d627a088ef0a2a42b2a2a2f48eb4010ce0f97f58

--- browser/components/ipprotection/tests/browser/browser_ipprotection_proxy_errors.js
+++ browser/components/ipprotection/tests/browser/browser_ipprotection_proxy_errors.js
@@ -141,13 +141,7 @@ initialize
 "
 proxyInfo
 .
-host
-proxyInfo
-.
-port
-proxyInfo
-.
-type
+server
 )
 ;
 filter
--- browser/components/ipprotection/tests/browser/browser_ipprotection_usage.js
+++ browser/components/ipprotection/tests/browser/browser_ipprotection_usage.js
@@ -90,13 +90,7 @@ initialize
 "
 proxyInfo
 .
-host
-proxyInfo
-.
-port
-proxyInfo
-.
-type
+server
 )
 ;
 filter
