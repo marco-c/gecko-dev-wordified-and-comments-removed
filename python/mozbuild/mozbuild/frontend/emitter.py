a996448c24722e3a73f038300360589e0e2308b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 02:56:15 +0000

    Bug 988938 - Replace toolkit/components/telemetry/Makefile.in by moz.build (s) r=glandium,toolkit-telemetry-reviewers
    The idea is to rely on existing support for XPI packaging, adding a
    flavor for test files in the process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D273009
    
    UltraBlame original commit: d21341848997257e477c0c6122b3833e1087dac7

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -7793,6 +7793,10 @@ WASM_LIBS
 "
 XPI_PKGNAME
 "
+            
+"
+XPI_TESTDIR
+"
         
 ]
         
@@ -7824,6 +7828,34 @@ context
 [
 v
 ]
+        
+if
+"
+XPI_TESTDIR
+"
+in
+context
+and
+"
+XPI_PKGNAME
+"
+not
+in
+context
+:
+            
+raise
+SandboxValidationError
+(
+"
+XPI_TESTDIR
+set
+but
+XPI_PKGNAME
+not
+set
+"
+)
         
 if
 (
