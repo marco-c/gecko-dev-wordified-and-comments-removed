f2f62ae23c7cedd7781370609a8bf34272f88a2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-01-07 18:40:56 +0000

    Bug 2007635 - Update NSS to 213eff47d0697b06a123d954db4ce2fd46e5bd1a UPGRADE_NSS_RELEASE r=nss-reviewers,djackson
    Differential Revision: https://phabricator.services.mozilla.com/D277499
    
    UltraBlame original commit: 5ca81a021645fefb98feed3eca6c0c36df09d7e1

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -22,7 +22,8 @@ NSSUTIL_VERSION
 "
 3
 .
-119
+120
+Beta
 "
 #
 define
@@ -31,7 +32,7 @@ NSSUTIL_VMAJOR
 #
 define
 NSSUTIL_VMINOR
-119
+120
 #
 define
 NSSUTIL_VPATCH
@@ -43,7 +44,7 @@ NSSUTIL_VBUILD
 #
 define
 NSSUTIL_BETA
-PR_FALSE
+PR_TRUE
 SEC_BEGIN_PROTOS
 extern
 const
