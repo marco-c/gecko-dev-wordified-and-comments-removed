fff790c4eccca5754ed0325a5f9212d409bf8c0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-20 00:36:09 +0000

    Bug 2023091 - Extend backup names to include seconds and milliseconds to avoid naming collisions. r=cdupuis
    Differential Revision: https://phabricator.services.mozilla.com/D288210
    
    UltraBlame original commit: 246c63f71facf3d370036b621bd5b91120eda1fa

--- browser/components/backup/tests/xpcshell/test_BackupService_finalizeSingleFileArchive.js
+++ browser/components/backup/tests/xpcshell/test_BackupService_finalizeSingleFileArchive.js
@@ -45,7 +45,8 @@ _
 [
 0
 -
-9_
+9
+.
 -
 ]
 +
@@ -307,7 +308,9 @@ profileName
 }
 _20200101
 -
-0000
+000000
+.
+000
 .
 html
 )
@@ -391,7 +394,9 @@ join
 gTestDestPath
 FirefoxBackup_SomeOtherProfile_20200101
 -
-0000
+000000
+.
+000
 .
 html
 )
