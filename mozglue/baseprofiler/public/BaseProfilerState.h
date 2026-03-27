820ffa355e7277ee34ce1999dfea3bc34ad19156
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 00:51:21 +0000

    Bug 2024724 - Fix PowerCounters.h because we do not compile PowerCounters-win.cpp against __MINGW32__ r=canaltinova,profiler-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D288844
    
    UltraBlame original commit: 17b1d9c5a780b9743a6c0b07fe4cc12b5dca2165

--- mozglue/baseprofiler/public/BaseProfilerState.h
+++ mozglue/baseprofiler/public/BaseProfilerState.h
@@ -561,6 +561,12 @@ defined
 (
 GP_OS_windows
 )
+&
+&
+defined
+(
+_MSC_VER
+)
 #
 define
 POWER_HELP
--- tools/profiler/public/ProfilerState.h
+++ tools/profiler/public/ProfilerState.h
@@ -146,6 +146,12 @@ defined
 (
 GP_OS_windows
 )
+&
+&
+defined
+(
+_MSC_VER
+)
 #
 define
 POWER_HELP
