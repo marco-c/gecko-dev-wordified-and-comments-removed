b1c6699e8d79fa23f5c658969e6c50cce44b7b5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 00:26:41 +0000

    Revert "Bug 2016667, Bug 1999953 - Bump gecko profiler format version r=dcarver,profiler-reviewers" for causing bc failures at browser_test_feature_js_source_table.js
    This reverts commit 771625c80e95a66d2443c3aab92c10c62121c713.
    
    This reverts commit f93aaa2bb63ace845513e91db7c2bebf9dd7076f.
    
    This reverts commit 8f232d27ef9b552d8c6b507ec0c254ebb77500e2.
    
    This reverts commit fa0bd68c5a4bdf05dfadbda014ef6c320fb462db.
    
    This reverts commit a5990db079f138996f4bbcd4a6ebdd898b7f541c.
    
    This reverts commit acf8ae84505ee5d7c303344e319a8f3cbd7e91c6.
    
    UltraBlame original commit: d95a978b1f29d7dea5595cd2f536afa234591ee6

--- tools/profiler/tests/browser/browser_test_feature_js_sources_retrieval.js
+++ tools/profiler/tests/browser/browser_test_feature_js_sources_retrieval.js
@@ -281,7 +281,7 @@ length
 for
 (
 const
-sourceId
+sourceUuid
 in
 sources
 )
@@ -291,7 +291,7 @@ sourceText
 =
 sources
 [
-sourceId
+sourceUuid
 ]
 ;
 Assert
@@ -299,7 +299,7 @@ Assert
 ok
 (
 typeof
-sourceId
+sourceUuid
 =
 =
 =
@@ -310,11 +310,11 @@ string
 &
 !
 !
-sourceId
+sourceUuid
 .
 length
 "
-sourceId
+sourceUuid
 should
 be
 a
@@ -346,7 +346,7 @@ sourceText
 length
 Source
 {
-sourceId
+sourceUuid
 }
 should
 be
@@ -587,7 +587,7 @@ inlineSourceLength
 for
 (
 const
-sourceId
+sourceUuid
 in
 sources
 )
@@ -597,7 +597,7 @@ sourceText
 =
 sources
 [
-sourceId
+sourceUuid
 ]
 ;
 if
@@ -931,7 +931,7 @@ null
 for
 (
 const
-sourceId
+sourceUuid
 in
 sources
 )
@@ -941,7 +941,7 @@ sourceText
 =
 sources
 [
-sourceId
+sourceUuid
 ]
 ;
 if
