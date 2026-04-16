cc11d1d05303edd3382dfd8623d189ca493057fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:31:47 +0000

    Bug 2022634: Patch unique_ptr to js::UniquePtr in RegExpMacroAssemblerTracer r=spidermonkey-reviewers,dminor
    `js::UniquePtr<T> is a typedef for `std::unique_ptr<T, JS::DeletePolicy<T>>`. The tracer constructor expects a bare std::unique_ptr, which we can't construct without calling new. As an alternative to this patch, we could exclude RegExpAPI.cpp from check_vanilla_allocations.py, but it feels like it is on the SM side of the line.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288793
    
    UltraBlame original commit: 72cc5a2dbedb349ddbbf0dd7acd62dc7d71f5ef5

--- js/src/irregexp/imported/regexp-macro-assembler-tracer.h
+++ js/src/irregexp/imported/regexp-macro-assembler-tracer.h
@@ -42,10 +42,10 @@ public
 explicit
 RegExpMacroAssemblerTracer
 (
-std
+js
 :
 :
-unique_ptr
+UniquePtr
 <
 RegExpMacroAssembler
 >
@@ -842,10 +842,10 @@ override
 ;
 private
 :
-std
+js
 :
 :
-unique_ptr
+UniquePtr
 <
 RegExpMacroAssembler
 >
