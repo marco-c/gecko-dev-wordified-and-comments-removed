de2345734a3ee35e2587f85824a31329b3c26fdd
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-14 10:35:25 +0000

    Bug 2037368 - Add support for tiled images via quad shaders. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D298783
    
    UltraBlame original commit: 5259ea5e95ea27727ecc45fb7c9bbd05e055f180

--- gfx/wr/webrender/src/quad.rs
+++ gfx/wr/webrender/src/quad.rs
@@ -820,6 +820,10 @@ local_rect
 :
 &
 LayoutRect
+local_clip_rect
+:
+&
+LayoutRect
 aligned_aa_edges
 :
 EdgeMask
@@ -981,9 +985,6 @@ strategy
 &
 pattern
 local_rect
-&
-clip_chain
-.
 local_clip_rect
 aligned_aa_edges
 transfomed_aa_edges
