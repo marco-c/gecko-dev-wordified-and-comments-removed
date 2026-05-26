e2f3d98af18388c0bb6d3483635b07890d1cb358
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-26 17:38:50 +0000

    Bug 2025893 - Add desktop telemetry r=sharing-reviewers,TravisLong,niklas
    Differential Revision: https://phabricator.services.mozilla.com/D299901
    
    UltraBlame original commit: 2b96010febf24c9e4260086d495e40086735438a

--- browser/components/contentsharing/tests/browser/head.js
+++ browser/components/contentsharing/tests/browser/head.js
@@ -250,6 +250,9 @@ assertContentSharingModal
 (
 window
 expected
+leaveOpen
+=
+false
 )
 {
 Assert
@@ -719,6 +722,15 @@ text
 ;
 }
 }
+if
+(
+leaveOpen
+)
+{
+return
+modalEl
+;
+}
 window
 .
 gDialogBox
@@ -728,6 +740,9 @@ dialog
 close
 (
 )
+;
+return
+null
 ;
 }
 async
