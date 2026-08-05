f94d173e579cefc5e1b5d6c441657e711f946cde
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-08-05 19:08:49 +0000

    Bug 2047140 [Linux] Keep D&D source GtkWidget live until D&D operations is finished r=emilio
    Recently we delete source GtkWidget while the D&D is still live. That may lead to various issues/crashes as
    Gtk still renders and uses the widget surface etc.
    
    The early delete was introduced as a hack for mem leak in Bug 219521. Let's fix that better.
    
    Differential Revision: https://phabricator.services.mozilla.com/D312259
    
    UltraBlame original commit: 18de4079c305bd793d6088f87deca0d7c7d49f11

--- widget/gtk/nsDragSessionSource.h
+++ widget/gtk/nsDragSessionSource.h
@@ -11,13 +11,6 @@ nsDragService
 .
 h
 "
-#
-include
-"
-nsIObserver
-.
-h
-"
 class
 nsICookieJarSettings
 ;
@@ -36,13 +29,9 @@ nsDragSessionSource
 :
 public
 nsDragSession
-public
-nsIObserver
 {
 public
 :
-NS_DECL_ISUPPORTS_INHERITED
-NS_DECL_NSIOBSERVER
 nsDragSessionSource
 (
 )
