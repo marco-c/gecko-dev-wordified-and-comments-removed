d1881434719c93c8d6ad99417052d5ec30833742
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-09-01 15:48:27 +0000

    Bug 2061352 - Handle eSetSelection for EditContext. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D317152
    
    UltraBlame original commit: 8ec3378d4f333b24839c402d2de67bb3f68ccf0a

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -12548,6 +12548,16 @@ aAdditionalFlags
 SELECTION_SET_FLAG_REVERSE
 )
 ;
+selectionEvent
+.
+mExpandToClusterBoundary
+=
+(
+aAdditionalFlags
+&
+SELECTION_EXPAND_TO_CLUSTER_BOUNDARY
+)
+;
 widget
 -
 >
